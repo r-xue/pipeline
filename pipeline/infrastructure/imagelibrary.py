c75cc2287a2a732b38e470cedd5f23eb9d0e84c6
vincent.geers <vincent.geers@ac9c0da5-131f-0410-9009-a9c195f183e6>
2018-10-10 09:39:42 +0000

    CAS-11891: minor pep8 fixes.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project/pipeline@42081 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/infrastructure/imagelibrary.py
+++ pipeline/infrastructure/imagelibrary.py
@@ -6,7 +6,6 @@ LOG = logging.get_logger(__name__)
 
 
 # This class contains a list ImageItem objects encoded as dictionaries.
-
 class ImageLibrary(object):
     def __init__(self):
         self._images = []
@@ -16,20 +15,19 @@ class ImageLibrary(object):
         return self._images
 
     # Clear the image list
-    def clear_imlist (self):
+    def clear_imlist(self):
         del self._images[:]
 
     # Add image item to the list as a dictionary
-    def add_item (self, imageitem):
-        if self.find_imageitem (imageitem) < 0:
+    def add_item(self, imageitem):
+        if self.find_imageitem(imageitem) < 0:
             self._images.append(dict(imageitem))
         else:
-            LOG.warning('Image item %s already in list' % \
-                    (imageitem.imagename))
+            LOG.warning('Image item %s already in list' % imageitem.imagename)
 
     # Remove image item from the list
     def delete_item(self, imageitem):
-        index = self.find_imageitem (imageitem)
+        index = self.find_imageitem(imageitem)
         if index >= 0:
             del self._images[index]
 
@@ -40,11 +38,11 @@ class ImageLibrary(object):
                 return i
         return -1
 
-# This class contains information for image data product
 
+# This class contains information for image data product
 class ImageItem (object):
-    def __init__(self, imagename, sourcename, spwlist, specmode, sourcetype,
-        multiterm = None, imageplot = '', metadata = {}):
+    def __init__(self, imagename, sourcename, spwlist, specmode, sourcetype, multiterm=None, imageplot='',
+                 metadata={}):
         self.imagename = imagename
         self.sourcename = sourcename
         self.spwlist = spwlist
@@ -53,19 +51,22 @@ class ImageItem (object):
         self.multiterm = multiterm
         self.imageplot = imageplot
         self._metadata = metadata
+
     def __iter__(self):
         return vars(self).iteritems()
 
+
 # This class defines the image meta data that will be stored for
 # each final cleaned image.
-
 class ImageMetadata (dict):
     # This is a test.
     _keys = ['IMCENTER', 'IMRES']
+
     def __init__(self):
         dict.__init__(self)
         for key in ImageMetadata._keys:
             self[key] = None
+
     def __setitem__(self, key, val):
         if key not in ImageMetadata._keys:
             raise KeyError
