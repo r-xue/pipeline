ea1467d4126b81fdb80cf6b19dda59cab945c547
vincent.geers <vincent.geers@ac9c0da5-131f-0410-9009-a9c195f183e6>
2019-01-09 13:20:41 +0000

    CAS-12107, CAS-11891: removed unused modules from pipeline/hsd/heuristics/: - gridding.py - grouping.py - observingpattern.py
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project/pipeline@42233 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/hsd/heuristics/__init__.py
+++ pipeline/hsd/heuristics/__init__.py
@@ -1,13 +1,8 @@
 from __future__ import absolute_import
 
-from .gridding import GenerateGrid
-from .grouping import GroupByPosition
 from .grouping2 import GroupByPosition2
-from .grouping import GroupByTime
 from .grouping2 import GroupByTime2
-from .grouping import MergeGapTables
 from .grouping2 import MergeGapTables2
-from .observingpattern import ObservingPattern
 from .observingpattern2 import ObservingPattern2
 from .sdbeamsize import SingleDishBeamSize
 from .sdbeamsize import SingleDishBeamSizeFromName
