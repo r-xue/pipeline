d1ea8381d6076dd5f4d14d56fb8ee8f22bfd51e4
vincent.geers <vincent.geers@ac9c0da5-131f-0410-9009-a9c195f183e6>
2019-01-09 13:38:19 +0000

    CAS-12107, CAS-11891: removed unused flux calibration and flux normalisation modules from pipeline/hif/tasks/fluxscale/: - fluxcal.py - normflux.py
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project/pipeline@42238 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/hif/tasks/fluxscale/__init__.py
+++ pipeline/hif/tasks/fluxscale/__init__.py
@@ -1,14 +1,5 @@
 from __future__ import absolute_import
 
-# The following 2 tasks are old general purpose utility tasks which
-# are no longer used or imported into the task dictionaries but are
-# retained for the purposes of illusrating how to combine several
-# CASA tasks to derive a flux scale and how to do a simple flux
-# normalization.
-
-#from .fluxcal import Fluxcal
-#from .normflux import NormaliseFlux
-
 from .fluxscale import Fluxscale
 
 import pipeline.infrastructure.renderer.weblog as weblog
