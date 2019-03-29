24bcd12506fb33d6d4a93406f9982211b9d2879f
takeshi.nakazato <takeshi.nakazato@ac9c0da5-131f-0410-9009-a9c195f183e6>
2019-03-29 05:21:57 +0000

    PIPE-227 define hsdn_exportdata task
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project/pipeline@42360 ac9c0da5-131f-0410-9009-a9c195f183e6

---
+++ pipeline/hsdn/tasks/exportdata/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+
+import pipeline.infrastructure.renderer.weblog as weblog
+import pipeline.infrastructure.renderer.basetemplates as super_renderer
+
+from .exportdata import NROExportData
+
+# NOTE: QA handling is not necessary since SDExportData produces 
+#       same results class as ExportData so that results are handled
+#       in the same way as ExportData.
+
+weblog.add_renderer(NROExportData, 
+                    super_renderer.T2_4MDetailsDefaultRenderer(uri='exportdata.mako',
+                                                               description='Prepare pipeline data products for export',
+                                                               always_rerender=True),
+                    group_by=weblog.UNGROUPED)
---
+++ pipeline/hsdn/tasks/exportdata/exportdata.py
@@ -0,0 +1,50 @@
+"""
+Single-Dish Exportdata task dedicated to NRO data.
+
+Please see hsd/tasks/exportdata/exportdata.py for generic 
+description on how Exportdata task works.
+"""
+from __future__ import absolute_import
+
+import pipeline.h.tasks.exportdata.exportdata as exportdata
+import pipeline.infrastructure as infrastructure
+from pipeline.infrastructure import task_registry
+import pipeline.hsd.tasks.exportdata.exportdata as sdexportdata
+
+
+# the logger for this module
+LOG = infrastructure.get_logger(__name__)
+
+
+class NROPipelineNameBuilder(exportdata.PipelineProductNameBuiler):
+    @classmethod
+    def _build_from_oussid(self, basename, ousstatus_entity_id=None, output_dir=None):
+        return self._join_dir(basename, output_dir)
+
+    @classmethod
+    def _build_from_ps_oussid(self, basename, project_structure=None, ousstatus_entity_id=None, output_dir=None):
+        return self._join_dir(basename, output_dir)
+    
+    @classmethod
+    def _build_from_oussid_session(self, basename, ousstatus_entity_id=None, session_name=None, output_dir=None):
+        return self._join_dir(basename, output_dir)
+
+
+@task_registry.set_equivalent_casa_task('hsdn_exportdata')
+@task_registry.set_casa_commands_comment('The output data products are computed.')
+class NROExportData(sdexportdata.SDExportData):
+    """
+    NROExportData is the base class for exporting data to the products
+    subdirectory. It performs the following operations:
+
+    - Saves the pipeline processing request in an XML file
+    - Saves the images in FITS cubes one per target and spectral window
+    - Saves the final flags and bl coefficient per ASDM in a compressed / tarred CASA flag
+      versions file
+    - Saves the final web log in a compressed / tarred file
+    - Saves the text formatted list of contents of products directory
+    """
+    Inputs = sdexportdata.SDExportDataInputs
+
+    NameBuilder = NROPipelineNameBuilder
+
