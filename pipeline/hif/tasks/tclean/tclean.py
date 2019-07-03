r-xue/pipeline

commit 8ac9eba8cc0bf32de6aa29be4b3ef5c2ef8ff9a5

PIPE-355: Added new image header keywords. OUS IDs still missing.
git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project/pipeline@42585 ac9c0da5-131f-0410-9009-a9c195f183e6

pipeline/hif/tasks/tclean/tclean.py
@@ -616,6 +616,8 @@ class Tclean(cleanbase.CleanBase):
 
         # All continuum
         if inputs.specmode == 'cube' and inputs.spwsel_all_cont:
+            self._update_miscinfo(result.image, len(self.image_heuristics.field(inputs.intent, inputs.field)), pbcor_image_min, pbcor_image_max)
+
             result.set_image_min(pbcor_image_min)
             result.set_image_max(pbcor_image_max)
             result.set_image_rms(nonpbcor_image_non_cleanmask_rms)
@@ -722,6 +724,8 @@ class Tclean(cleanbase.CleanBase):
                                                   pblimit_image=self.pblimit_image,
                                                   pblimit_cleanmask=self.pblimit_cleanmask,
                                                   cont_freq_ranges=self.cont_freq_ranges)
+
+            self._update_miscinfo(result.image, len(self.image_heuristics.field(inputs.intent, inputs.field)), pbcor_image_min, pbcor_image_max)
 
             keep_iterating, hm_masking = self.image_heuristics.keep_iterating(iteration, inputs.hm_masking,
                                                                               result.tclean_stopcode,
@@ -930,7 +934,8 @@ class Tclean(cleanbase.CleanBase):
             cleanbase.set_miscinfo(name=mom0_name, spw=self.inputs.spw,
                                    field=self.inputs.field, iter=maxiter, type='mom0_fc',
                                    intent=self.inputs.intent, specmode=self.inputs.specmode,
-                                   observing_run=context.observing_run)
+                                   observing_run=context.observing_run,
+                                   project_summary=context.project_summary)
 
             # Update the result.
             result.set_mom0_fc(maxiter, mom0_name)
@@ -944,7 +949,8 @@ class Tclean(cleanbase.CleanBase):
             cleanbase.set_miscinfo(name=mom8_name, spw=self.inputs.spw,
                                    field=self.inputs.field, iter=maxiter, type='mom8_fc',
                                    intent=self.inputs.intent, specmode=self.inputs.specmode,
-                                   observing_run=context.observing_run)
+                                   observing_run=context.observing_run,
+                                   project_summary=context.project_summary)
 
             # Update the result.
             result.set_mom8_fc(maxiter, mom8_name)
@@ -975,3 +981,15 @@ class Tclean(cleanbase.CleanBase):
         unit = qa.getunit(velo)
         velocity = qa.tos(qa.quantity(val, unit))
         return velocity
+
+    def _update_miscinfo(self, imagename, nfield, datamin, datamax):
+
+        # Update metadata
+        with casatools.ImageReader(imagename) as image:
+            info = image.miscinfo()
+
+            info['nfield'] = nfield
+            info['datamin'] = datamin
+            info['datamax'] = datamax
+
+            image.setmiscinfo(info)

pipeline/hif/tasks/tclean/cleanbase.py
@@ -4,12 +4,14 @@ import os
 
 import numpy as np
 
+import pipeline as pipeline
 import pipeline.infrastructure as infrastructure
 import pipeline.infrastructure.basetask as basetask
 import pipeline.infrastructure.casatools as casatools
 import pipeline.infrastructure.mpihelpers as mpihelpers
 import pipeline.infrastructure.utils as utils
 import pipeline.infrastructure.vdp as vdp
+from pipeline.infrastructure.renderer.htmlrenderer import get_casa_version
 from pipeline.infrastructure import casa_tasks
 from .resultobjects import TcleanResult
 
@@ -585,23 +587,26 @@ class CleanBase(basetask.StandardTaskTemplate):
                          type='model', iter=iter, multiterm=result.multiterm,
                          intent=inputs.intent, specmode=inputs.specmode,
                          is_per_eb=inputs.is_per_eb,
-                         observing_run=context.observing_run)
+                         observing_run=context.observing_run,
+                         project_summary=context.project_summary)
             result.set_model(iter=iter, image=model_name)
 
             # Always set info on the uncorrected image for plotting
             set_miscinfo(name=image_name, spw=inputs.spw, field=inputs.field,
                          type='image', iter=iter, multiterm=result.multiterm,
-                         intent=inputs.intent, specmode=inputs.specmode,
+                         intent=inputs.intent, specmode=inputs.specmode, robust=inputs.robust,
                          is_per_eb=inputs.is_per_eb,
-                         observing_run=context.observing_run)
+                         observing_run=context.observing_run,
+                         project_summary=context.project_summary)
 
             # Store the PB corrected image.
             if os.path.exists('%s' % (pbcor_image_name.replace('.image.pbcor', '.image.tt0.pbcor' if result.multiterm else '.image.pbcor'))):
                 set_miscinfo(name=pbcor_image_name, spw=inputs.spw, field=inputs.field,
                              type='pbcorimage', iter=iter, multiterm=result.multiterm,
-                             intent=inputs.intent, specmode=inputs.specmode,
+                             intent=inputs.intent, specmode=inputs.specmode, robust=inputs.robust,
                              is_per_eb=inputs.is_per_eb,
-                             observing_run=context.observing_run)
+                             observing_run=context.observing_run,
+                             project_summary=context.project_summary)
                 result.set_image(iter=iter, image=pbcor_image_name)
             else:
                 result.set_image(iter=iter, image=image_name)
@@ -615,7 +620,8 @@ class CleanBase(basetask.StandardTaskTemplate):
                      type='residual', iter=iter, multiterm=result.multiterm,
                      intent=inputs.intent, specmode=inputs.specmode,
                      is_per_eb=inputs.is_per_eb,
-                     observing_run=context.observing_run)
+                     observing_run=context.observing_run,
+                     project_summary=context.project_summary)
         result.set_residual(iter=iter, image=residual_name)
 
         # Store the PSF.
@@ -623,7 +629,8 @@ class CleanBase(basetask.StandardTaskTemplate):
                      type='psf', iter=iter, multiterm=result.multiterm,
                      intent=inputs.intent, specmode=inputs.specmode,
                      is_per_eb=inputs.is_per_eb,
-                     observing_run=context.observing_run)
+                     observing_run=context.observing_run,
+                     project_summary=context.project_summary)
         result.set_psf(image=psf_name)
 
         # Store the flux image.
@@ -631,7 +638,8 @@ class CleanBase(basetask.StandardTaskTemplate):
                      type='flux', iter=iter, multiterm=result.multiterm,
                      intent=inputs.intent, specmode=inputs.specmode,
                      is_per_eb=inputs.is_per_eb,
-                     observing_run=context.observing_run)
+                     observing_run=context.observing_run,
+                     project_summary=context.project_summary)
         result.set_flux(image=flux_name)
 
         # Make sure mask has path name
@@ -640,7 +648,8 @@ class CleanBase(basetask.StandardTaskTemplate):
                          type='cleanmask', iter=iter,
                          intent=inputs.intent, specmode=inputs.specmode,
                          is_per_eb=inputs.is_per_eb,
-                         observing_run=context.observing_run)
+                         observing_run=context.observing_run,
+                         project_summary=context.project_summary)
             result.set_cleanmask(iter=iter, image=inputs.mask)
         elif os.path.exists(mask_name):
             # Use mask made by tclean
@@ -648,7 +657,8 @@ class CleanBase(basetask.StandardTaskTemplate):
                          type='cleanmask', iter=iter,
                          intent=inputs.intent, specmode=inputs.specmode,
                          is_per_eb=inputs.is_per_eb,
-                         observing_run=context.observing_run)
+                         observing_run=context.observing_run,
+                         project_summary=context.project_summary)
             result.set_cleanmask(iter=iter, image=mask_name)
 
         # Keep threshold and sensitivity for QA and weblog
@@ -669,11 +679,12 @@ def rename_image(old_name, new_name, extensions=['']):
 
 
 def set_miscinfo(name, spw=None, field=None, type=None, iter=None, multiterm=None, intent=None, specmode=None,
-                 is_per_eb=None, observing_run=None):
+                 robust=None, is_per_eb=None, observing_run=None, project_summary=None):
     """
     Define miscellaneous image information
     """
     if name != '':
+        # Image name
         if multiterm:
             if name.find('.image.pbcor') != -1 and type == 'pbcorimage':
                 imagename = name.replace('.image.pbcor', '.image.tt0.pbcor')
@@ -681,6 +692,7 @@ def set_miscinfo(name, spw=None, field=None, type=None, iter=None, multiterm=Non
                 imagename = '%s.tt0' % name
         else:
             imagename = name
+
         with casatools.ImageReader(imagename) as image:
             info = image.miscinfo()
             if imagename is not None:
@@ -688,7 +700,7 @@ def set_miscinfo(name, spw=None, field=None, type=None, iter=None, multiterm=Non
                 info['nfilnam'] = len(filename_components)
                 for i in xrange(len(filename_components)):
                     info['filnam%02d' % (i+1)] = filename_components[i]
-            if spw:
+            if spw is not None:
                 if observing_run is not None:
                     spw_names = [
                         observing_run.virtual_science_spw_shortnames.get(
@@ -701,22 +713,58 @@ def set_miscinfo(name, spw=None, field=None, type=None, iter=None, multiterm=Non
                 info['nspwnam'] = len(spw_names)
                 for i in xrange(len(spw_names)):
                     info['spwnam%02d' % (i+1)] = spw_names[i]
-            if field:
+            if field is not None:
                 # TODO: Find common key calculation. Long VLASS lists cause trouble downstream.
                 #       Truncated list may cause duplicates.
                 #       Temporarily (?) remove any '"' characters
                 tmpfield = field.split(',')[0].replace('"', '')
                 info['field'] = tmpfield
-            if type:
+
+            if observing_run is not None and project_summary is not None:
+                # TODO: Use more generic approach like in the imaging heuristics
+                if project_summary.telescope == 'ALMA':
+                    corrstring = observing_run.measurement_sets[0].get_alma_corrstring()
+                elif project_summary.telescope == 'VLA':
+                    corrstring = observing_run.measurement_sets[0].get_vla_corrstring()
+                else:
+                    corrstring = ''
+
+                info['npol'] = len(corrstring.split(','))
+
+            if type is not None:
                 info['type'] = type
+
             if iter is not None:
                 info['iter'] = iter
+
             if intent is not None:
                 info['intent'] = intent
+
             if specmode is not None:
                 info['specmode'] = specmode
+
+            if robust is not None:
+                info['robust'] = robust
+
             if is_per_eb is not None:
                 info['per_eb'] = is_per_eb
+
+            # Pipeline / CASA information
+            info['pipever'] = pipeline.revision
+            info['casaver'] = get_casa_version()
+
+            # Project information
+            if project_summary is not None:
+                info['propcode'] = project_summary.proposal_code
+                info['group'] = 'N/A'
+                info['member'] = 'N/A'
+                info['sgoal'] = 'N/A'
+
+            # Some keywords should be present but are filled only
+            # for other modes (e.g. single dish).
+            info['offra'] = -999.0
+            info['offdec'] = -999.0
+
             image.setmiscinfo(info)
 
 

commit a9d118c67296e52ec7b16a3ca00b942511145bb6

PIPE-391: a fix to file name issue introduced in r42585 as a part of the work for PIPE-355.
git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project/pipeline@42624 ac9c0da5-131f-0410-9009-a9c195f183e6

pipeline/hif/tasks/tclean/tclean.py
@@ -620,7 +620,7 @@ class Tclean(cleanbase.CleanBase):
 
         # All continuum
         if inputs.specmode == 'cube' and inputs.spwsel_all_cont:
-            self._update_miscinfo(result.image, len(self.image_heuristics.field(inputs.intent, inputs.field)), pbcor_image_min, pbcor_image_max)
+            self._update_miscinfo(result.image+extension, len(self.image_heuristics.field(inputs.intent, inputs.field)), pbcor_image_min, pbcor_image_max)
 
             result.set_image_min(pbcor_image_min)
             result.set_image_max(pbcor_image_max)
@@ -729,7 +729,7 @@ class Tclean(cleanbase.CleanBase):
                                                   pblimit_cleanmask=self.pblimit_cleanmask,
                                                   cont_freq_ranges=self.cont_freq_ranges)
 
-            self._update_miscinfo(result.image, len(self.image_heuristics.field(inputs.intent, inputs.field)), pbcor_image_min, pbcor_image_max)
+            self._update_miscinfo(result.image+extension, len(self.image_heuristics.field(inputs.intent, inputs.field)), pbcor_image_min, pbcor_image_max)
 
             keep_iterating, hm_masking = self.image_heuristics.keep_iterating(iteration, inputs.hm_masking,
                                                                               result.tclean_stopcode,