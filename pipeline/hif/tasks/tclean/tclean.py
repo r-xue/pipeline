979752390795d794cca5ea700f81845ac60a021d
dirk.muders <dirk.muders@ac9c0da5-131f-0410-9009-a9c195f183e6>
2019-07-17 20:15:53 +0000

    PIPE-391: Fixed bug in nterms=2 name for pbcor case.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project/pipeline@42713 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/hif/tasks/tclean/tclean.py
+++ pipeline/hif/tasks/tclean/tclean.py
@@ -640,7 +640,7 @@ class Tclean(cleanbase.CleanBase):
 
         # All continuum
         if inputs.specmode == 'cube' and inputs.spwsel_all_cont:
-            self._update_miscinfo(result.image+extension, max([len(field_ids.split(',')) for field_ids in self.image_heuristics.field(inputs.intent, inputs.field)]), pbcor_image_min, pbcor_image_max)
+            self._update_miscinfo(result.image.replace('.image', '.image'+extension), max([len(field_ids.split(',')) for field_ids in self.image_heuristics.field(inputs.intent, inputs.field)]), pbcor_image_min, pbcor_image_max)
 
             result.set_image_min(pbcor_image_min)
             result.set_image_max(pbcor_image_max)
@@ -749,7 +749,7 @@ class Tclean(cleanbase.CleanBase):
                                                   pblimit_cleanmask=self.pblimit_cleanmask,
                                                   cont_freq_ranges=self.cont_freq_ranges)
 
-            self._update_miscinfo(result.image+extension, max([len(field_ids.split(',')) for field_ids in self.image_heuristics.field(inputs.intent, inputs.field)]), pbcor_image_min, pbcor_image_max)
+            self._update_miscinfo(result.image.replace('.image', '.image'+extension), max([len(field_ids.split(',')) for field_ids in self.image_heuristics.field(inputs.intent, inputs.field)]), pbcor_image_min, pbcor_image_max)
 
             keep_iterating, hm_masking = self.image_heuristics.keep_iterating(iteration, inputs.hm_masking,
                                                                               result.tclean_stopcode,
