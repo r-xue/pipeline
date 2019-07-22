cc7ae7d4c3e3bab5cb71f7a5aa5d82b377be5401
takeshi.nakazato <takeshi.nakazato@ac9c0da5-131f-0410-9009-a9c195f183e6>
2019-07-22 04:57:32 +0000

    Formatting
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project/pipeline@42742 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/hsd/tasks/k2jycal/qa.py
+++ pipeline/hsd/tasks/k2jycal/qa.py
@@ -9,6 +9,7 @@ from . import k2jycal
 
 LOG = logging.get_logger(__name__)
 
+
 class SDK2JyCalQAHandler(pqa.QAPlugin):
     result_cls = k2jycal.SDK2JyCalResults
     child_cls = None
@@ -19,7 +20,7 @@ class SDK2JyCalQAHandler(pqa.QAPlugin):
         shortmsg = "Missing Jy/K factors for some data" if is_missing_factor else "Jy/K factors are found for all data"
         longmsg = shortmsg + (" in "+result.vis if result.vis is not None else "") + (". Those data will remain in the unit of Kelvin after applying the calibration tables." if is_missing_factor else "")
         score = 0.0 if is_missing_factor else 1.0
-        scores = [ pqa.QAScore(score, longmsg=longmsg, shortmsg=shortmsg) ]
+        scores = [pqa.QAScore(score, longmsg=longmsg, shortmsg=shortmsg)]
 
         # PIPE-384 lower the score to 0.8 if DB access was failed
         if result.dbstatus is not None:
@@ -34,7 +35,7 @@ class SDK2JyCalQAHandler(pqa.QAPlugin):
             longmsg = shortmsg + " for " + (result.vis if result.vis is not None else "input vis")
             scores.append(pqa.QAScore(score, longmsg=longmsg, shortmsg=shortmsg))
         result.qa.pool.extend(scores)
-        #result.qa.pool[:] = scores
+
 
 class SDK2JyCalListQAHandler(pqa.QAPlugin):
     result_cls = collections.Iterable
