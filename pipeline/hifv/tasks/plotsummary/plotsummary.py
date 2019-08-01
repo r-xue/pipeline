cb3ce0bbe424bac0a583bc661cb42d3c90dcada4
stewart.williams <stewart.williams@ac9c0da5-131f-0410-9009-a9c195f183e6>
2019-08-01 13:47:42 +0000

    PIPE-409: hifv_plotsummary fails to render weblog
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project/pipeline@42772 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/hifv/tasks/plotsummary/plotsummary.py
+++ pipeline/hifv/tasks/plotsummary/plotsummary.py
@@ -1,10 +1,11 @@
 from __future__ import absolute_import
 
+import pipeline.h.tasks.applycal as h_applycal
 import pipeline.infrastructure as infrastructure
 import pipeline.infrastructure.basetask as basetask
+import pipeline.infrastructure.callibrary as callibrary
 import pipeline.infrastructure.vdp as vdp
 from pipeline.infrastructure import task_registry
-from ..finalcals import applycals
 
 LOG = infrastructure.get_logger(__name__)
 
@@ -15,49 +16,20 @@ class PlotSummaryInputs(vdp.StandardInputs):
         self.vis = vis
 
 
-class PlotSummaryResults(basetask.Results):
-    def __init__(self, final=None, pool=None, preceding=None):
-        if final is None:
-            final = []
-        if pool is None:
-            pool = []
-        if preceding is None:
-            preceding = []
-
-        super(PlotSummaryResults, self).__init__()
-
-        self.vis = None
-        self.pool = pool[:]
-        self.final = final[:]
-        self.preceding = preceding[:]
-        self.error = set()
-
-
 @task_registry.set_equivalent_casa_task('hifv_plotsummary')
 class PlotSummary(basetask.StandardTaskTemplate):
     Inputs = PlotSummaryInputs
 
     def prepare(self):
+        # get the applied calibration state from callibrary.active. This holds
+        # the CalApplications for everything applied by the pipeline in this run.
+        applied = self.inputs.context.callibrary.applied.merged()
 
-        result = self._do_plotsummary()
+        calapps = [callibrary.CalApplication(calto, calfroms) for calto, calfroms in applied.iteritems()]
+
+        result = h_applycal.ApplycalResults(applied=calapps)
 
         return result
 
     def analyse(self, results):
         return results
-
-    def _do_plotsummary(self):
-
-        applycal_inputs = applycals.ApplycalsInputs(self.inputs.context,
-                                                    vis=self.inputs.vis,
-                                                    field='',
-                                                    spw='',
-                                                    intent='',
-                                                    flagbackup=False,
-                                                    flagsum=False, flagdetailedsum=False)
-
-        applycal_task = applycals.Applycals(applycal_inputs)
-
-        result = applycal_task.execute(dry_run=True)
-
-        return result
