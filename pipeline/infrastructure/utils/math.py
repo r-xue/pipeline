9a9045a11a7b94e8f3ad0db6498fbba6caf2c360
dirk.muders <dirk.muders@ac9c0da5-131f-0410-9009-a9c195f183e6>
2019-10-29 13:53:07 +0000

    PIPE-466: Added precision parameter.
    git-svn-id: https://svn.cv.nrao.edu/svn/casa/branches/project/Pipeline-CASA6-Python3-B@43118 ac9c0da5-131f-0410-9009-a9c195f183e6

--- pipeline/infrastructure/utils/math.py
+++ pipeline/infrastructure/utils/math.py
@@ -4,5 +4,5 @@ import decimal
 
 __all__ = ['round_half_up']
 
-def round_half_up(x):
-    return float(decimal.Decimal(float(x)).to_integral_value(rounding=decimal.ROUND_HALF_UP))
+def round_half_up(value, prec=0):
+    return float(decimal.Decimal(float(value) * 10**prec).to_integral_value(rounding=decimal.ROUND_HALF_UP)) / 10**prec
