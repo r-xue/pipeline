74dab0e9a9edd1b8677580ad0e76c89ac17b355d
joe.masters <jmasters@nrao.edu>
2019-12-16 11:10:45 -0500

    Fixed minor typo "threhold" -> "threshold"

--- pipeline/hif/tasks/tclean/renderer.py
+++ pipeline/hif/tasks/tclean/renderer.py
@@ -224,8 +224,8 @@ class T2_4MDetailsTcleanRenderer(basetemplates.T2_4MDetailsDefaultRenderer):
             # cleaning threshold cell
             #
             if 'VLASS' in r.imaging_mode:
-                threhold_quantity = utils.get_casa_quantity(r.threshold)
-                row_cleaning_threshold = '%.2g %s' % (threhold_quantity['value'], threhold_quantity['unit'])
+                threshold_quantity = utils.get_casa_quantity(r.threshold)
+                row_cleaning_threshold = '%.2g %s' % (threshold_quantity['value'], threshold_quantity['unit'])
             elif 'VLA' in r.imaging_mode:
                 row_cleaning_threshold = '-'
             else:
