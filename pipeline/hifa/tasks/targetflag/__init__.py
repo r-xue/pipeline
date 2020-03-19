303c89c68650920f738edf7d460b3879f0b9af05
Dirk Muders <dmuders@mpifr-bonn.mpg.de>
2020-03-19 17:23:24 +0000

    PIPE-337: Initial QA score implementation.

--- pipeline/hifa/tasks/targetflag/__init__.py
+++ pipeline/hifa/tasks/targetflag/__init__.py
@@ -1,7 +1,7 @@
 import pipeline.infrastructure.renderer.qaadapter as qaadapter
 import pipeline.infrastructure.renderer.weblog as weblog
 
-#from . import qa
+from . import qa
 from . import renderer
 from . import targetflag
 from .targetflag import Targetflag
---
+++ pipeline/hifa/tasks/targetflag/qa.py
@@ -0,0 +1,42 @@
+import pipeline.infrastructure.basetask as basetask
+import pipeline.infrastructure.logging as logging
+import pipeline.infrastructure.pipelineqa as pqa
+import pipeline.infrastructure.utils as utils
+
+from . import targetflag
+
+LOG = logging.get_logger(__name__)
+
+
+class TargetflagQAHandler(pqa.QAPlugin):
+    """
+    QA handler for an uncontained TargetflagResults.
+    """
+    result_cls = targetflag.TargetflagResults
+    child_cls = None
+
+    def handle(self, context, result):
+        vis = result.inputs['vis']
+        ms = context.observing_run.get_ms(vis)
+
+        # Run correctedampflag QA on correctedampflag results.
+        for cafresult in result.cafresults.values():
+            pqa.qa_registry.do_qa(context, cafresult)
+
+            # Gather scores, store in result.
+            scores = cafresult.qa.pool
+            result.qa.pool.extend(scores)
+
+
+class TargetflagListQAHandler(pqa.QAPlugin):
+    """
+    QA handler for a list containing TargetflagResults.
+    """
+    result_cls = basetask.ResultsList
+    child_cls = targetflag.TargetflagResults
+
+    def handle(self, context, result):
+        # collate the QAScores from each child result, pulling them into our
+        # own QAscore list
+        collated = utils.flatten([r.qa.pool for r in result])
+        result.qa.pool[:] = collated
