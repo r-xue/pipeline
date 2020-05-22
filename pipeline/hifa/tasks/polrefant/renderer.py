8fb070dea0a78bc73e0ca3a28211e709f058f770
Vincent Geers <11389220+vcgeers@users.noreply.github.com>
2020-05-22 18:22:36 +0100

    PIPE-605: updated weblog for hifa_polrefant to include table of final choice of refant per session.

--- pipeline/hifa/tasks/polrefant/renderer.py
+++ pipeline/hifa/tasks/polrefant/renderer.py
@@ -11,8 +11,22 @@ LOG = logging.get_logger(__name__)
 class T2_4MDetailsPolRefAntRenderer(basetemplates.T2_4MDetailsDefaultRenderer):
     def __init__(self, uri='polrefant.mako', description='Select polarisation reference antennas',
                  always_rerender=False):
-        super(T2_4MDetailsPolRefAntRenderer, self).__init__(uri=uri, description=description,
-                                                            always_rerender=always_rerender)
+        super(T2_4MDetailsPolRefAntRenderer, self).__init__(
+            uri=uri, description=description, always_rerender=always_rerender)
 
-    def update_mako_context(self, mako_context, pipeline_context, result):
-        pass
+    def update_mako_context(self, mako_context, pipeline_context, results):
+        # Collect information about which MSes were evaluated, corresponding
+        # session names, and final chosen reference antenna, for display in
+        # table in task weblog page.
+        sessions = {}
+        for result in results:
+            for session_name in result.refant:
+                sessions[session_name] = {
+                    'vislist': result.refant[session_name]['vislist'],
+                    'refant': result.refant[session_name]['refant'],
+                }
+
+        # Update the mako context.
+        mako_context.update({
+            'sessions': sessions,
+        })
