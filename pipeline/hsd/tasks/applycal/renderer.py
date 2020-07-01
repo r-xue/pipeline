3906e721b4fe88a4b468285deb2d1ee51295fd4b
Stewart Williams <16945415+bravostuzero@users.noreply.github.com>
2020-07-01 10:12:09 +0100

    PIPE-600: bug fix for hsd_applycal weblog

--- pipeline/hsd/tasks/applycal/renderer.py
+++ pipeline/hsd/tasks/applycal/renderer.py
@@ -3,9 +3,16 @@ Created on 24 Oct 2014
 
 @author: sjw
 """
+import collections
+import functools
+import os.path
+
+import pipeline.domain.measures as measures
 import pipeline.infrastructure
 import pipeline.infrastructure.casatools as casatools
-from pipeline.h.tasks.applycal.renderer import *
+import pipeline.infrastructure.logging as logging
+import pipeline.infrastructure.utils as utils
+import pipeline.h.tasks.applycal.renderer as super_renderer
 from pipeline.h.tasks.common.displays import applycal as applycal
 
 LOG = logging.get_logger(__name__)
@@ -13,8 +20,7 @@ LOG = logging.get_logger(__name__)
 FlagTotal = collections.namedtuple('FlagSummary', 'flagged total')
 
 
-class T2_4MDetailsSDApplycalRenderer(T2_4MDetailsApplycalRenderer,
-                                     basetemplates.T2_4MDetailsDefaultRenderer):
+class T2_4MDetailsSDApplycalRenderer(super_renderer.T2_4MDetailsApplycalRenderer):
     def __init__(self, uri='applycal.mako', 
                  description='Apply calibrations from context',
                  always_rerender=False):
@@ -25,10 +31,22 @@ class T2_4MDetailsSDApplycalRenderer(T2_4MDetailsApplycalRenderer,
         weblog_dir = os.path.join(context.report_dir,
                                   'stage%s' % result.stage_number)
 
+        # Find out which intents to list in the flagging table
+        # First get all intents across all MSes in context
+        context_intents = functools.reduce(lambda x, m: x.union(m.intents), context.observing_run.measurement_sets, set())
+        # then match intents against those we want in the table, removing those not
+        # present. List order is preserved in the table.
+        # hsd_applycal only cares about target intent
+        all_flag_summary_intents = ['TARGET']
+        intents_to_summarise_flags = [i for i in all_flag_summary_intents
+                                      if i in context_intents.intersection(set(all_flag_summary_intents))]
+        flag_table_intents = ['TOTAL', 'SCIENCE SPWS']
+        flag_table_intents.extend(intents_to_summarise_flags)
+
         flag_totals = {}
         for r in result:
             if r.inputs['flagsum'] == True:
-                flag_totals = utils.dict_merge(flag_totals, self.flags_for_result(r, context))
+                flag_totals = utils.dict_merge(flag_totals, super_renderer.flags_for_result(r, context, intents_to_summarise_flags))
 
         calapps = {}
         for r in result:
@@ -49,13 +67,20 @@ class T2_4MDetailsSDApplycalRenderer(T2_4MDetailsApplycalRenderer,
         # return all agents so we get ticks and crosses against each one
         agents = ['before', 'applycal']
 
+        # PIPE-615: Add links to the hif_applycal weblog for viewing each
+        # callibrary table (and store all callibrary tables in the weblog
+        # directory)
+        callib_map = super_renderer.copy_callibrary(result, context.report_dir)
+
         ctx.update({
             'flags': flag_totals,
             'calapps': calapps,
             'caltypes': caltypes,
             'agents': agents,
             'dirname': weblog_dir,
-            'filesizes': filesizes
+            'filesizes': filesizes,
+            'callib_map': callib_map,
+            'flag_table_intents': flag_table_intents
         })
 
         # CAS-5970: add science target plots to the applycal page
@@ -92,7 +117,7 @@ class T2_4MDetailsSDApplycalRenderer(T2_4MDetailsApplycalRenderer,
             if len(representative_source) >= 1:
                 representative_source = representative_source.pop()
 
-            brightest_field = get_brightest_field(ms, representative_source)
+            brightest_field = super_renderer.get_brightest_field(ms, representative_source)
             plots = self.science_plots_for_result(context,
                                                   result,
                                                   applycal.RealVsFrequencySummaryChart,
@@ -115,11 +140,11 @@ class T2_4MDetailsSDApplycalRenderer(T2_4MDetailsApplycalRenderer,
                                                       result,
                                                       applycal.RealVsFrequencySummaryChart,
                                                       fields, None,
-                                                      ApplycalAmpVsFreqSciencePlotRenderer)
+                                                      super_renderer.ApplycalAmpVsFreqSciencePlotRenderer)
                 amp_vs_freq_detail_plots[vis] = plots
 
         for d, plotter_cls in (
-                (amp_vs_freq_detail_plots, ApplycalAmpVsFreqSciencePlotRenderer),):
+                (amp_vs_freq_detail_plots, super_renderer.ApplycalAmpVsFreqSciencePlotRenderer),):
             if d:
                 all_plots = list(utils.flatten([v for v in d.values()]))
                 renderer = plotter_cls(context, results, all_plots)
