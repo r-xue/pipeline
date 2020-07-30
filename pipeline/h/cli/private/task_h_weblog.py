7a2bbd96e008a9c84bda8c551a3d9c07dc79abae
Stewart Williams <swilliam@cvpost019.cv.nrao.edu>
2020-07-30 09:02:21 -0400

    Fix typo in h_weblog argument name

--- pipeline/h/cli/private/task_h_weblog.py
+++ pipeline/h/cli/private/task_h_weblog.py
@@ -3,5 +3,5 @@ from casatasks import casalog
 import pipeline
 
 
-def h_weblog(ipelinemode=None, relpath=None):
+def h_weblog(pipelinemode=None, relpath=None):
     pipeline.show_weblog(index_path=relpath)
