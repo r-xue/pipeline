f971f4e60b55afd4eaad7a6d78811301c685dfcb
joe.masters <jmasters@nrao.edu>
2020-10-06 15:38:08 -0400

    PIPE-864 changed '=' to '==' in first round_half_up() example

--- pipeline/infrastructure/utils/math.py
+++ pipeline/infrastructure/utils/math.py
@@ -12,7 +12,7 @@ def round_half_up(value: Union[int, str], precision: float = 0) -> float:
     In Python 2, round() was rounding to the nearest integer away from 0.
 
     For example,
-    [round(a) for a in [-2.5, -1.5, -0.5, 0.5, 1.5, 2.5]] = [-3.0, -2.0, -1.0, 1.0, 2.0, 3.0]
+    [round(a) for a in [-2.5, -1.5, -0.5, 0.5, 1.5, 2.5]] == [-3.0, -2.0, -1.0, 1.0, 2.0, 3.0]
 
     In Python 3, round() become "Banker's rounding", rounding to the nearest
     even integer, following the IEEE 754 standard for floating-point arithmetic.
