32f54870367d94d1df570fcbcd0da1b2e52916dd
joe.masters <jmasters@nrao.edu>
2020-10-06 15:28:00 -0400

    PIPE-864 edited docstring to start on 2nd line
    I also made the second (working) example doctest just to make
    it easy to automatically test that it's accurate now and with
    future changes.

--- pipeline/infrastructure/utils/math.py
+++ pipeline/infrastructure/utils/math.py
@@ -5,17 +5,21 @@ __all__ = ['round_half_up']
 
 
 def round_half_up(value: Union[int, str], precision: float = 0) -> float:
-    """Provide the Python2 rounding behavior
+    """
+    Provide the Python2 rounding behavior
 
     The behaviour of the "round" built-in changed from Python 2 to Python 3.
     In Python 2, round() was rounding to the nearest integer away from 0.
 
-    e.g. [round(a) for a in [-2.5, -1.5, -0.5, 0.5, 1.5, 2.5]] = [-3.0, -2.0, -1.0, 1.0, 2.0, 3.0]
+    For example,
+    [round(a) for a in [-2.5, -1.5, -0.5, 0.5, 1.5, 2.5]] = [-3.0, -2.0, -1.0, 1.0, 2.0, 3.0]
 
     In Python 3, round() become "Banker's rounding", rounding to the nearest
     even integer, following the IEEE 754 standard for floating-point arithmetic.
 
-    e.g. [round(a) for a in [-2.5, -1.5, -0.5, 0.5, 1.5, 2.5]] = [-2, -2, 0, 0, 2, 2]
+    For example,
+    >>> [round(a) for a in [-2.5, -1.5, -0.5, 0.5, 1.5, 2.5]]
+    [-2, -2, 0, 0, 2, 2]
 
     Args:
         value: Un-rounded value
