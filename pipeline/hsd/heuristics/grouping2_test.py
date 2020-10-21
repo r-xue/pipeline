3fe3b7da6c0e761f2a1e7a3e3aa4648c33a1ff89
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2020-10-21 15:28:23 +0900

    PIPE-922 define tests for GroupByPosition2

--- pipeline/hsd/heuristics/grouping2_test.py
+++ pipeline/hsd/heuristics/grouping2_test.py
@@ -1,7 +1,137 @@
 import pytest
 
+import functools
+import numpy as np
+import pipeline.infrastructure.casatools as casatools
+
 from .grouping2 import GroupByPosition2
 
+qa = casatools.quanta
 
-def test_simple():
-    assert True
+
+# TODO
+# test_GropuByTime2.calculate
+# test_ThresholdForGroupByTime_calculate
+# test_MergeGapTables2_calculate
+
+def random_noise(n, mean=0, amp=1, rs=None):
+    if rs is None:
+        r = np.random.rand(n)
+    else:
+        r = rs.rand(n)
+    return (r - (0.5 - mean)) * amp / 0.5
+
+
+def generate_position_data_psw():
+    xlist = [0, 1]
+    ylist = [0, 1]
+    rs = np.random.RandomState(seed=1234567)
+    noise_mean = 0
+    noise_amp = 0.02
+    num_pointings_per_grid = 10
+    ra_list = []
+    dec_list = []
+    for x in xlist:
+        for y in ylist:
+            xnoise = random_noise(num_pointings_per_grid, noise_mean, noise_amp, rs)
+            ynoise = random_noise(num_pointings_per_grid, noise_mean, noise_amp, rs)
+            ra_list.append(xnoise + x)
+            dec_list.append(ynoise + y)
+    ra_list = np.concatenate(ra_list)
+    dec_list = np.concatenate(dec_list)
+
+    return ra_list, dec_list
+
+
+def generate_position_data_raster():
+    xlist = np.arange(0, 1, 0.05)
+    xlist = [xlist, xlist[::-1]]
+    ylist = [0, 1]
+    rs = np.random.RandomState(seed=1234567)
+    noise_mean = 0
+    noise_amp = 0.00001
+    ra_list = []
+    dec_list = []
+    for x, y in zip(xlist, ylist):
+        ndata = len(x)
+        xnoise = random_noise(ndata, noise_mean, noise_amp, rs)
+        ynoise = random_noise(ndata, noise_mean, noise_amp, rs)
+        ra_list.append(xnoise + x)
+        dec_list.append(ynoise + y)
+    ra_list = np.concatenate(ra_list)
+    dec_list = np.concatenate(dec_list)
+
+    return ra_list, dec_list
+
+
+@pytest.fixture(name='data_psw')
+@functools.lru_cache(1)
+def fixture_data_psw():
+    return generate_position_data_psw()
+
+
+@pytest.fixture(name='data_raster')
+@functools.lru_cache(1)
+def fixture_data_raster():
+    return generate_position_data_raster()
+
+
+@functools.lru_cache(1)
+def expected_posdict_normal():
+    posdict = dict((i, [-1, (i // 10) * 10]) for i in range(40))
+    for i in range(0, 40, 10):
+        posdict[i] = list(range(i, i + 10))
+    return posdict
+
+
+@functools.lru_cache(1)
+def expected_posdict_one():
+    posdict = dict((i, [-1, 0]) for i in range(40))
+    posdict[0] = list(range(40))
+    return posdict
+
+
+@pytest.mark.parametrize(
+    "combine_radius, allowance_radius, expected_posdict, expected_posgap",
+    [
+        (0.4, 0.05, expected_posdict_normal(), [10, 20, 30]),
+        (qa.quantity(0.4, 'deg'), 0.05, expected_posdict_normal(), [10, 20, 30]),
+        (0.4, qa.quantity(0.05, 'deg'), expected_posdict_normal(), [10, 20, 30]),
+        (qa.quantity(0.4, 'deg'), qa.quantity(0.05, 'deg'), expected_posdict_normal(), [10, 20, 30]),
+        # unit conversion: rad -> deg
+        (qa.quantity(0.4 * np.pi / 180, 'rad'), 0.05, expected_posdict_normal(), [10, 20, 30]),
+    ]
+)
+def test_group_by_position_psw(combine_radius, allowance_radius, expected_posdict, expected_posgap, data_psw):
+    '''test grouping by position on position switch pattern'''
+    ra_list, dec_list = data_psw
+    h = GroupByPosition2()
+    posdict, posgap = h.calculate(ra_list, dec_list, combine_radius, allowance_radius)
+    assert posdict == expected_posdict
+    assert posgap == expected_posgap
+
+
+@pytest.mark.parametrize(
+    "combine_radius, allowance_radius, expected_posdict, expected_posgap",
+    [
+        (0.249, 0.01, expected_posdict_normal(), [20]),
+        (qa.quantity(0.249, 'deg'), 0.01, expected_posdict_normal(), [20]),
+        (0.249, qa.quantity(0.01, 'deg'), expected_posdict_normal(), [20]),
+        (qa.quantity(0.249, 'deg'), qa.quantity(0.01, 'deg'), expected_posdict_normal(), [20]),
+        # too large allowance radius -> all gaps are detected
+        (0.249, 10, expected_posdict_normal(), list(range(1, 40))),
+        # moderate allowance radius -> no gap is detected
+        (0.249, 1, expected_posdict_normal(), []),
+        # too large combine radius -> only one group
+        (10, 0.01, expected_posdict_one(), [20]),
+        # too small combine radius -> all data are separated
+        (1e-5, 0.01, dict((i, [i]) for i in range(40)), [20]),
+    ]
+)
+def test_group_by_position_raster(combine_radius, allowance_radius, expected_posdict, expected_posgap, data_raster):
+    '''test grouping by position on raster pattern including some edge cases'''
+    ra_list, dec_list = data_raster
+    h = GroupByPosition2()
+    posdict, posgap = h.calculate(ra_list, dec_list, combine_radius, allowance_radius)
+    assert posdict == expected_posdict
+    assert posgap == expected_posgap
