3844b85f44407168ad8edd106dc694a8fe03c46c
Brian Kent <bkent@nrao.edu>
2020-12-01 16:24:00 -0500

    PIPE-720:  Code cleanup.  Removed tclean convenience command, and non-used keyword inputs.

--- pipeline/hifv/tasks/vlassmasking/vlassmasking.py
+++ pipeline/hifv/tasks/vlassmasking/vlassmasking.py
@@ -43,17 +43,14 @@ class VlassmaskingResults(basetask.Results):
 
 
 class VlassmaskingInputs(vdp.StandardInputs):
-    phasecenter = vdp.VisDependentProperty(default='')
     vlass_ql_database = vdp.VisDependentProperty(default='/home/vlass/packages/VLASS1Q.fits')
     maskingmode = vdp.VisDependentProperty(default='vlass-se-tier-1')
     catalog_search_size = vdp.VisDependentProperty(default=1.5)
-    mask_shape = vdp.VisDependentProperty(default=[1024, 1024,    1,    1])
 
-    def __init__(self, context, vis=None, phasecenter=None, vlass_ql_database=None, maskingmode=None,
+    def __init__(self, context, vis=None, vlass_ql_database=None, maskingmode=None,
                  catalog_search_size=None):
         self.context = context
         self.vis = vis
-        self.phasecenter = phasecenter
         self.vlass_ql_database = vlass_ql_database
         self.maskingmode = maskingmode
         self.catalog_search_size = catalog_search_size
@@ -98,11 +95,7 @@ class Vlassmasking(basetask.StandardTaskTemplate):
             if not os.path.exists(self.inputs.vlass_ql_database) and self.inputs.maskingmode == 'vlass-se-tier-1':
                 LOG.error("VLASS Quicklook database file {!s} does not exist.".format(self.inputs.vlass_ql_database))
 
-            # This command here is just for the sake of creating an image - no longer needed
-            # tclean_result = self.run_tclean('iter0', cfcache=cfcache_nowb, robust=-2.0, uvtaper='3arcsec',
-            #                                 calcres=False, parallel=False, wbawp=False)
-
-            LOG.debug("Executing mask_from_catalog")
+            LOG.debug("Executing mask_from_catalog masking mode = {!s}".format(self.inputs.maskingmode))
 
             outext = QLmask
             catalog_fits_file = self.inputs.vlass_ql_database
@@ -116,8 +109,7 @@ class Vlassmasking(basetask.StandardTaskTemplate):
             combinedmask = imagename_base + outext
 
         elif self.inputs.maskingmode == 'vlass-se-tier-2':
-            # hifv_vlassmasking(known_mask='tier-1', uniform_image='image_name', maskingmode='vlass-se-tier-2')
-            LOG.debug("Executing mask_from_catalog")
+            LOG.debug("Executing mask_from_catalog masking mode = {!s}".format(self.inputs.maskingmode))
 
             outext = "secondmask.mask"
             catalog_fits_file = imagename_base + 'iter1b.image.smooth5.cat.edited.fits'
@@ -150,42 +142,3 @@ class Vlassmasking(basetask.StandardTaskTemplate):
 
     def analyse(self, results):
         return results
-
-    def run_tclean(self, image_iter, datacolumn='data', cfcache='', scales=[0], robust=1.0, uvtaper='', niter=0,
-                   gain=0.1, nsigma=2.0, \
-                   cycleniter=5000, cyclefactor=3, mask='', savemodel="none", calcres=True, calcpsf=True,
-                   parallel=True, wbawp=True):
-        """
-        This class method is simply here for convenience and reference to run tclean and obtain *iter0.psf.tt0 images
-        """
-
-        field = ''
-        spw = ''
-        antenna = ''
-        scan = ''
-
-        imagename_base = 'VIP_'
-        imsize = 1024
-        cell = '2.0arcsec'
-        reffreq = '3.0GHz'
-        uvrange = '<12km'
-        intent = 'OBSERVE_TARGET#UNSPECIFIED'
-        wprojplanes = 32
-        usepointing = True
-
-        if mask:
-            mask = imagename_base + mask
-        task = casa_tasks.tclean(vis=self.inputs.vis, field=field, spw=spw, uvrange=uvrange, datacolumn=datacolumn,
-                                 imagename=imagename_base + image_iter, imsize=imsize,
-                                 antenna=antenna, scan=scan, intent=intent, pointingoffsetsigdev=[300, 30],
-                                 cell=cell, phasecenter=self.inputs.phasecenter, reffreq=reffreq, gridder="awproject",
-                                 wprojplanes=wprojplanes, cfcache=cfcache, conjbeams=True,
-                                 usepointing=usepointing, rotatepastep=5.0, pblimit=0.02, deconvolver="mtmfs",
-                                 scales=scales, nterms=2, smallscalebias=0.4,
-                                 weighting="briggs", robust=robust, uvtaper=uvtaper, niter=niter, gain=gain,
-                                 threshold=0.0, nsigma=nsigma, cycleniter=cycleniter,
-                                 cyclefactor=cyclefactor, usemask="user", mask=mask, restart=True, savemodel=savemodel,
-                                 calcres=calcres, calcpsf=calcpsf, parallel=parallel, wbawp=wbawp)
-
-        return self._executor.execute(task)
-
