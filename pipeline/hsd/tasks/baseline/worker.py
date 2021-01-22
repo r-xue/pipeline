7e3fe235c8c6c90235ebe58a1aaad705b3834f91
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2021-01-22 16:46:32 +0900

    PIPE-979 remove OnDemandStringParseLogger

--- pipeline/hsd/tasks/baseline/worker.py
+++ pipeline/hsd/tasks/baseline/worker.py
@@ -14,8 +14,7 @@ from pipeline.infrastructure import casa_tools
 from . import plotter
 from .. import common
 
-_LOG = infrastructure.get_logger(__name__)
-LOG = sdutils.OnDemandStringParseLogger(_LOG)
+LOG = infrastructure.get_logger(__name__)
 
 
 class BaselineSubtractionInputsBase(vdp.StandardInputs):
@@ -171,17 +170,17 @@ class BaselineSubtractionWorker(basetask.StandardTaskTemplate):
         field_id_list = self.inputs.field
         antenna_id_list = self.inputs.antenna
         spw_id_list = self.inputs.spw
-        LOG.debug('subgroup member for {vis}:\n\tfield: {field}\n\tantenna: {antenna}\n\tspw: {spw}',
-                  vis=ms.basename,
-                  field=field_id_list,
-                  antenna=antenna_id_list,
-                  spw=spw_id_list)
+        LOG.debug('subgroup member for %s:\n\tfield: %s\n\tantenna: %s\n\tspw: %s',
+                  ms.basename,
+                  field_id_list,
+                  antenna_id_list,
+                  spw_id_list)
 
         # initialization of blparam file
         # blparam file needs to be removed before starting iteration through
         # reduction group
         if os.path.exists(blparam):
-            LOG.debug('Cleaning up blparam file for {vis}', vis=vis)
+            LOG.debug('Cleaning up blparam file for %s', vis)
             os.remove(blparam)
 
         #datatable = DataTable(context.observing_run.ms_datatable_name)
@@ -226,7 +225,7 @@ class BaselineSubtractionWorker(basetask.StandardTaskTemplate):
         plot_list = []
         for (field_id, antenna_id, spw_id, grid_table, channelmap_range) in accum.iterate_all():
 
-            LOG.info('field {0} antenna {1} spw {2}', field_id, antenna_id, spw_id)
+            LOG.info('field %s antenna %s spw %s', field_id, antenna_id, spw_id)
             if (field_id, antenna_id, spw_id) in deviationmask_list:
                 deviationmask = deviationmask_list[(field_id, antenna_id, spw_id)]
             else:
