517c9ccff734e8f466285cf9f9e8a87a5d854d40
Dirk Muders <dmuders@mpifr-bonn.mpg.de>
2021-01-29 07:16:50 +0000

    PIPE-958/PIPE-419/PIPE-843: Corrected path to test datasets.

--- pipeline/infrastructure/contfilehandler_test.py
+++ pipeline/infrastructure/contfilehandler_test.py
@@ -7,13 +7,13 @@ from . import contfilehandler
 
 to_topo_test_params = (
     ('214.5.0~215.5GHz;215.6~216.1GHz LSRK',
-     [casa_tools.utils.resolve('uid___A002_Xc46ab2_X15ae_repSPW_spw16_17_small.ms')],
+     [casa_tools.utils.resolve('pl-unittest/uid___A002_Xc46ab2_X15ae_repSPW_spw16_17_small.ms')],
      ['3'], '0', Mock(spec=domain.ObservingRun, **{'virtual2real_spw_id.return_value': 0, 'get_ms.return_value': 'uid___A002_Xc46ab2_X15ae_repSPW_spw16_17_small.ms'}),
      (['214.4948696490~215.5104946490GHz;215.6042446490~216.1198696490GHz TOPO'],
       ['55~119;16~48'],
       {'unit': 'GHz', 'value': 1.5})),
     ('230.4~230.7GHz;231.5~231.6GHz;232.3~232.4GHz SOURCE',
-     [casa_tools.utils.resolve('uid___A002_Xcfc232_X2eda_test.ms')],
+     [casa_tools.utils.resolve('pl-unittest/uid___A002_Xcfc232_X2eda_test.ms')],
      ['3'], '18', Mock(spec=domain.ObservingRun, **{'virtual2real_spw_id.return_value': 18, 'get_ms.return_value': 'uid___A002_Xcfc232_X2eda_test.ms'}),
      (['230.4733757345~230.7038444845GHz;231.5026726095~231.6042351095GHz;232.3024772970~232.4040397970GHz TOPO'],
       ['0~235;1054~1157;1873~1976'],
