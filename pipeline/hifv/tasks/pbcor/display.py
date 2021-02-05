50eb88a678f0294a78ce593c0aacb4a14db59571
Rui Xue <rxue@nrao.edu>
2021-02-05 16:26:01 -0500

    PIPE-1009: add the "ImageHistDisplay" class for image histogram

--- pipeline/hifv/tasks/pbcor/display.py
+++ pipeline/hifv/tasks/pbcor/display.py
@@ -1,9 +1,13 @@
 import collections
 import os
 
+import matplotlib.pyplot as plt
+
 import pipeline.infrastructure as infrastructure
+import pipeline.infrastructure.renderer.logger as logger
 from pipeline.h.tasks.common.displays import sky as sky
 from pipeline.infrastructure import casa_tools
+
 
 LOG = infrastructure.get_logger(__name__)
 
@@ -27,21 +31,67 @@ class PbcorimagesSummary(object):
         plot_wrappers = []
 
         for pbcorimagename in self.result.pbcorimagenames:
+            plot_wrappers.append(sky.SkyDisplay().plot(self.context, pbcorimagename,
+                                                       reportdir=stage_dir, intent='',
+                                                       collapseFunction='mean'))
             if 'residual.pbcor' in pbcorimagename:
-                plot_wrappers.append(sky.SkyDisplay().plot(self.context, pbcorimagename,
-                                                           reportdir=stage_dir, intent='',
-                                                           collapseFunction='mean'))
                 with casa_tools.ImageReader(pbcorimagename) as image:
                     self.result.residual_stats = image.statistics(robust=True)
             elif 'image.pbcor' in pbcorimagename:
-                plot_wrappers.append(sky.SkyDisplay().plot(self.context, pbcorimagename,
-                                                           reportdir=stage_dir, intent='',
-                                                           collapseFunction='mean'))
                 with casa_tools.ImageReader(pbcorimagename) as image:
                     self.result.pbcor_stats = image.statistics(robust=True)
             else:
-                plot_wrappers.append(sky.SkyDisplay().plot(self.context, pbcorimagename,
-                                                           reportdir=stage_dir, intent='',
-                                                           collapseFunction='mean'))
+                plot_wrappers.append(ImageHistDisplay(self.context, pbcorimagename, reportdir=stage_dir).plot())
 
         return [p for p in plot_wrappers if p is not None]
+
+
+class ImageHistDisplay(object):
+    """
+    A display class to generate histogram of a CASA image
+    # to do: 
+    #   make region / chanel selections work
+    #   plot stats-ROI-boundary back in the "sky" plot
+    """
+
+    def __init__(self, context, imagename, reportdir='./', region='', box='-1,-1', chans=''):
+        self.context = context
+        self.imagename = imagename
+        self.reportdir = reportdir
+        self.region = region
+        self.box = box
+        self.chans = chans
+        self.figfile = self._get_figfile()
+
+    def plot(self):
+        if os.path.exists(self.figfile):
+            LOG.debug('Returning existing image histogram plot')
+            return self._get_plot_object()
+
+        LOG.debug('Creating new image histogram plot')
+        try:
+            with casa_tools.ImageReader(self.imagename) as myia:
+                im_val = myia.getchunk()
+            fig, ax = plt.subplots()
+            ax.hist(im_val.ravel(), bins=20,
+                    histtype='barstacked', align='mid', label='')
+            ax.set_xlabel('Primary Beam Reponse')
+            ax.set_ylabel('Num. of Pixel')
+            ax.set_title('PB histogram')
+            LOG.debug('Saving new image histogram plot to {}'.format(self.figfile))
+            fig.savefig(self.figfile)
+        except:
+            return None
+
+        return self._get_plot_object()
+
+    def _get_figfile(self):
+
+        return os.path.join(self.reportdir,
+                            self.imagename+'.hist.png')
+
+    def _get_plot_object(self):
+        return logger.Plot(self.figfile,
+                           x_axis='Pixel Value',
+                           y_axis='Histogram',
+                           parameters={'placeholder': 'placeholder'})
