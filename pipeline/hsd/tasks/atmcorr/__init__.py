7a711f5b7d231a3112cd1a73705cde5e867594d0
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2021-04-23 18:53:43 +0900

    add default renderer to hsd_correctatm

--- pipeline/hsd/tasks/atmcorr/__init__.py
+++ pipeline/hsd/tasks/atmcorr/__init__.py
@@ -1,1 +1,9 @@
+import pipeline.infrastructure.renderer.basetemplates as basetemplates
+import pipeline.infrastructure.renderer.weblog as weblog
+
 from .atmcorr import SerialSDATMCorrection
+
+SDATMCorrection = SerialSDATMCorrection
+
+weblog.add_renderer(SDATMCorrection, basetemplates.T2_4MDetailsDefaultRenderer(description='Restore Calibrated Data'),
+                    group_by=weblog.UNGROUPED)
