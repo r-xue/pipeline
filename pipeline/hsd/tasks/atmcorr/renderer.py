f39464f90ff9ca0808bb6c2e4647c172162dacff
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2021-04-30 13:41:26 +0900

    add summary plots and detail subpage

--- pipeline/hsd/tasks/atmcorr/renderer.py
+++ pipeline/hsd/tasks/atmcorr/renderer.py
@@ -1,6 +1,10 @@
+import itertools
+import os
+
+import pipeline.infrastructure.filenamer as filenamer
 import pipeline.infrastructure.logging as logging
 import pipeline.infrastructure.renderer.basetemplates as basetemplates
-import pipeline.infrastructure.utils as utils
+from .display import PlotmsRealVsFreqPlotter
 
 LOG = logging.get_logger(__name__)
 
@@ -15,3 +19,64 @@ class T2_4MDetailsSingleDishATMCorRenderer(basetemplates.T2_4MDetailsDefaultRend
         super().update_mako_context(mako_context, pipeline_context, result)
 
         LOG.info('This is ATMCorRenderer')
+
+        stage_dir = os.path.join(
+            pipeline_context.report_dir,
+            'stage{}'.format(result.stage_number)
+        )
+        if not os.path.exists(stage_dir):
+            os.mkdir(stage_dir)
+
+        summary_plots = {}
+        detail_plots = []
+        for r in result:
+            LOG.info('result for "%s"', r.inputs['vis'])
+            if not hasattr(r, 'atmcorr_ms_name'):
+                raise RuntimeError('Wrong result object is given.')
+
+            vis = r.inputs['vis']
+            atmvis = r.atmcorr_ms_name
+            ms = pipeline_context.observing_run.get_ms(os.path.basename(vis))
+            antennas = [a.name for a in ms.get_antenna()]
+            field_names = [f.clean_name for f in ms.get_fields(intent='TARGET')]
+            science_spws = [s.id for s in ms.get_spectral_windows(science_windows_only=True)]
+            spw_selection = r.inputs['spw']
+            if len(spw_selection) > 0:
+                selected_spws = set(map(int, spw_selection.split(','))).intersection(science_spws)
+            else:
+                selected_spws = science_spws
+
+            plotter = PlotmsRealVsFreqPlotter(
+                vis=ms.basename, atmvis=atmvis,
+                atmtype=r.inputs['atmtype'], output_dir=stage_dir
+            )
+            summaries = {}
+            for field_name, spw_id in itertools.product(field_names, selected_spws):
+                LOG.info(f'field {field_name} spw {spw_id}')
+                spw = str(spw_id)
+                summaries.setdefault(field_name, {})
+                plotter.set_field(field_name)
+                plotter.set_spw(spw)
+                p = plotter.plot()
+                summaries[field_name][spw] = p
+                for antenna_name in antennas:
+                    plotter.set_antenna(antenna_name)
+                    p = plotter.plot()
+                    detail_plots.append(p)
+            LOG.info('spw list %s', list(summaries.keys()))
+            summary_plots[os.path.basename(vis)] = summaries
+
+        detail_page_title = 'ATM corrected amplitude vs frequency'
+        renderer = basetemplates.JsonPlotRenderer('generic_x_vs_y_field_spw_ant_detail_plots.mako',
+                                                  pipeline_context,
+                                                  result,
+                                                  detail_plots,
+                                                  detail_page_title,
+                                                  filenamer.sanitize('%s.html' % (detail_page_title.lower())))
+        with renderer.get_file() as fileobj:
+            fileobj.write(renderer.render())
+
+        mako_context.update({
+            'summary_plots': summary_plots,
+            'detail_page': renderer.path,
+        })
--- pipeline/hsd/templates/hsd_atmcor.mako
+++ pipeline/hsd/templates/hsd_atmcor.mako
@@ -9,6 +9,28 @@ from pipeline.infrastructure.renderer import rendererutils
 
 <%block name="title">Apply correction for atmospheric effects</%block>
 
+<%
+def plot_title(plot):
+    spw = plot.parameters['spw']
+    field = plot.parameters['field']
+    vis = plot.parameters['vis']
+    return f'Spectra after ATM correction {vis} {field} Spw {spw}'
+
+def get_spw_exp(spw):
+    spw_exp = 'Spectral Window {}'.format(spw)
+    #if dovirtual:
+    #    spw_exp = 'Virtual ' + spw_exp
+    return spw_exp
+
+def get_spw_desc(spw):
+    spw_exp = get_spw_exp(spw).replace('Window', 'Window:')
+    #if dovirtual:
+    #    spw_name = pcontext.observing_run.virtual_science_spw_ids[spw]
+    #    spw_short_name = pcontext.observing_run.virtual_science_spw_shortnames[spw_name]
+    #    spw_exp += '<br>({})'.format(spw_short_name)
+    return spw_exp
+%>
+
 <p>This task applies the correction for atmospheric effects to
 science targets in the calibrated measurement sets.</p>
 
@@ -63,3 +85,50 @@ on each plot in pink.</p>
 
 <p>Data are plotted for all antennas and correlations, with different antennas shown
 in different colors.</p>
+
+% for vis, plots_vis in summary_plots.items():
+    <h3><a class="replace"
+           href="${os.path.relpath(detail_page, pcontext.report_dir)}"
+           data-vis="${vis}">${vis}</a>
+    </h3>
+    % for field, plots_fields in plots_vis.items():
+        <% print(list(plots_fields.keys())) %>
+        <h3><a class="replace"
+               href="${os.path.relpath(detail_page, pcontext.report_dir)}"
+               data-vis="${vis}"
+               data-field="${field}">${field}</a>
+        </h3>
+        % for spw, plot in plots_fields.items():
+            % if os.path.exists(plot.thumbnail):
+	        <div class="col-md-3">
+	            <div class="thumbnail">
+	                <a href="${os.path.relpath(plot.abspath, pcontext.report_dir)}"
+	                   data-fancybox="thumbs">
+	                    <img class="lazyload"
+                             data-src="${os.path.relpath(plot.thumbnail, pcontext.report_dir)}"
+	                         title="${plot_title(plot)}">
+	                </a>
+
+	                <div class="caption">
+	                    <h4>
+	                        <a href="${os.path.relpath(detail_page, pcontext.report_dir)}"
+	                           class="replace"
+                               data-vis="${vis}"
+	                           data-field="${field}"
+	                           data-spw="${spw}">
+	                           ${get_spw_exp(spw)}
+	                        </a>
+	                    </h4>
+	                    <p>Field: ${field}<br>
+	                       ${get_spw_desc(spw)}
+	                    </p>
+	                </div>
+	            </div>
+	        </div>
+            % endif
+        % endfor
+	<div class="clearfix"></div><!--  flush plots, break to next row -->
+    % endfor
+% endfor
+
+
