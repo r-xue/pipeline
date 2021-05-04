affa7b3cdd09cb772aacb0b78c191184648b6436
Vincent Geers <11389220+vcgeers@users.noreply.github.com>
2021-05-04 17:14:03 +0100

    PIPE-1132: refactor phasespwmap to use spectralspec attribute of spectral window domain objects.

--- pipeline/hifa/heuristics/phasespwmap.py
+++ pipeline/hifa/heuristics/phasespwmap.py
@@ -43,16 +43,6 @@ def combine_spwmap(scispws):
         return combinespwmap
 
 
-def get_spectral_spec(spw):
-    """Extract spectral spec from spectral window name."""
-    spectralspec = ''
-    if spw.name and 'ALMA' in spw.name:
-        i = spw.name.find('#')
-        if i != -1:
-            spectralspec = spw.name[:i]
-    return spectralspec
-
-
 def snr_n2wspwmap(allspws, scispws, snrs, goodsnrs):
     # Heuristics for computing an spwmap which uses SNR information.
     # Here an spw is the spw object stored in the domain object.
@@ -94,9 +84,6 @@ def snr_n2wspwmap(allspws, scispws, snrs, goodsnrs):
             LOG.debug('No good SNR spw in receiver band so match spw id %s to itself' % scispw.id)
             continue
 
-        # Retrieve the SpectralSpec for the current science spw.
-        scispwspec = get_spectral_spec(scispw)
-
         # Loop through the other science windows looking for a match
         bestspw = None
         bestsnr = None
@@ -114,8 +101,7 @@ def snr_n2wspwmap(allspws, scispws, snrs, goodsnrs):
                 continue
 
             # Don't match across SpectralSpec if a non-empty SpectralSpec is available (PIPE-316).
-            matchspec = get_spectral_spec(matchspw)
-            if scispwspec and scispwspec != matchspec:
+            if scispw.spectralspec and scispw.spectralspec != matchspw.spectralspec:
                 LOG.debug('Skipping bad spectral spec match to spw id %s' % matchspw.id)
                 continue
 
