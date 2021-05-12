minor fix: avoid  '+/- 1.0 rms' text colliding each other in the 'Masked-averaged spectrum' plot

pipeline/hsd/tasks/imaging/detectcontamination.py
@@ -159,7 +159,7 @@ def make_figures(peak_sn, mask_map, rms_threshold, rms_map,
         plt.plot([abc[0], abc[-1]], [np.nanmean(rms_map) * peak_sn_threshold, np.nanmean(rms_map) * peak_sn_threshold], "--", color="green")
         plt.text(minabc + w * 0.5, np.nanmean(rms_map) * peak_sn_threshold, "lower 10% level", fontsize=18, color="green")
     plt.text(minabc + w * 0.1, np.nanmean(rms_map) * 1., "1.0 x rms", fontsize=18, color="blue")
-    plt.text(minabc + w * 0.1, np.nanmean(rms_map) * (-1.), "-1.0 x rms", fontsize=18, color="blue")
+    plt.text(minabc + w * 0.1, np.nanmean(rms_map) * (-1.), "-1.0 x rms", fontsize=18, color="blue", va='top')
     plt.text(minabc + w * 0.6, -4. * std_value, "-4.0 x std", fontsize=18, color="red")
     plt.legend()
     if np.nanmin(masked_average_spectrum) <= (-1) * std_value * std_threshold: