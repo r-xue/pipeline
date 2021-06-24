1db8fff440bd4eda6dd49a1ffcf2d8184b5f5e6f
Dirk Muders <dmuders@mpifr-bonn.mpg.de>
2021-06-24 13:09:48 +0000

    PIPE-1160: Changed score calculation algorithm to John Hibbard's proposal given in the ticket.

--- pipeline/hifa/tasks/renorm/qa.py
+++ pipeline/hifa/tasks/renorm/qa.py
@@ -18,20 +18,47 @@ class RenormQAHandler(pqa.QAPlugin):
     child_cls = None
 
     def handle(self, context, result):
+        threshold = result.threshold 
+        threshold_factor = 1.0 + threshold 
         for source in result.stats: 
-            for spw in result.stats[source]: 
-                if result.stats[source][spw]['max_rn'] < 1.0 + result.threshold:
-                    score = 1.0
-                    shortmsg = 'No renormalization'
-                    longmsg = 'No renormalization necessary for EB {} source {} spw {}.'.format( \
-                              os.path.basename(result.vis), source, spw)
+            for spw in result.stats[source]:
+                max_factor = result.stats[source][spw]['max_rn']
+                if False:
+                    # TODO: Handle failure cases here
+                    pass
                 else:
-                    score = 0.5
-                    shortmsg = 'Renormalization applied'
-                    longmsg = 'Renormalization applied for EB {} source {} spw {}. ' \
-                              'Maximum factor {:.3f} for field {}.'.format(os.path.basename(result.vis), \
-                              source, spw, result.stats[source][spw]['max_rn'], \
-                              result.stats[source][spw]['max_rn_field'])
+                    if 0.0 <= max_factor < 2.0-threshold_factor:
+                        if result.apply:
+                            score = 0.9
+                            shortmsg = 'Renormalization applied'
+                            longmsg = 'EB {} source {} spw {}: maximum renormalization factor of {:.3f} ' \
+                                      'is outside threshold of {:.1%} so corrections were applied to data'.format( \
+                                      os.path.basename(result.vis), source, spw, max_factor, threshold)
+                        else:
+                            score = 0.34+max_factor*(0.66-0.34)/(2.0-threshold_factor)
+                            shortmsg = 'Renormalization factor outside threshold'
+                            longmsg = 'EB {} source {} spw {}: maximum renormalization factor of {:.3f} ' \
+                                      'is outside threshold of {:.1%}'.format( \
+                                      os.path.basename(result.vis), source, spw, max_factor, threshold)
+                    elif 2.0-threshold_factor <= max_factor <= threshold_factor:
+                        score = 1.0
+                        shortmsg = 'Renormalization factor within threshold'
+                        longmsg = 'EB {} source {} spw {}: maximum renormalization factor of {:.3f} ' \
+                                  'is within threshold of {:.1%}'.format( \
+                                  os.path.basename(result.vis), source, spw, max_factor, threshold)
+                    elif max_factor > threshold_factor:
+                        if result.apply:
+                            score = 0.9
+                            shortmsg = 'Renormalization applied'
+                            longmsg = 'EB {} source {} spw {}: maximum renormalization factor of {:.3f} ' \
+                                      'is outside threshold of {:.1%} so corrections were applied to data'.format( \
+                                      os.path.basename(result.vis), source, spw, max_factor, threshold)
+                        else:
+                            score = max(0.66+threshold_factor-max_factor, 0.34)
+                            shortmsg = 'Renormalization factor outside threshold'
+                            longmsg = 'EB {} source {} spw {}: maximum renormalization factor of {:.3f} ' \
+                                      'is outside threshold of {:.1%}'.format( \
+                                      os.path.basename(result.vis), source, spw, max_factor, threshold)
 
                 origin = pqa.QAOrigin(metric_name='MaxRenormFactor',
                                       metric_score=result.stats[source][spw]['max_rn'],
