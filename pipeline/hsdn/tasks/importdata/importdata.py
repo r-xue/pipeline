0b2ce6dffafb9214abd2cccc9cccffed50523e39
kazuhiko.shimada <kazuhiko.shimada@nao.ac.jp>
2021-10-11 11:26:18 +0900

    PIPE-1275: add docstrings and annotations

--- pipeline/hsdn/tasks/importdata/importdata.py
+++ pipeline/hsdn/tasks/importdata/importdata.py
@@ -1,22 +1,44 @@
 """
+Importdata tasks for observation data of Nobeyama.
+
 Created on Dec 4, 2017
 
 @author: kana
 """
 import os
+from typing import Any, Dict, List
 
 import pipeline.h.tasks.importdata.importdata as importdata
 import pipeline.hsd.tasks.importdata.importdata as sd_importdata
 import pipeline.infrastructure as infrastructure
-from pipeline.infrastructure import casa_tools
-from pipeline.infrastructure import task_registry
+from pipeline.domain.fluxmeasurement import FluxMeasurement
+from pipeline.domain.measurementset import MeasurementSet
+from pipeline.domain.singledish import MSReductionGroupDesc
+from pipeline.infrastructure import casa_tools, task_registry
+from pipeline.infrastructure.launcher import Context
 
 LOG = infrastructure.get_logger(__name__)
 
 
 class NROImportDataInputs(importdata.ImportDataInputs):
-    def __init__(self, context=None, vis=None, output_dir=None, session=None,
-                 overwrite=None, nocopy=None, createmms=None):
+    """Class of inputs of NROImportData.
+
+    This class extends importdata.ImportDataInputs.
+    """
+
+    def __init__(self, context: Context=None, vis: str=None, output_dir: str=None, session: str=None,
+                 overwrite: bool=None, nocopy: bool=None, createmms: str=None):
+        """Initialise NROImportDataInputs class.
+
+        Args:
+            context: pipeline context
+            vis: List of input visibility data
+            output_dir: path of output directory
+            session: List of visibility data sessions
+            overwrite: Overwrite existing files on import
+            nocopy: Disable copying of MS to working directory
+            createmms: Create an MMS
+        """
         #no-op parameters for MS
         asis=''
         process_caldevice=False
@@ -33,8 +55,8 @@ class NROImportDataInputs(importdata.ImportDataInputs):
 
 
 class NROImportDataResults(sd_importdata.SDImportDataResults):
-    """
-    NROImportDataResults is an equivalent class with ImportDataResults. 
+    """NROImportDataResults is an equivalent class with ImportDataResults.
+    
     Purpose of NROImportDataResults is to replace QA scoring associated 
     with ImportDataResults with single dish specific QA scoring, which 
     is associated with this class.
@@ -44,11 +66,25 @@ class NROImportDataResults(sd_importdata.SDImportDataResults):
     SetJy results generated from flux entries in Source.xml.
     """
 
-    def __init__(self, mses=None, reduction_group_list=None, datatable_prefix=None, setjy_results=None):
+    def __init__(self, mses: List[MeasurementSet]=None, reduction_group_list: List[MSReductionGroupDesc]=None, 
+                 datatable_prefix: str=None, setjy_results: List[FluxMeasurement]=None):
+        """Initialise NROImportDataResults class.
+        
+        Args:
+            mses: list of MeasurementSet domain objects
+            reduction_group_list: list of MSReductionGroupDesc
+            datatable_prefix: table name prefix of MeasurementSet
+            setjy_results: the flux results generated from Source.xml
+        """
         super(NROImportDataResults, self).__init__(mses=mses, reduction_group_list=reduction_group_list,
                                                    datatable_prefix=datatable_prefix, setjy_results=setjy_results)
 
-    def merge_with_context(self, context):
+    def merge_with_context(self, context: Context):
+        """Override method of basetask.Results.merge_with_context.
+
+        Args:
+            context: pipeline context
+        """
         super(NROImportDataResults, self).merge_with_context(context)
         # Set observatory information
         for ms in self.mses:
@@ -61,9 +97,21 @@ class NROImportDataResults(sd_importdata.SDImportDataResults):
 @task_registry.set_equivalent_casa_task('hsdn_importdata')
 @task_registry.set_casa_commands_comment('Import Nobeyama MeasurementSets.')
 class NROImportData(sd_importdata.SDImportData):
+    """NRO Data import execution task.
+
+    This class extends importdata.ImportData class, and methods execute main logics depends on it.
+    """
+
     Inputs = NROImportDataInputs 
 
-    def prepare(self, **parameters):
+    def prepare(self, **parameters: Dict[str, Any]) -> NROImportDataResults:
+        """Prepare job requests for execution.
+
+        Args:
+            parameters: the parameters to pass through from the superclass.
+        Returns:
+            NROImportDataResults : result object
+        """
         # the input data should be MSes
         # TODO: check data type
         # get results object by running super.prepare()
@@ -73,8 +121,14 @@ class NROImportData(sd_importdata.SDImportData):
         myresults.origin = results.origin
         return myresults
 
-    def analyse(self, result):
-        # get version information for merge2
+    def analyse(self, result) -> NROImportDataResults:
+        """Get version information for merge2 and set it to MS of result.
+
+        Args:
+            result: Result object
+        Returns:
+            NROImportDataResults : result object
+        """
         for ms in result.mses:
             with casa_tools.TableReader(os.path.join(ms.name, 'OBSERVATION')) as tb:
                 col = 'RELEASE_DATE'
