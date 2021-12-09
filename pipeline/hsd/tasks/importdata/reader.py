6dd6bd07e9dd6629aa35093ddc1b6182325c6353
kazuhiko.shimada <kazuhiko.shimada@nao.ac.jp>
2021-12-09 13:33:59 +0900

    PIPE-1275: modified comment

--- pipeline/hsd/tasks/importdata/reader.py
+++ pipeline/hsd/tasks/importdata/reader.py
@@ -341,7 +341,7 @@ class MetaDataReader(object):
         self.invalid_pointing_data[antenna_id].append(row)
 
     def generate_flagcmd(self) -> Dict:
-        """Generate flag command.
+        """Generate flag commands based on pointings of observation and save them in the flag template file.
 
         Returns:
             Dict: generated flag commands
