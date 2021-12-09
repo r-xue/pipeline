859897b2ed69d1fc5e1dd35ca06071063303e6ff
kazuhiko.shimada <kazuhiko.shimada@nao.ac.jp>
2021-12-09 13:25:14 +0900

    PIPE-1275: modified comment

--- pipeline/hsd/tasks/importdata/reader.py
+++ pipeline/hsd/tasks/importdata/reader.py
@@ -411,7 +411,7 @@ class MetaDataReader(object):
         return flagdict
 
     def _generate_flagcmd(self, flagtemplate: str, flag_dict: Dict[Union[Tuple[str, int], int], List[int]], reason: str=''):
-        """Generate flag template command from flag_dict and write to a flag command file.
+        """Generate and store flag commands in the flag template file.
 
         Args:
             flagtemplate: filename of flagtemplate
