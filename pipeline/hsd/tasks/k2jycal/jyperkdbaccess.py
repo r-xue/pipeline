d9ac5612b9931e47594c9f328202a28d491782a5
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2021-12-16 16:07:49 +0900

    PIPE-1282 PR review: update docstring according to the suggestion

--- pipeline/hsd/tasks/k2jycal/jyperkdbaccess.py
+++ pipeline/hsd/tasks/k2jycal/jyperkdbaccess.py
@@ -553,7 +553,7 @@ def get_mean_temperature(vis: str) -> float:
         vis: Name of MS
 
     Returns:
-        Mean temperature during observation. Usually the value in Kelvin.
+        Mean temperature during observation, usually in Kelvin
     """
     with casa_tools.TableReader(os.path.join(vis, 'WEATHER')) as tb:
         valid_temperatures = numpy.ma.masked_array(
