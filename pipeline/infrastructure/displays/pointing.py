e694b05496ad7f73ad9699ea644e5e948dac0b68
kazuhiko.shimada <kazuhiko.shimada@nao.ac.jp>
2021-12-17 11:59:15 +0900

    PIPE-1362: added a comment

--- pipeline/infrastructure/displays/pointing.py
+++ pipeline/infrastructure/displays/pointing.py
@@ -1000,6 +1000,9 @@ class SingleDishPointingChart(object):
 
         ret = self._get_plot_object()
 
+        # execute gc.collect() when the number of uncollected objects reaches 100 or more.
+        # figure.Figure creates a huge number of objects, and if count of plotting points are large,
+        # the python kernel cannot collect objects all at once when process is finished.
         t0, t1, t2 = gc.get_count()
         if t0 > 100:
             gc.collect()
