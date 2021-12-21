4d6e76ccdda86b7c04409d10e31d68ab21d0abad
kazuhiko.shimada <kazuhiko.shimada@nao.ac.jp>
2021-12-21 15:32:42 +0900

    PIPE-1362: modified GC parameter

--- pipeline/infrastructure/displays/pointing.py
+++ pipeline/infrastructure/displays/pointing.py
@@ -1000,10 +1000,10 @@ class SingleDishPointingChart(object):
 
         ret = self._get_plot_object()
 
-        # execute gc.collect() when the number of uncollected objects reaches 100 (decided ad hoc) or more.
+        # execute gc.collect() when the number of uncollected objects reaches 256 (decided ad hoc) or more.
         # figure.Figure creates a huge number of objects, and if plot() is called a significant number of times to plot points,
         # the python kernel cannot collect objects all at once by default GC setting.
-        if gc.get_count()[0] > 100:
+        if gc.get_count()[0] > 255:
             gc.collect()
 
         return ret
