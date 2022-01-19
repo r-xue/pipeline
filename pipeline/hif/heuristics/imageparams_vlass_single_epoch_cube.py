9ecb382486843aff16ec9d9fc4b35bf55c5c5c61
Rui Xue <rxue@nrao.edu>
2022-01-19 13:10:52 -0500

    PIPE-1401: update ImageParamsHeuristicsVlassSeCube()

--- pipeline/hif/heuristics/imageparams_vlass_single_epoch_cube.py
+++ pipeline/hif/heuristics/imageparams_vlass_single_epoch_cube.py
@@ -1,256 +1,56 @@
 import re
-from typing import Union, Tuple
+from typing import Union, Tuple, Optional
 
 import numpy
 
 import pipeline.infrastructure as infrastructure
 from pipeline.infrastructure import casa_tools
 from .imageparams_base import ImageParamsHeuristics
+from .imageparams_vlass_single_epoch_continuum import ImageParamsHeuristicsVlassSeContMosaic
 
 LOG = infrastructure.get_logger(__name__)
 
 
-class ImageParamsHeuristicsVlassSeCube(ImageParamsHeuristics):
+class ImageParamsHeuristicsVlassSeCube(ImageParamsHeuristicsVlassSeContMosaic):
 
     def __init__(self, vislist, spw, observing_run, imagename_prefix='', proj_params=None, contfile=None, linesfile=None, imaging_params={}):
-        ImageParamsHeuristics.__init__(self, vislist, spw, observing_run, imagename_prefix, proj_params, contfile, linesfile, imaging_params)
+        super().__init__(vislist, spw, observing_run, imagename_prefix, proj_params, contfile, linesfile, imaging_params)
         self.imaging_mode = 'VLASS-SE-CUBE'
 
-    # niter
-    def niter_correction(self, niter, cell, imsize, residual_max, threshold, residual_robust_rms, mask_frac_rad=0.0, intent='TARGET'):
-        """Adjust niter value between cleaning iteration steps based on imaging parameters, mask and residual"""
-        if niter:
-            return int(niter)
-        else:
-            return 20000
-
-    def niter(self) -> int:
-        """Tclean niter parameter heuristics."""
-        return self.niter_correction(None, None, None, None, None, None)
-
-    def deconvolver(self, specmode, spwspec) -> str:
-        """Tclean deconvolver parameter heuristics."""
-        return 'mtmfs'
-
-    def robust(self) -> float:
-        """Tclean robust parameter heuristics."""
-        return 1.0
-
-    def gridder(self, intent, field) -> str:
-        """Tclean gridder parameter heuristics."""
-        return 'mosaic'
-
-    def cell(self, beam=None, pixperbeam=None) -> Union[str, list]:
-        """Tclean cell parameter heuristics."""
-        return ['0.6arcsec']
-
-    def imsize(self, fields=None, cell=None, primary_beam=None, sfpblimit=None, max_pixels=None, centreonly=None,
-               vislist=None, spwspec=None) -> Union[list, int]:
-        return [12150, 12150]
-
-    def reffreq(self) -> str:
-        """Tclean reffreq parameter heuristics."""
-        return '3.0GHz'
-
-    def cyclefactor(self, iteration: int) -> float:
-        """Tclean cyclefactor parameter heuristics."""
-        return 3.0
-
-    def cycleniter(self, iteration: int ) -> int:
-        """Tclean cycleniter parameter heuristics."""
-        return 2000
-
-    def scales(self, iteration: Union[int, None] = None) -> list:
-        """Tclean scales parameter heuristics."""
-        return [0]
-
-    def uvtaper(self, beam_natural=None, protect_long=None) -> Union[str, list]:
-        """Tclean uvtaper parameter heuristics."""
-        return []
-
-    def uvrange(self, field=None, spwspec=None) -> tuple:
-        """Tclean uvrange parameter heuristics."""
-        return None, None
-
-    def mask(self, hm_masking=None, rootname=None, iteration=None, mask=None,
-             results_list: Union[list, None] = None) -> str:
-        return ''
-
-    def buffer_radius(self) -> float:
-        return 1000.
-
-    def specmode(self) -> str:
-        """Tclean specmode parameter heuristics."""
-        return 'mfs'
-
-    def intent(self) -> str:
-        """Tclean intent parameter heuristics."""
-        return 'TARGET'
-
-    def nterms(self, spwspec) -> int:
-        """Tclean stokes parameter heuristics."""
-        return 2
-
-    def stokes(self) -> str:
-        """Tclean stokes parameter heuristics."""
-        return 'IQU'
-
-    def pb_correction(self) -> bool:
-        return False
-
-    def conjbeams(self) -> bool:
-        """Tclean conjbeams parameter heuristics."""
-        return False
-
-    def get_sensitivity(self, ms_do, field, intent, spw, chansel, specmode, cell, imsize, weighting, robust, uvtaper):
-        return 0.0, None, None
-
-    def find_fields(self, distance='0deg', phase_center=None, matchregex=''):
-
-        # Created STM 2016-May-16 use center direction measure
-        # Returns list of fields from msfile within a rectangular box of size distance
-
-        qa = casa_tools.quanta
-        me = casa_tools.measures
-        tb = casa_tools.table
-
-        msfile = self.vislist[0]
-
-        fieldlist = []
-
-        phase_center = phase_center.split()
-        center_dir = me.direction(phase_center[0], phase_center[1], phase_center[2])
-        center_ra = center_dir['m0']['value']
-        center_dec = center_dir['m1']['value']
-
-        try:
-            qdist = qa.toangle(distance)
-            qrad = qa.convert(qdist, 'rad')
-            maxrad = qrad['value']
-        except:
-            print('ERROR: cannot parse distance {}'.format(distance))
-            return
-
-        try:
-            tb.open(msfile + '/FIELD')
-        except:
-            print('ERROR: could not open {}/FIELD'.format(msfile))
-            return
-        field_dirs = tb.getcol('PHASE_DIR')
-        field_names = tb.getcol('NAME')
-        tb.close()
-
-        (nd, ni, nf) = field_dirs.shape
-        print('Found {} fields'.format(nf))
-
-        # compile field dictionaries
-        ddirs = {}
-        flookup = {}
-        for i in range(nf):
-            fra = field_dirs[0, 0, i]
-            fdd = field_dirs[1, 0, i]
-            rapos = qa.quantity(fra, 'rad')
-            decpos = qa.quantity(fdd, 'rad')
-            ral = qa.angle(rapos, form=["tim"], prec=9)
-            decl = qa.angle(decpos, prec=10)
-            fdir = me.direction('J2000', ral[0], decl[0])
-            ddirs[i] = {}
-            ddirs[i]['ra'] = fra
-            ddirs[i]['dec'] = fdd
-            ddirs[i]['dir'] = fdir
-            fn = field_names[i]
-            ddirs[i]['name'] = fn
-            if fn in flookup:
-                flookup[fn].append(i)
-            else:
-                flookup[fn] = [i]
-        print('Cataloged {} fields'.format(nf))
-
-        # Construct offset separations in ra,dec
-        print('Looking for fields with maximum separation {}'.format(distance))
-        nreject = 0
-        skipmatch = matchregex == '' or matchregex == []
-        for i in range(nf):
-            dd = ddirs[i]['dir']
-            dd_ra = dd['m0']['value']
-            dd_dec = dd['m1']['value']
-            sep_ra = abs(dd_ra - center_ra)
-            if sep_ra > numpy.pi:
-                sep_ra = 2.0 * numpy.pi - sep_ra
-            # change the following to use dd_dec 2017-02-06
-            sep_ra_sky = sep_ra * numpy.cos(dd_dec)
-
-            sep_dec = abs(dd_dec - center_dec)
-
-            ddirs[i]['offset_ra'] = sep_ra_sky
-            ddirs[i]['offset_ra'] = sep_dec
-
-            if sep_ra_sky <= maxrad:
-                if sep_dec <= maxrad:
-                    if skipmatch:
-                        fieldlist.append(i)
-                    else:
-                        # test regex against name
-                        foundmatch = False
-                        fn = ddirs[i]['name']
-                        for rx in matchregex:
-                            mat = re.findall(rx, fn)
-                            if len(mat) > 0:
-                                foundmatch = True
-                        if foundmatch:
-                            fieldlist.append(i)
-                        else:
-                            nreject += 1
-
-        print('Found {} fields within {}'.format(len(fieldlist), distance))
-        if not skipmatch:
-            print('Rejected {} distance matches for regex'.format(nreject))
-
-        return fieldlist
-
-    def keep_iterating(self, iteration, hm_masking, tclean_stopcode, dirty_dynamic_range, residual_max,
-                       residual_robust_rms, field, intent, spw, specmode) -> Tuple[bool, str]:
-        """Determine if another tclean iteration is necessary."""
-        if iteration == 0:
-            return True, hm_masking
-        elif iteration == 1:
-            LOG.info('Final VLASS single epoch tclean call with no mask')
-            return True, 'none'
-        else:
-            return False, hm_masking
-
-    def threshold(self, iteration: int, threshold: Union[str, float], hm_masking: str) -> Union[str, float]:
-        """Tclean threshold parameter heuristics."""
-        if hm_masking == 'auto':
-            return '0.0mJy'
-        elif hm_masking == 'none':
-            if iteration in [0, 1]:
-                return threshold
-            else:
-                return '0.0mJy'
-        else:
-            return threshold
-
-    def nsigma(self, iteration: int, hm_nsigma: float, hm_masking: str) -> Union[float, None]:
-        """Tclean nsigma parameter heuristics."""
-        if hm_nsigma:
-            return hm_nsigma
-
-        if iteration == 0:
-            return None
-        elif iteration == 1:
-            return 3.0
-        else:
-            return 4.5
-
-    def savemodel(self, iteration: int) -> Union[str, None]:
-        """Tclean savemodel parameter heuristics."""
+    def reffreq(self) -> Optional[str]:
+        """Tclean reffreq parameter heuristics.
+        
+        Default to None for CoarseCube/tclean (automatically calculated as the middle of the selected frequency range)
+        """
         return None
 
-    def usepointing(self) -> bool:
-        """clean flag to use pointing table."""
-        return True
+    def mask(self, hm_masking=None, rootname=None, iteration=None, mask=None,
+             results_list: Union[list, None] = None, clean_no_mask=None) -> Union[str, list]:
+        """Tier-1 mask name to be used for computing Tier-1 and Tier-2 combined mask.
 
-    def wprojplanes(self) -> int:
-        """Tclean wprojplanes parameter heuristics."""
-        return 32
+            Obtain the mask name from the latest MakeImagesResult object in context.results.
+            If not found, then set empty string (as base heuristics)."""
+        mask_list = ''
+        if results_list and type(results_list) is list:
+            for result in results_list:
+                result_meta = result.read()
+                if hasattr(result_meta, 'pipeline_casa_task') and result_meta.pipeline_casa_task.startswith(
+                        'hifv_restorepims'):
+                    mask_list = [r.mask_list for r in result_meta][0]
+
+        # Add 'pb' string as a placeholder for cleaning without mask (pbmask only, see PIPE-977). This should
+        # always stand at the last place in the mask list.
+        # On request for first imaging stage (selfcal image) and automatically for the final imaging stage.
+        if (clean_no_mask and self.vlass_stage == 1) or self.vlass_stage == 3:
+            if clean_no_mask:
+                LOG.info('Cleaning without user mask is performed in pre-self calibration imaging stage '
+                         '(clean_no_mask_selfcal_image=True)')
+            if type(mask_list) is list:
+                mask_list.append('pb')
+            elif mask_list != '':  # mask is non-empty string
+                mask_list = [mask_list, 'pb']
+            else:
+                mask_list = 'pb'
+
+        # In case hif_makeimages result was not found or results_list was not provided
+        return mask_list
