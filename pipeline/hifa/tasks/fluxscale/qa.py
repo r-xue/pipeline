269f47689c93c1ae2c7240286abd5bca7892b5e0
Jose Sabater <jsm@jsabater.info>
2022-01-24 11:59:20 +0000

    PIPE-1208: Clean and better info messages

--- pipeline/hifa/tasks/fluxscale/qa.py
+++ pipeline/hifa/tasks/fluxscale/qa.py
@@ -337,12 +337,11 @@ def gaincalSNR(context, ms, tsysTable, flux, field, spws, intent='PHASE', requir
             median_tsys[phase_spw.id] = numpy.ma.median(scan_data['FPARAM'])
 
     # PIPE-1208: If any scan is fully masked we retrieve the SNR estimated from hifa_spwphaseup
+    snr_info = None
     if get_snr_info:
         snr_info = ms.spwphaseup_snr_info
         if snr_info is None:
             LOG.error("Estimated SNR from hifa_spwphaseup could not be retrieved")
-    else:
-        snr_info = None  # This line should not be reached
 
     # 6) compute the expected channel-averaged SNR
     # TODO Ask Todd if this is an error or a confusingly-named variable
@@ -397,11 +396,11 @@ def gaincalSNR(context, ms, tsysTable, flux, field, spws, intent='PHASE', requir
                         min([aggregate_bandwidth, max_effective_bandwidth_per_baseband * num_basebands]) /
                         min([spw.bandwidth, max_effective_bandwidth_per_baseband]))
                 )
-                LOG.info(f"spw {spw.id} SNR extracted from hifa_spwphaseup ({snr_value})")
+                LOG.info(f"{ms.basename}: for SpW {spw.id} SNR extracted from hifa_spwphaseup ({snr_value})")
             else:
                 mydict[spw.id]['snr'] = Decimal('0.0')
                 mydict[spw.id]['snr_aggregate'] = Decimal('0.0')
-                LOG.error(f"spw {spw.id} SNR could not be extracted from hifa_spwphaseup, SNR set to 0")
+                LOG.error(f"{ms.basename}: for SpW {spw.id}  SNR could not be extracted from hifa_spwphaseup, SNR set to 0")
         else:
             mydict[spw.id]['snr'] = snr_per_spw
             mydict[spw.id]['snr_aggregate'] = spw_to_flux_density[spw.id] / aggregate_bandwidth_sensitivity
