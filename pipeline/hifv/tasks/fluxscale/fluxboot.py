536897cac9c6615ed3c5560cc3efadad55c643b9
Brian Kent <bkent@nrao.edu>
2022-01-31 11:11:53 -0500

    PIPE-1287.  PIPE-1350.  Small update to account for cases when fields are identified as both POINTING and TARGET or PHASE, and there is more than 1 intent listed for the field.  These fields will not be used in the transfer kwarg of the fluxscale task.

--- pipeline/hifv/tasks/fluxscale/fluxboot.py
+++ pipeline/hifv/tasks/fluxscale/fluxboot.py
@@ -397,15 +397,19 @@ class Fluxboot(basetask.StandardTaskTemplate):
         calfieldlist = []
         for field in calfieldliststrings:
             fieldobj = m.get_fields(field_id=int(field))
-            if (len(fieldobj[0].intents) == 1 and 'POINTING' in fieldobj[0].intents) or \
-                    (len(fieldobj[0].intents) == 1 and 'SYSTEM_CONFIGURATION' in fieldobj[0].intents) or \
-                    (len(fieldobj[0].intents) == 1 and 'UNSPECIFIED#UNSPECIFIED' in fieldobj[0].intents) or \
-                    (len(fieldobj[0].intents) == 2 and 'POINTING' in fieldobj[0].intents and 'SYSTEM_CONFIGURATION' in fieldobj[0].intents) or \
-                    (len(fieldobj[0].intents) == 2 and 'POINTING' in fieldobj[0].intents and 'UNSPECIFIED#UNSPECIFIED' in fieldobj[0].intents) or \
-                    (len(fieldobj[0].intents) == 2 and 'SYSTEM_CONFIGURATION' in fieldobj[0].intents and 'UNSPECIFIED#UNSPECIFIED' in fieldobj[0].intents) or \
-                    (len(fieldobj[0].intents) == 3 and 'POINTING' in fieldobj[0].intents and 'SYSTEM_CONFIGURATION' in fieldobj[0].intents and 'UNSPECIFIED#UNSPECIFIED' in fieldobj[0].intents):
+            nfldobj = len(fieldobj[0].intents)
+            if (nfldobj == 1 and 'POINTING' in fieldobj[0].intents) or \
+               (nfldobj == 1 and 'SYSTEM_CONFIGURATION' in fieldobj[0].intents) or \
+               (nfldobj == 1 and 'UNSPECIFIED#UNSPECIFIED' in fieldobj[0].intents) or \
+               (nfldobj == 2 and 'POINTING' in fieldobj[0].intents and 'SYSTEM_CONFIGURATION' in fieldobj[0].intents) or \
+               (nfldobj == 2 and 'POINTING' in fieldobj[0].intents and 'UNSPECIFIED#UNSPECIFIED' in fieldobj[0].intents) or \
+               (nfldobj == 2 and 'SYSTEM_CONFIGURATION' in fieldobj[0].intents and 'UNSPECIFIED#UNSPECIFIED' in fieldobj[0].intents) or \
+               (nfldobj == 3 and 'POINTING' in fieldobj[0].intents and 'SYSTEM_CONFIGURATION' in fieldobj[0].intents and 'UNSPECIFIED#UNSPECIFIED' in fieldobj[0].intents) or \
+               (nfldobj > 1 and 'POINTING' in fieldobj[0].intents and 'TARGET' in fieldobj[0].intents) or \
+               (nfldobj > 1 and 'POINTING' in fieldobj[0].intents and 'PHASE' in fieldobj[0].intents):
 
-                LOG.debug("Single INTENT not included")
+                LOG.info("Single INTENT not included for field {!s}: {!s} "
+                         "and intents {!s}".format(field, fieldobj[0].name, fieldobj[0].intents))
             else:
                 calfieldlist.append(field)
 
