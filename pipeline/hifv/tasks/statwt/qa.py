7206f527de3636eb574a6330ecfb79952332b199
Kristin Berry <kberry@nrao.edu>
2022-07-11 15:11:16 -0400

    PIPE-1341: Fix order of QA conditional so that correct scores are assigned

--- pipeline/hifv/tasks/statwt/qa.py
+++ pipeline/hifv/tasks/statwt/qa.py
@@ -41,24 +41,23 @@ class StatwtQAHandler(pqa.QAPlugin):
                                 metric_score=mean, #TODO: update to include both mean and variance
                                 metric_units='')
 #        if mean > 0.1 and variance > 0.1: #TODO: remove -- only here to test how this shows up.
-        if mean > 1000 and variance > 50000:
+        if mean > 1000000 and variance > 5000000:
+            score = 0.0
+            shortmsg = 'Very high mean and variance of weights'
+            longmsg = 'Very High mean and variance of weights; bad weights are very likely to be present and require flagging'
+            result.qa.pool.append(pqa.QAScore(score, longmsg=longmsg, shortmsg=shortmsg, vis=vis, origin=jobs_origin))
+        elif mean > 10000 and variance > 500000:
+            score = 0.1
+            shortmsg = 'High mean and variance of weights'
+            longmsg = 'High mean and variance of weights; possibly erroneous weights present that may require flagging'
+            result.qa.pool.append(pqa.QAScore(score, longmsg=longmsg, shortmsg=shortmsg, vis=vis, origin=jobs_origin))
+        elif mean > 1000 and variance > 50000:
             #score = 0.1 
             score=0.75
             shortmsg = 'Moderately high mean and variance for weights.'
             longmsg = 'Moderately high mean and variance for weight; possibly erroneous weights present that may require flagging.'
             result.qa.pool.append(pqa.QAScore(score, longmsg=longmsg, shortmsg=shortmsg, vis=vis, origin=jobs_origin, weblog_location=pqa.WebLogLocation.ACCORDION))
 
-        elif mean > 10000 and variance > 500000:
-            score = 0.1
-            shortmsg = 'High mean and variance of weights'
-            longmsg = 'High mean and variance of weights; possibly erroneous weights present that may require flagging'
-            result.qa.pool.append(pqa.QAScore(score, longmsg=longmsg, shortmsg=shortmsg, vis=vis, origin=jobs_origin))
-
-        elif mean > 1000000 and variance > 5000000:
-            score = 0.0
-            shortmsg = 'Very high mean and variance of weights'
-            longmsg = 'Very High mean and variance of weights; bad weights are very likely to be present and require flagging'
-            result.qa.pool.append(pqa.QAScore(score, longmsg=longmsg, shortmsg=shortmsg, vis=vis, origin=jobs_origin))
         # does this need an else score? -- do they always want some score here. i.e. else = some default (failure or pass)
         #TODO: check--does the QA have a default score? 
 
