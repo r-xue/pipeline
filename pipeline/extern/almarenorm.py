d8ca17aff9d8450b41197558b87faa75ddbe22e9
joe.masters <jmasters@nrao.edu>
2022-12-02 16:11:18 -0300

    PIPE-1693: copy consecutive_groups from more_itertools package

--- pipeline/extern/almarenorm.py
+++ pipeline/extern/almarenorm.py
@@ -452,6 +452,54 @@ mytb=tbtool()
 myms=mstool()
 
 
+def consecutive_groups(iterable, ordering=lambda x: x):
+    """Yield groups of consecutive items using :func:`itertools.groupby`.
+    The *ordering* function determines whether two items are adjacent by
+    returning their position.
+
+    By default, the ordering function is the identity function. This is
+    suitable for finding runs of numbers:
+
+        >>> iterable = [1, 10, 11, 12, 20, 30, 31, 32, 33, 40]
+        >>> for group in consecutive_groups(iterable):
+        ...     print(list(group))
+        [1]
+        [10, 11, 12]
+        [20]
+        [30, 31, 32, 33]
+        [40]
+
+    For finding runs of adjacent letters, try using the :meth:`index` method
+    of a string of letters:
+
+        >>> from string import ascii_lowercase
+        >>> iterable = 'abcdfgilmnop'
+        >>> ordering = ascii_lowercase.index
+        >>> for group in consecutive_groups(iterable, ordering):
+        ...     print(list(group))
+        ['a', 'b', 'c', 'd']
+        ['f', 'g']
+        ['i']
+        ['l', 'm', 'n', 'o', 'p']
+
+    Each group of consecutive items is an iterator that shares it source with
+    *iterable*. When an an output group is advanced, the previous group is
+    no longer available unless its elements are copied (e.g., into a ``list``).
+
+        >>> iterable = [1, 2, 11, 12, 21, 22]
+        >>> saved_groups = []
+        >>> for group in consecutive_groups(iterable):
+        ...     saved_groups.append(list(group))  # Copy group elements
+        >>> saved_groups
+        [[1, 2], [11, 12], [21, 22]]
+
+    Borrowed for PL use from https://pypi.org/project/more-itertools/
+    """
+    for k, g in groupby(
+        enumerate(iterable), key=lambda x: x[0] - ordering(x[1])
+    ):
+        yield map(itemgetter(1), g)
+
 class ACreNorm(object):
 
 
@@ -4236,7 +4284,6 @@ class ACreNorm(object):
             Purpose: Find subset ranges within an array of consecutive values and
                      return the sub-ranges.
             """
-            from more_itertools import consecutive_groups
             subsets = [list(group) for group in consecutive_groups(chans)]
             ranges = []
             for ss in subsets:
