d75d38dbe8b8f4581f2753714040b5b2edeb3d69
kazuhiko.shimada <kazuhiko.shimada@nao.ac.jp>
2022-12-19 16:04:41 +0900

    PIPE-1207: blushed up debugwrapper

--- pipeline/infrastructure/utils/debugwrapper.py
+++ pipeline/infrastructure/utils/debugwrapper.py
@@ -3,43 +3,69 @@ import functools
 import inspect
 import logging
 import os
+import pprint
 
 import pipeline.infrastructure as infrastructure
 
 
-def debugwrapper(msg: str=''):
+def debugwrapper(msg: str='', pp: bool=False):
     """Decorate methods to debug
 
     Args:
-        msg (str, optional): debug message. Defaults to ''.
+        msg : description string to output to log
+        pp : pretty-print flag of argument objects
     """
     def wrapper(func):
-        logger = infrastructure.get_logger(func.__name__)
-
         @functools.wraps(func)
         def f(*args, **kwargs):
+            logger = infrastructure.get_logger(func.__name__)
             retval = func(*args, **kwargs)
             if logger.level <= logging.DEBUG:
-                value = kwargs['value'] if 'value' in kwargs else args[-1]
-                __debug(logger, args[0], value, msg)
+                _self = args[0]
+
+                # check whether it wraps __setattr__()
+                if func.__name__ == '__setattr__':
+                    attr = args[1]
+                else:
+                    attr = False
+                
+                value = kwargs.get('value', args[-1])
+                __debug(logger, _self, attr, value, msg, pp)
             return retval
         return f
     return wrapper
 
 
-def __debug(logger: logging.Logger, cls: object, obj: object, msg: str):
+def __debug(logger: logging.Logger, cls: object, attr: str, obj: object, msg: str, pp: bool):
     """Output debug strings.
 
     Args:
         cls : caller class object
+        attr : attribute when the wrapper wrapped __setattr__
         obj : object to output
         msg : action message
+        pp : pretty-print flag of argument objects
     """
-    outerframes = inspect.getouterframes(inspect.currentframe())
+    outer_frames = inspect.getouterframes(inspect.currentframe())
+
+    # step frame position outer of debugwrapper.py
     for i, frame in enumerate(inspect.getouterframes(inspect.currentframe())):
         if not frame.filename.endswith(__file__):
             break
-    source = os.path.basename(outerframes[i].filename)
-    clsname = cls.__class__.__name__
-    logger.debug(f'{source}[{outerframes[i].lineno}] {msg}: {clsname}.{outerframes[i-1].function}: {type(obj)} {obj} '
-                 f'at {outerframes[i].function}')
+
+    source = os.path.basename(outer_frames[i].filename)
+    cls_name = cls.__class__.__name__
+    msg_str = f'{source}[{outer_frames[i].lineno}] {msg}: {cls_name}.{outer_frames[i].function}'
+
+    # if the wrapper wrapped __setattr__ then output the variable name
+    if attr is not False:
+        msg_str += f'.{attr}'
+
+    # pretty-print
+    if pp:
+        obj_s = pprint.pformat(obj)
+        msg_str += f':\n{type(obj)}\n{obj_s}'
+    else:
+        msg_str += f': {type(obj)} {obj}'
+
+    logger.debug(msg_str)
