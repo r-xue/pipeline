e6c07d3adcadb4d922796d29647a4f3259c472f2
Dirk Muders <dmuders@mpifr-bonn.mpg.de>
2023-04-20 13:53:25 +0000

    PIPE-1655: Removed uvconfit related imports.

--- pipeline/hif/tasks/uvcontsub/__init__.py
+++ pipeline/hif/tasks/uvcontsub/__init__.py
@@ -5,15 +5,14 @@ import pipeline.infrastructure.renderer.weblog as weblog
 from pipeline.h.tasks.applycal import applycal
 from . import qa
 from . import renderer
-from . import uvcontfit
 from . import uvcontsub
-from .uvcontfit import UVcontFit
 from .uvcontsub import UVcontSub
 
-qaadapter.registry.register_to_dataset_topic(uvcontfit.UVcontFitResults)
-qaadapter.registry.register_to_dataset_topic(applycal.ApplycalResults)
+#qaadapter.registry.register_to_dataset_topic(uvcontfit.UVcontFitResults)
+qaadapter.registry.register_to_dataset_topic(uvcontsub.UVcontSubResults)
+#qaadapter.registry.register_to_dataset_topic(applycal.ApplycalResults)
 
-weblog.add_renderer(UVcontFit, renderer.T2_4MDetailsUVcontFitRenderer(), group_by=weblog.UNGROUPED)
+#weblog.add_renderer(UVcontFit, renderer.T2_4MDetailsUVcontFitRenderer(), group_by=weblog.UNGROUPED)
 weblog.add_renderer(UVcontSub, renderer.T2_4MDetailsUVcontSubRenderer(), group_by=weblog.UNGROUPED)
 # weblog.add_renderer(UVcontSub,
 #                     basetemplates.T2_4MDetailsDefaultRenderer(description='Continuum subtract the TARGET data'),
