dfc6a1bc004b36a02052be33e360b532c80e577f
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2023-05-08 11:39:26 +0900

    avoid TypeError when previous task execution was interrupted or failed

--- pipeline/infrastructure/timetracker.py
+++ pipeline/infrastructure/timetracker.py
@@ -112,7 +112,15 @@ class TaskTimeTracker(object):
             for k, stages in db.items():
                 r[k] = {}
                 for e in stages.values():
-                    duration = e.end - e.start
+                    # Avoid TypeError (PIPE-1321)
+                    # Assumption here is that e.start always holds valid
+                    # datetime while e.end can be None if task execution
+                    # is interrupted/failed (cf. on_lifecycle_event method).
+                    if e.end is not None:
+                        duration = e.end - e.start
+                    else:
+                        # set zero as a sign of unreliable value
+                        duration = datetime.timedelta(0)
                     duration_secs = duration.total_seconds()
                     duration_hms = utils.format_timedelta(duration)
                     r[k][e.stage] = {'seconds': duration_secs, 'hms': duration_hms}
