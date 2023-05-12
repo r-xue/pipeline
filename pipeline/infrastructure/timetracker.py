58e2ae0593a5af6b6fd5130bea5259284048e51a
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2023-05-12 13:31:05 +0900

    Revert "additional fix to avoid TypeError"
    This reverts commit a53442c4d2fa1032db4ef7eecd80502e9fc20e30.

--- pipeline/infrastructure/timetracker.py
+++ pipeline/infrastructure/timetracker.py
@@ -131,11 +131,7 @@ class TaskTimeTracker(object):
                     result_state = db['results'][stage_number]
                 except KeyError:
                     continue
-                # Avoid TypeError (PIPE-1321)
-                if result_state.end is not None:
-                    duration = result_state.end - task_state.start
-                else:
-                    duration = datetime.timedelta(0)
+                duration = result_state.end - task_state.start
                 duration_secs = duration.total_seconds()
                 duration_hms = utils.format_timedelta(duration)
                 r['total'][result_state.stage] = {'seconds': duration_secs, 'hms': duration_hms}
