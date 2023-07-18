da22f84c43675b58affc2e61cb38f1aaf2f82caa
Vincent Geers <11389220+vcgeers@users.noreply.github.com>
2023-07-18 10:17:19 +0100

    PIPE-1776: minor corrections to hifa_polcal task description.

--- pipeline/hifa/cli/hifa_polcal.py
+++ pipeline/hifa/cli/hifa_polcal.py
@@ -6,28 +6,27 @@ import pipeline.h.cli.utils as utils
 def hifa_polcal(vis=None, solint_chavg=None, vs_stats=None, vs_thresh=None, pipelinemode=None, dryrun=None,
                 acceptresults=None):
     """
-    hifa_polcal ---- Derive instrumental polarisation calibration for ALMA.
+    hifa_polcal ---- Derive instrumental polarization calibration for ALMA.
 
 
-    Derive the instrumental polarisation calibrations for ALMA using the
-    polarisation calibrators.
+    Derive the instrumental polarization calibrations for ALMA using the
+    polarization calibrators.
 
     --------- parameter descriptions ---------------------------------------------
 
     vis             The list of input MeasurementSets. Defaults to the list of MeasurementSets
-                    specified in the pipeline context
-
-                    example: ['M32A.ms', 'M32B.ms']
+                    specified in the pipeline context.
+                    Example: ['M32A.ms', 'M32B.ms']
     solint_chavg    Channel averaging to include in solint for gaincal steps
                     producing X-Y delay, X-Y phase, and leakage solutions.
-                    Default: '5MHz'.
+                    Default: '5MHz'
     vs_stats        List of visstat statistics to use for diagnostic comparison
                     between session MS and individual MSes in that session.
                     Default: ['min','max','mean']
     vs_thresh       Threshold to use in diagnostic comparison of visstat
                     statistics; relative differences larger than this threshold
                     are reported in the CASA log.
-                    Default:
+                    Default: 1e-3
     pipelinemode    The pipeline operating mode. In 'automatic' mode the pipeline
                     determines the values of all context defined pipeline inputs automatically.
                     In interactive mode the user can set the pipeline context defined
@@ -41,7 +40,7 @@ def hifa_polcal(vis=None, solint_chavg=None, vs_stats=None, vs_thresh=None, pipe
     --------- examples -----------------------------------------------------------
 
 
-    1. Compute the polarisation calibrations:
+    1. Compute the polarization calibrations:
 
     hifa_polcal()
 
