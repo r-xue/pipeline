86d4055ff6452ee371c7432cb4feb9f337113712
Vincent Geers <11389220+vcgeers@users.noreply.github.com>
2023-08-25 16:38:21 +0100

    PIPE-1701: update docstring hifa_bpsolint CLI to separate bullet points in task description.

--- pipeline/hifa/cli/hifa_bpsolint.py
+++ pipeline/hifa/cli/hifa_bpsolint.py
@@ -20,39 +20,55 @@ def hifa_bpsolint(vis=None, field=None, intent=None, spw=None, phaseupsnr=None, 
     - For each data set the list of source(s) to use for bandpass solution
       signal-to-noise estimation is compiled based on the values of the
       ``field``, ``intent``, and ``spw`` parameters.
+
     - Source fluxes are determined for each spw and source combination.
+
     - Fluxes in Jy are derived from the pipeline context.
+
     - Pipeline context fluxes are derived from the online flux calibrator
       catalog, the ASDM, or the user via the flux.csv file.
+
     - If no fluxes are available the task terminates.
+
     - Atmospheric calibration and observations scans are determined for each
       spw and source combination.
+
     - If ``intent`` is set to 'PHASE' are there are no atmospheric scans
       associated with the 'PHASE' calibrator, 'TARGET' atmospheric scans will be
       used instead.
+
     - If atmospheric scans cannot be associated with any of the spw and source
       combinations the task terminates.
+
     - Science spws are mapped to atmospheric spws for each science spw and
       source combination.
+
     - If mappings cannot be determined for any of the spws the task terminates.
+
     - The median Tsys value for each atmospheric spw and source combination is
       determined from the SYSCAL table. Medians are computed first by channel,
       then by antenna, in order to reduce sensitivity to deviant values.
+
     - The science spw parameters, exposure time(s), and integration time(s) are
       determined.
+
     - The phase-up time interval, in time units and number of integrations
       required to meet the ``phaseupsnr`` are computed, along with the
       sensitivity in mJy and the signal-to-noise per integration. Nominal Tsys
       and sensitivity values per receiver band provided by the ALMA project are
       used for this estimate.
+
     - Warnings are issued if estimated phase-up gain time solution would contain
       fewer than ``minphaseupints`` solutions.
+
     - The frequency interval, in MHz and number of channels required to meet the
       ``bpsnr`` are computed, along with the per channel sensitivity in mJy and
       the per channel signal-to-noise. Nominal Tsys and sensitivity values per
       receiver band provided by the ALMA project are used for this estimate.
+
     - Warnings are issued if estimated bandpass solution would contain fewer
       than ``minbpnchan`` solutions.
+
     - If strong atmospheric features are detected in the Tsys spectrum for a
       given spw, the frequency interval of bandpass solution is recalculated to
       meet the lower threshold, ``minbpsnr`` - i.e. a lower snr is tolerated in
