75a46aa981f89e7ab13f78f4f4dbcf7b8a994070
Rui Xue <rxue@nrao.edu>
2023-09-11 15:07:46 -0500

    PIPE-1945: add inline comments to clarify task Result vs. ResultList in the T2_4MDetailsDefaultRenderer class.

--- pipeline/hifv/tasks/vlassmasking/renderer.py
+++ pipeline/hifv/tasks/vlassmasking/renderer.py
@@ -21,6 +21,9 @@ class T2_4MDetailsVlassmaskingRenderer(basetemplates.T2_4MDetailsDefaultRenderer
         weblog_dir = os.path.join(context.report_dir, 'stage%s' % results.stage_number)
 
         summary_plots = {}
+        # PIPE-1945: even hifv_vlassmasking is converted into a multi-vis task, i.e., its task return is a Result
+        # instead of ResultList Object, htmlrenderer.py/T2_4MDetailsRender.render() always tries to wrap
+        # the task result into a list. Here again, we need to pick the first element in the list.
         result = results[0]
 
         plotter = display.MaskSummary(context, result)
@@ -36,7 +39,6 @@ class T2_4MDetailsVlassmaskingRenderer(basetemplates.T2_4MDetailsDefaultRenderer
         #     numfound = str(int(stdout.split()[0]) - 3)   # minus three to remove region header
         # except Exception as e:
         #     numfound = ""
-
 
         ctx.update({'summary_plots': summary_plots,
                     'dirname': weblog_dir})
