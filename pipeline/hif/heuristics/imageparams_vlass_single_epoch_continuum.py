r-xue/pipeline

commit ec2c50cd8a8b2bbedf78abe21a301b2ec8268848

PIPE-1945: adaptions after some tasks got converted into multi-vis tasks.

pipeline/hif/heuristics/imageparams_vlass_single_epoch_continuum.py
@@ -113,7 +113,7 @@ class ImageParamsHeuristicsVlassSeCont(ImageParamsHeuristics):
         if self.vlass_stage == 3:
             return ''
         else:
-            # PIPE-1679: the previous default value of '3arcsec' has been changed to '3/(pi/(4ln(2)))arcsec' 
+            # PIPE-1679: the previous default value of '3arcsec' has been changed to '3/(pi/(4ln(2)))arcsec'
             # since CASA ver>=6.5.3 to maintain the beam size consistency due to the math correction from CAS-13260.
             return ['2.6476arcsec']
 
@@ -133,7 +133,7 @@ class ImageParamsHeuristicsVlassSeCont(ImageParamsHeuristics):
                 result_meta = result.read()
                 if hasattr(result_meta, 'pipeline_casa_task') and result_meta.pipeline_casa_task.startswith(
                         'hifv_vlassmasking'):
-                    mask_list = [r.combinedmask for r in result_meta][0]
+                    mask_list = result_meta.combinedmask[0]
 
         # Add 'pb' string as a placeholder for cleaning without mask (pbmask only, see PIPE-977). This should
         # always stand at the last place in the mask list.
@@ -364,7 +364,6 @@ class ImageParamsHeuristicsVlassSeCont(ImageParamsHeuristics):
         return 5.0
 
     def get_autobox_params(self, iteration: int, intent: str, specmode: str, robust: float) -> tuple:
-
         """Default auto-boxing parameters."""
 
         sidelobethreshold = None
@@ -598,7 +597,6 @@ class ImageParamsHeuristicsVlassSeContMosaic(ImageParamsHeuristicsVlassSeCont):
         return [12500, 12500]
 
     def mosweight(self, intent, field) -> bool:
-
         """tclean flag to use mosaic weighting."""
 
         # Currently only ALMA has decided to use this flag (CAS-11840). So

pipeline/hifv/tasks/analyzestokescubes/renderer.py
@@ -51,7 +51,7 @@ class T2_4MDetailsAnalyzestokesCubeRenderer(basetemplates.T2_4MDetailsDefaultRen
                     result_meta = result
                     if hasattr(result_meta, 'pipeline_casa_task') and result_meta.pipeline_casa_task.startswith(
                             'hif_makecutoutimages'):
-                        r_makecutoutimages_copy = copy.deepcopy(result_meta[0])
+                        r_makecutoutimages_copy = copy.deepcopy(result_meta)
             r_makecutoutimages_copy.stage_number = result.stage_number
             plotter = VlassCubeCutoutRmsSummary(context, r_makecutoutimages_copy)
             rmsplots = {'Rms Summary Plot': plotter.plot(improp_list=[('rms', 'Median')])}

commit c3caed75ab792876fb18661857a455804b37f103

PIPE-1945: fix a bug introduced in ec2c50cd.

pipeline/hif/heuristics/imageparams_vlass_single_epoch_continuum.py
@@ -133,7 +133,7 @@ class ImageParamsHeuristicsVlassSeCont(ImageParamsHeuristics):
                 result_meta = result.read()
                 if hasattr(result_meta, 'pipeline_casa_task') and result_meta.pipeline_casa_task.startswith(
                         'hifv_vlassmasking'):
-                    mask_list = result_meta.combinedmask[0]
+                    mask_list = result_meta.combinedmask
 
         # Add 'pb' string as a placeholder for cleaning without mask (pbmask only, see PIPE-977). This should
         # always stand at the last place in the mask list.

pipeline/hifv/tasks/vlassmasking/renderer.py
@@ -28,8 +28,8 @@ class T2_4MDetailsVlassmaskingRenderer(basetemplates.T2_4MDetailsDefaultRenderer
 
         plotter = display.MaskSummary(context, result)
         plots = plotter.plot()
-        ms = os.path.basename(result.inputs['vis'])
-        summary_plots[ms] = plots
+        mslist_str = '<br>'.join([os.path.basename(vis) for vis in result.inputs['vis']])
+        summary_plots[mslist_str] = plots
 
         # Number of islands found
         # try: