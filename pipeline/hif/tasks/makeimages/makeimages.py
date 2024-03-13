efbabc9da563e88701fd02b20f6a8a0636ffb4d9
Dirk Muders <dmuders@mpifr-bonn.mpg.de>
2024-03-13 15:26:19 +0000

    PIPE-2100: Cosmetic change to trigger a new build.

--- pipeline/hif/tasks/makeimages/makeimages.py
+++ pipeline/hif/tasks/makeimages/makeimages.py
@@ -154,7 +154,7 @@ class MakeImages(basetask.StandardTaskTemplate):
                     # Note add_result() removes 'heuristics' from worker_result
                     heuristics = target['heuristics']
                     result.add_result(worker_result, target, outcome='success')
-                    # Export RMS of  sources              
+                    # Export RMS of  sources
                     if self._is_target_for_sensitivity(worker_result, heuristics):
                         s = self._get_image_rms_as_sensitivity(worker_result, target, heuristics)
                         if s is not None:
