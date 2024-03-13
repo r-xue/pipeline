76faafe6bb510383b8b40f3ed43181ad2668796f
Shawn Booth <sbooth@nrao.edu>
2024-03-13 14:27:24 -0400

    PIPE-672: implemented algorithm for determining possible maser overlap, including conversion from native TOPO reference frame to LSRK; added function to modify SPECTRAL_WINDOW table to track which SPWs were smoothed

--- pipeline/hifv/tasks/hanning/hanning.py
+++ pipeline/hifv/tasks/hanning/hanning.py
@@ -7,7 +7,7 @@ import pipeline.infrastructure.vdp as vdp
 from pipeline.infrastructure import casa_tasks
 from pipeline.infrastructure import casa_tools
 from pipeline.infrastructure import task_registry
-from casatools import ms
+from pipeline.domain.measures import FrequencyUnits
 
 LOG = infrastructure.get_logger(__name__)
 
@@ -91,8 +91,27 @@ class Hanning(basetask.StandardTaskTemplate):
             HanningResults() type object
         """
 
+        def create_message(smoothing_windows):
+            message_lists = [[smoothing_windows[0]]]
+            for x in smoothing_windows[1:]:
+                if int(x) != int(message_lists[-1][-1]) + 1:
+                    message_lists.append([])
+                message_lists[-1].append(x)
+            message = ""
+            for message_list in message_lists:
+                if len(message_list) > 1:
+                    message += "{}~{}, ".format(message_list[0], message_list[-1])
+                else:
+                    message += message_list[0] + ", "
+            if message[-2:] == ", ":
+                message = message[:-2]
+            message = message.replace("~", "-")
+            message = ", and".join(message.rsplit(",", 1))
+            return message
+
         spw_preaverage = self._getpreaveraged()
         if not spw_preaverage:
+            LOG.info("All spectral windows were selected for hanning smoothing")
             try:
                 self._do_hanningsmooth()
                 LOG.info("Removing original VIS " + self.inputs.vis)
@@ -101,12 +120,21 @@ class Hanning(basetask.StandardTaskTemplate):
                 os.rename('temphanning.ms', self.inputs.vis)
             except Exception as ex:
                 LOG.warning('Problem encountered with hanning smoothing. ' + str(ex))
+            finally:
+                smoothing_windows = self.inputs.vis.spectral_windows
         else:
             smoothing_windows = self._getsmoothingwindows(spw_preaverage)
-            with casa_tools.MSReader(self.inputs.vis, nomodify=False) as ms:
-                staql = {'spw': ",".join(smoothing_windows)}
-                ms.select(staql)
-                ms.hanningsmooth('data')
+            if smoothing_windows:
+                message = create_message(smoothing_windows)
+                LOG.info("Smoothing spectral window(s) {}.".format(message))
+                with casa_tools.MSReader(self.inputs.vis, nomodify=False) as ms:
+                    staql = {'spw': ",".join(smoothing_windows)}
+                    ms.msselect(staql)
+                    ms.hanningsmooth('data')
+            else:
+                LOG.info("None of the SPWs were selected for smoothing.")
+        if smoothing_windows:
+            self._track_hsmooth(smoothing_windows)
 
         return HanningResults()
 
@@ -144,26 +172,21 @@ class Hanning(basetask.StandardTaskTemplate):
             spw_preaverage(dict): SDM_NUM_BIN column from SPECTRAL_WINDOW table of vis
 
         Return: List
-            Spectral window IDs that need to be hanning smoothed
+            Spectral window IDs that need to be hanning smoothed; an empty list will mean no windows should be smoothed
         """
 
         smooth_windows = list()
-        with casa_tools.MSReader(self.inputs.vis) as ms:
-            spws = ms.getspectralwindowinfo()
         for key, value in spw_preaverage.items():
             spw = str(int(key.split("r")[1]) - 1)
+            self._checkmaserline(spw)
             if value > 1:
-                info = spws[spw]
-                freq1 = info['Chan1Freq'] / 1e6
-                freq2 = freq1 + (info['TotalWidth'] / 1e6)
-                if self._checkmaserline(freq1, freq2):
+                # smooth preaveraged windows if maser line possible in window to avoid Gibbs ringing
+                if self._checkmaserline(spw):
                     smooth_windows.append(spw)
             else:
                 smooth_windows.append(spw)
 
-        assert smooth_windows  # throws error if list is empty
-
-        return smooth_windows
+        return sorted(smooth_windows, key=int)
 
     def _getpreaveraged(self):
         """Return SDM_NUM_BIN table row if it exists. Empty dict signifies to smooth all windows.
@@ -172,54 +195,79 @@ class Hanning(basetask.StandardTaskTemplate):
         """
 
         with casa_tools.TableReader(self.inputs.vis + '/SPECTRAL_WINDOW') as table:
-            # effective_bw = table.getvarcol('EFFECTIVE_BW')
-            # resolution = table.getvarcol('RESOLUTION')
             try:
                 sdm_num_bin = table.getvarcol('SDM_NUM_BIN')
-                # max_sdm_num_bin = max([sdm_num_bin[key][0] for key in sdm_num_bin])
             except Exception as e:
-                # max_sdm_num_bin = 1
                 sdm_num_bin = dict()
-                LOG.debug('Column SDM_NUM_BIN was not found in the SDM.  Proceeding with hanning smoothing.')
+                LOG.debug('Column SDM_NUM_BIN was not found in the SDM.')
 
         return sdm_num_bin
 
-        # return not(resolution['r1'][0][0] < effective_bw['r1'][0][0])
-
-        # if max_sdm_num_bin > 1:
-        #     return False
-        # else:
-        #     return True
-
-    def _checkmaserline(self, freq1, freq2):
+    def _checkmaserline(self, spw: str):
         """Confirm if known maser line(s) appear in frequency range of spectral window
 
         Args:
-            freq1(float): frequency in MHz of the beginning of spectral window
-            freq2(float): frequency in MHz of the end of spectral window
+            spw(dict): spectral window number
 
         Return: Boolean
             True if maser line may exist in window; False otherwise
         """
 
-        maser_dict = {
-            'OH (1)': 1612.2310,
-            'OH (2)': 1665.4018,
-            'OH (3)': 1667.3590,
-            'OH (4)': 1720.5300,
-            'H2O': 22235.08,
-            'CH3OH (1)': 6668.5192,
-            'CH3OH (2)':  12178.597,
-            'SiOv0': 43423.858,
-            'SiOv1': 43122.079,
-            'SiOv2': 42820.582,
-            'SiOv3': 42519.373,
-            '29SiOv0': 42879.916,
-            '30SiOv0': 42373.359,
-            'SiS': 18154.880,
-        }
-        maser_list = [x for x in maser_dict.values() if min(freq1,freq2) < x < max(freq1,freq2)]
+        def freq_to_vel(rest_freq, obs_freq):
+            c_kms = 2.99792458e5
+            return ((rest_freq - obs_freq) / rest_freq) * c_kms
 
-        if len(maser_list) > 0:
-            return True
+        maser_dict = {
+            'OH (1)': 1612231000,
+            'OH (2)': 1665401800,
+            'OH (3)': 1667359000,
+            'OH (4)': 1720530000,
+            'H2O': 22235080000,
+            'CH3OH (1)': 6668519200,
+            'CH3OH (2)': 1217859700,
+            'SiOv0': 43423858000,
+            'SiOv1': 43122079000,
+            'SiOv2': 42820582000,
+            'SiOv3': 42519373000,
+            '29SiOv0': 42879916000,
+            '30SiOv0': 42373359000,
+            'SiS': 18154880000,
+        }
+
+        qaTool = casa_tools.quanta
+        suTool = casa_tools.synthesisutils
+
+        for ms in self.inputs.context.observing_run.measurement_sets:
+            if ms.name == self.inputs.vis:
+                ms_info = ms
+        
+        spw_info = ms_info.get_spectral_window(spw)
+        freq_low = spw_info._min_frequency.convert_to(newUnits=FrequencyUnits.HERTZ).value
+        freq_high = spw_info._max_frequency.convert_to(newUnits=FrequencyUnits.HERTZ).value
+        if spw_info._ref_frequency_frame != 'TOPO':
+            LOG.info("Spectral window reference frame not TOPO. Skipping maser detection.")
+            return False
+
+        to_lsrk = suTool.advisechansel(msname=ms_info.name, spwselection=spw, getfreqrange=True, freqframe='LSRK')
+        freq_low = float(qaTool.getvalue(qaTool.convert(to_lsrk['freqstart'], 'Hz')))
+        freq_high = float(qaTool.getvalue(qaTool.convert(to_lsrk['freqend'], 'Hz')))
+
+        for value in maser_dict.values():
+            vel_low = freq_to_vel(value, freq_low)
+            vel_high = freq_to_vel(value, freq_high)
+            if abs(vel_low) <= 200 or abs(vel_high) <= 200:
+                LOG.info("Maser line possible in spw {}. Hanning smoothing will be applied.".format(spw))
+                return True
         return False
+
+    def _track_hsmooth(self, spws: list):
+        LOG.info("Writing Hanning smoothing information to SPECTRAL_WINDOW table of MS.")
+        desc = {'OFFLINE_HANNING_SMOOTH': {'comment': 'Offline Hanning Smooth Flag',
+                                   'dataManagerGroup': 'StandardStMan',
+                                   'dataManagerType': 'StandardStMan',
+                                   'keywords': {"SPW_IDs": spws},
+                                   'maxlen': 0,
+                                   'option': 0,
+                                   'valueType': 'int'}}
+        with casa_tools.TableReader(self.inputs.vis + "/SPECTRAL_WINDOW", nomodify=False) as tb:
+            tb.addcols(desc)
