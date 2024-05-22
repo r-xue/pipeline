8c40b36af9c43bec429b4961b157c6759e2f71a4
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2024-05-22 13:21:54 +0900

    PIPE-2179 use astropy.time.Time instead of qa.splitdate
    Refactored relevant functions to centralize conversion from MJD to datetime.
    
    Also added unit test for hsd/tasks/common/utils.py and hsd/tasks/importdata/reader.py

---
+++ pipeline/hsd/tasks/common/utils_test.py
@@ -0,0 +1,49 @@
+"""
+utils_test.py : Unit tests for "hsd/tasks/common/utils.py".
+
+Unit tests for "hsd/tasks/common/utils.py"
+"""
+import datetime
+
+import pytest
+
+from .utils import mjd_to_datetime, mjd_to_datestring
+
+test_cases = [
+    (60414, datetime.datetime(2024, 4, 14, 0, 0, 0)),
+    (60414.9999999999, datetime.datetime(2024, 4, 14, 23, 59, 59, 999991)),
+    (60414.999999999985, datetime.datetime(2024, 4, 14, 23, 59, 59, 999999))
+]
+
+
+@pytest.mark.parametrize('mjd, expected', test_cases)
+def test_mjd_to_datetime(mjd: float, expected: datetime.datetime):
+    result = mjd_to_datetime(mjd)
+    assert result.year == expected.year
+    assert result.month == expected.month
+    assert result.day == expected.day
+    assert result.hour == expected.hour
+    assert result.minute == expected.minute
+    assert result.second == expected.second
+    assert result.microsecond == expected.microsecond
+
+
+test_cases = [
+    (60414, 'Sun Apr 14 00:00:00 2024 UTC'),
+    (60414.9999999999, 'Sun Apr 14 23:59:59 2024 UTC'),
+    (60414.999999999985, 'Sun Apr 14 23:59:59 2024 UTC')
+]
+
+
+@pytest.mark.parametrize('mjd, expected', test_cases)
+def test_mjd_to_datestring(mjd: float, expected: str):
+    result = mjd_to_datestring(mjd, 'd')
+    assert result == expected
+
+    mjd_sec = mjd * 86400
+    result = mjd_to_datestring(mjd_sec, 's')
+    assert result == expected
+
+    # default unit is 's'
+    result = mjd_to_datestring(mjd_sec)
+    assert result == expected
---
+++ pipeline/hsd/tasks/importdata/reader_test.py
@@ -0,0 +1,34 @@
+"""
+reader_test.py : Unit tests for "hsd/tasks/importdata/reader.py".
+
+Unit tests for "hsd/tasks/importdata/reader.py"
+"""
+import pytest
+
+from .reader import mjdsec2str
+
+test_cases = [
+    (60414 * 86400, '2024/4/14/0:0:0.0000000'),
+    (60414.9999999999 * 86400, '2024/4/14/23:59:59.9999912'),
+    (60414.999999999985 * 86400, '2024/4/14/23:59:59.9999987')
+]
+
+
+@pytest.mark.parametrize('mjd, expected', test_cases)
+def test_mjdsec2str(mjd: float, expected: str):
+    result = mjdsec2str(mjd)
+    print(f'result {result} expected {expected}')
+    # split string into two parts: year~second and microsecond
+    # year~second: must be identical
+    # microsecond: should be compared as float value with 1e-6 accuracy
+    result_datetime, result_microsec = result.split('.')
+    expected_datetime, expected_microsec = expected.split('.')
+
+    assert result_datetime == expected_datetime
+    result_usec = float(f'0.{result_microsec}')
+    expected_usec = float(f'0.{expected_microsec}')
+    if expected_usec == 0:
+        assert result_usec == expected_usec
+    else:
+        diff = abs((result_usec - expected_usec) / expected_usec)
+        assert diff < 1e-6
