a6305a7b84be0bed39832ac85f3791bcdf62b7c2
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2024-05-23 12:00:08 +0900

    refactoring unit test

--- pipeline/hsd/tasks/importdata/reader_test.py
+++ pipeline/hsd/tasks/importdata/reader_test.py
@@ -8,17 +8,18 @@ import pytest
 from .reader import mjdsec2str
 
 test_cases = [
-    (56839.91646527777 * 86400, '2014/7/1/21:59:42.599999'),
-    (56839.91647013888 * 86400, '2014/7/1/21:59:43.019999'),
-    (60414 * 86400, '2024/4/14/0:0:0.0000000'),
-    (60414.9999999999 * 86400, '2024/4/14/23:59:59.9999912'),
-    (60414.999999999985 * 86400, '2024/4/14/23:59:59.9999987')
+    (56839.91646527777, '2014/7/1/21:59:42.599999'),
+    (56839.91647013888, '2014/7/1/21:59:43.019999'),
+    (60414, '2024/4/14/0:0:0.0000000'),
+    (60414.9999999999, '2024/4/14/23:59:59.9999912'),
+    (60414.999999999985, '2024/4/14/23:59:59.9999987')
 ]
 
 
 @pytest.mark.parametrize('mjd, expected', test_cases)
 def test_mjdsec2str(mjd: float, expected: str):
-    result = mjdsec2str(mjd)
+    mjdsec = mjd * 86400
+    result = mjdsec2str(mjdsec)
     print(f'result {result} expected {expected}')
     # split string into two parts: year~second and microsecond
     # year~second: must be identical
