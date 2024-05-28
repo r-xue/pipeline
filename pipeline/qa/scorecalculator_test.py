4ef55a78871ae879b0cc1eee1a8cfb1e3ace7a77
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2024-05-28 15:06:07 +0900

    refactoring test code

--- pipeline/qa/scorecalculator_test.py
+++ pipeline/qa/scorecalculator_test.py
@@ -36,76 +36,75 @@ def test_line_wider_than(lines: List[Tuple[int, int]], nchan: int, fraction: flo
     assert is_wider == expected
 
 
-test_cases_edge = [
-    # line not extend to the edge
-    ([(1020, 1030)], 2048, 1.0),
-    ([(1, 11)], 1024, 1.0),
-    ([(800, 1022)], 1024, 1.0),
-    # too narrow edge lines
-    ([(0, 10)], 1024, 1.0),
-    ([(0, 203)], 1024, 1.0),
-    ([(990, 1023)], 1024, 1.0),
-    ([(-10, 200)], 1024, 1.0),
-    ([(900, 2047)], 1024, 1.0),
-    # wide edge line that lowers QA score
-    ([(0, 204)], 1024, 0.65),
-    ([(800, 1023)], 1024, 0.65),
-    ([(800, 1024)], 1024, 0.65),
-    ([(0, 1023)], 1024, 0.65),
-    ([(-10, 210)], 1024, 0.65),
-    ([(800, 2047)], 1024, 0.65)
-]
-
-
-@pytest.mark.parametrize('lines, nchan, expected', test_cases_edge)
+@pytest.mark.parametrize(
+    'lines, nchan, expected',
+    [
+        # line not extend to the edge
+        ([(1020, 1030)], 2048, 1.0),
+        ([(1, 11)], 1024, 1.0),
+        ([(800, 1022)], 1024, 1.0),
+        # too narrow edge lines
+        ([(0, 10)], 1024, 1.0),
+        ([(0, 203)], 1024, 1.0),
+        ([(990, 1023)], 1024, 1.0),
+        ([(-10, 200)], 1024, 1.0),
+        ([(900, 2047)], 1024, 1.0),
+        # wide edge line that lowers QA score
+        ([(0, 204)], 1024, 0.65),
+        ([(800, 1023)], 1024, 0.65),
+        ([(800, 1024)], 1024, 0.65),
+        ([(0, 1023)], 1024, 0.65),
+        ([(-10, 210)], 1024, 0.65),
+        ([(800, 2047)], 1024, 0.65)
+    ]
+)
 def test_score_sd_edge_lines(lines: List[Tuple[int, int]], nchan: int, expected: float):
     score = qacalc.score_sd_edge_lines(lines, nchan)
     assert score == expected
 
 
-test_cases_wide = [
-    # single narrow line
-    ([(1020, 1030)], 2048, 1.0),
-    ([(500, 840)], 1024, 1.0),
-    ([(1000, 1681)], 2048, 1.0),
-    # single wide line that lowers QA score
-    ([(0, 1023)], 1024, 0.65),
-    ([(0, 2047)], 1024, 0.65),
-    ([(500, 841)], 1024, 0.65),
-    ([(1000, 1682)], 2048, 0.65),
-    # multiple lines, less coverage than threshold
-    ([(100, 199), (300, 399), (500, 599), (700, 740)], 1024, 1.0),
-    # multiple lines, enough coverage to lower QA score
-    ([(100, 199), (300, 399), (500, 599), (700, 741)], 1024, 0.65)
-]
-
-
-@pytest.mark.parametrize('lines, nchan, expected', test_cases_wide)
+@pytest.mark.parametrize(
+    'lines, nchan, expected',
+    [
+        # single narrow line
+        ([(1020, 1030)], 2048, 1.0),
+        ([(500, 840)], 1024, 1.0),
+        ([(1000, 1681)], 2048, 1.0),
+        # single wide line that lowers QA score
+        ([(0, 1023)], 1024, 0.65),
+        ([(0, 2047)], 1024, 0.65),
+        ([(500, 841)], 1024, 0.65),
+        ([(1000, 1682)], 2048, 0.65),
+        # multiple lines, less coverage than threshold
+        ([(100, 199), (300, 399), (500, 599), (700, 740)], 1024, 1.0),
+        # multiple lines, enough coverage to lower QA score
+        ([(100, 199), (300, 399), (500, 599), (700, 741)], 1024, 0.65)
+    ]
+)
 def test_score_sd_wide_lines(lines: List[Tuple[int, int]], nchan: int, expected: float):
     score = qacalc.score_sd_wide_lines(lines, nchan)
     assert score == expected
 
 
-test_cases_qa_msg = [
-    # narrow line at the middle
-    ([(1019, 1029)], 2048, 1.0, 'Successfully detected spectral lines'),
-    # narrow edge line
-    ([(0, 10)], 1024, 1.0, 'Successfully detected spectral lines'),
-    # edge line with enough coverage to lower QA score
-    ([(0, 220)], 1024, 0.65, 'An edge line is detected.'),
-    # wide line with enough coverage to lower QA score
-    ([(200, 600)], 1024, 0.65, 'A wide line is detected.'),
-    # multiple narrow lines with enough coverage *in total* to lower QA score
-    ([(100, 200), (300, 400), (500, 600), (700, 742)], 1024, 0.65, 'A wide line is detected.'),
-    # wide edge lines that can violate both criteria
-    ([(0, 1023)], 1024, 0.65, 'An edge line is detected. A wide line is detected.'),
-    # no valid line, no QA score
-    ([], 2048, None, 'N/A'),
-
-]
-
-
-@pytest.mark.parametrize('lines, nchan, expected_score, expected_msg', test_cases_qa_msg)
+@pytest.mark.parametrize(
+    'lines, nchan, expected_score, expected_msg',
+    [
+        # narrow line at the middle
+        ([(1019, 1029)], 2048, 1.0, 'Successfully detected spectral lines'),
+        # narrow edge line
+        ([(0, 10)], 1024, 1.0, 'Successfully detected spectral lines'),
+        # edge line with enough coverage to lower QA score
+        ([(0, 220)], 1024, 0.65, 'An edge line is detected.'),
+        # wide line with enough coverage to lower QA score
+        ([(200, 600)], 1024, 0.65, 'A wide line is detected.'),
+        # multiple narrow lines with enough coverage *in total* to lower QA score
+        ([(100, 200), (300, 400), (500, 600), (700, 742)], 1024, 0.65, 'A wide line is detected.'),
+        # wide edge lines that can violate both criteria
+        ([(0, 1023)], 1024, 0.65, 'An edge line is detected. A wide line is detected.'),
+        # no valid line, no QA score
+        ([], 2048, None, 'N/A'),
+    ]
+)
 def test_score_sd_lines_overall(lines: List[Tuple[int, int]], nchan: int, expected_score: Optional[float], expected_msg: str):
     field_name = 'M100'
     spw_id = [17]
