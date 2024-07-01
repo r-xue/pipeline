ab2f624c7a55f7519a9f0b88f46c347629ac37fe
Rui Xue <rxue@nrao.edu>
2024-07-01 03:07:35 -0500

    PIPE-1820: add the "deconvolver" methods into the Pipeline selfcal imaging heuristics subclasses.

--- pipeline/hif/heuristics/imageparams_alma_scal.py
+++ pipeline/hif/heuristics/imageparams_alma_scal.py
@@ -2,53 +2,46 @@ from .imageparams_alma import ImageParamsHeuristicsALMA
 
 
 class ImageParamsHeuristicsALMAScal(ImageParamsHeuristicsALMA):
+    """Class for determining image parameters heuristics for ALMA-SCAL.
+
+    Inherits from ImageParamsHeuristicsALMA and offers specialized heuristics 
+    for the VLA-SCAL imaging mode. This class facilitates the initialization 
+    of Self-calibration CleanTarget objects used by the hif_selfcal(). 
+    It cannot be used directly with the hif_makeimages or hif_tclean.
+
+    Attributes:
+        imaging_mode (str): The imaging mode, set to 'ALMA-SCAL'.
+        selfcal (bool): A flag indicating if self-calibration is enabled.
+    """
 
     def __init__(self, vislist, spw, observing_run, imagename_prefix='', proj_params=None, contfile=None,
                  linesfile=None, imaging_params={}):
+        """Initializes the class with the provided parameters.
+
+        Args:
+            vislist (list): List of visibility data.
+            spw (str): Spectral window.
+            observing_run (str): Observing run identifier.
+            imagename_prefix (str, optional): Prefix for the image name. Defaults to ''.
+            proj_params (dict, optional): Project parameters. Defaults to None.
+            contfile (str, optional): Continuum file. Defaults to None.
+            linesfile (str, optional): Lines file. Defaults to None.
+            imaging_params (dict, optional): Imaging parameters. Defaults to {}.
+        """
         super().__init__(vislist, spw, observing_run, imagename_prefix, proj_params, contfile, linesfile, imaging_params)
         self.imaging_mode = 'ALMA-SCAL'
         self.selfcal = True
 
-    def keep_iterating(self, iteration, hm_masking, tclean_stopcode, dirty_dynamic_range, residual_max,
-                       residual_robust_rms, field, intent, spw, specmode):
-        """Determine if another tclean iteration is necessary."""
-        if iteration in [0, 1, 2]:
-            return True, hm_masking
-        else:
-            return False, hm_masking
+    def deconvolver(self, specmode, spwspec, intent: str = '', stokes: str = '') -> str:
+        """Returns the deconvolver type for ALMA-SCAL.
 
-    def get_autobox_params(self, iteration, intent, specmode, robust):
-        """Default auto-boxing parameters."""
+        Args:
+            specmode (str): Spectral mode.
+            spwspec (str): Spectral window specification.
+            intent (str, optional): Observation intent. Defaults to ''.
+            stokes (str, optional): Stokes parameters. Defaults to ''.
 
-        sidelobethreshold = None
-        noisethreshold = None
-        lownoisethreshold = None
-        negativethreshold = None
-        minbeamfrac = None
-        growiterations = None
-        dogrowprune = None
-        minpercentchange = None
-        fastnoise = None
-
-        return (sidelobethreshold, noisethreshold, lownoisethreshold, negativethreshold, minbeamfrac, growiterations,
-                dogrowprune, minpercentchange, fastnoise)
-
-    def is_selfcal_iteration(self, iteration):
-        """"Determine if we need to start the selfcal-solint inner-iteration sequence.
-
-        In the selfcal imaging sequence.
-        iter1: initial image: unselfcal imageing
-        iter2: i.e. the selfcal lopp
-            *solin_prior*iter2.image: imaging for creating selfcal/solint model
-            *solin_post*iter2.image: imaging for selfcal/solint QA
-        iter3: final image: imaging after selfcal apply
+        Returns:
+            str: The deconvolver type.
         """
-        if iteration == 2:
-            return True
-        else:
-            return False
-
-    # def copy_from_last(self,iteration):
-    #     """Copy the last iteration's products at the beginning of the current iteration."""
-    #     if iteration >=2 and
-    #     return False
+        return 'mtmfs'
