87e374a30c75dc5ffc6d7f7006fe3ff6875689a5
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2024-07-03 13:25:26 +0900

    add test cases for non-zero edge parameters

--- pipeline/qa/scorecalculator_test.py
+++ pipeline/qa/scorecalculator_test.py
@@ -23,50 +23,59 @@ def test_get_line_ranges(lines: List[Tuple[int, int, bool]], expected: List[Tupl
 
 
 @pytest.mark.parametrize(
-    'lines, nchan, expected',
+    'lines, nchan, edge, expected',
     [
         # line not extend to the edge
-        ([(1020, 1030)], 2048, False),
-        ([(1, 11)], 1024, False),
-        ([(800, 1022)], 1024, False),
+        ([(1020, 1030)], 2048, (0, 0), False),
+        ([(1, 11)], 1024, (0, 0), False),
+        ([(800, 1022)], 1024, (0, 0), False),
         # narrow edge lines
-        ([(0, 10)], 1024, True),
-        ([(0, 203)], 1024, True),
-        ([(990, 1023)], 1024, True),
-        ([(-10, 200)], 1024, True),
-        ([(900, 2047)], 1024, True),
+        ([(0, 10)], 1024, (0, 0), True),
+        ([(0, 203)], 1024, (0, 0), True),
+        ([(990, 1023)], 1024, (0, 0), True),
+        ([(-10, 200)], 1024, (0, 0), True),
+        ([(900, 2047)], 1024, (0, 0), True),
         # wide edge lines
-        ([(0, 204)], 1024, True),
-        ([(800, 1023)], 1024, True),
-        ([(800, 1024)], 1024, True),
-        ([(0, 1023)], 1024, True),
-        ([(-10, 210)], 1024, True),
-        ([(800, 2047)], 1024, True)
+        ([(0, 204)], 1024, (0, 0), True),
+        ([(800, 1023)], 1024, (0, 0), True),
+        ([(800, 1024)], 1024, (0, 0), True),
+        ([(0, 1023)], 1024, (0, 0), True),
+        ([(-10, 210)], 1024, (0, 0), True),
+        ([(800, 2047)], 1024, (0, 0), True),
+        # edge excuded
+        ([(2, 11)], 1024, (2, 0), True),
+        ([(2, 11)], 1024, (1, 0), False),
+        ([(800, 1021)], 1024, (0, 2), True),
+        ([(800, 1021)], 1024, (0, 1), False),
     ]
 )
-def test_test_sd_edge_lines(lines: List[Tuple[int, int]], nchan: int, expected: float):
-    score = qacalc.test_sd_edge_lines(lines, nchan)
+def test_test_sd_edge_lines(lines: List[Tuple[int, int]], nchan: int, edge: Tuple[int, int], expected: float):
+    score = qacalc.test_sd_edge_lines(lines, nchan, edge)
     assert score == expected
 
 
 @pytest.mark.parametrize(
-    'lines, nchan, expected',
+    'lines, nchan, edge, expected',
     [
         # single narrow line
-        ([(1020, 1030)], 2048, False),
-        ([(500, 840)], 1024, False),
-        ([(1000, 1681)], 2048, False),
+        ([(1020, 1030)], 2048, (0, 0), False),
+        ([(500, 840)], 1024, (0, 0), False),
+        ([(1000, 1681)], 2048, (0, 0), False),
         # single wide line that lowers QA score
-        ([(0, 1023)], 1024, True),
-        ([(0, 2047)], 1024, True),
-        ([(500, 841)], 1024, True),
-        ([(1000, 1682)], 2048, True),
+        ([(0, 1023)], 1024, (0, 0), True),
+        ([(0, 2047)], 1024, (0, 0), True),
+        ([(500, 841)], 1024, (0, 0), True),
+        ([(1000, 1682)], 2048, (0, 0), True),
         # multiple lines, less coverage than threshold
-        ([(100, 199), (300, 399), (500, 599), (700, 740)], 1024, False),
+        ([(100, 199), (300, 399), (500, 599), (700, 740)], 1024, (0, 0), False),
         # multiple lines, enough coverage to lower QA score
-        ([(100, 199), (300, 399), (500, 599), (700, 741)], 1024, True)
+        ([(100, 199), (300, 399), (500, 599), (700, 741)], 1024, (0, 0), True),
+        # edge excluded
+        ([(501, 840)], 1024, (3, 2), True),
+        ([(501, 840)], 1024, (5, 0), True),
+        ([(501, 840)], 1024, (0, 5), True),
     ]
 )
-def test_test_sd_wide_lines(lines: List[Tuple[int, int]], nchan: int, expected: float):
-    score = qacalc.test_sd_wide_lines(lines, nchan)
+def test_test_sd_wide_lines(lines: List[Tuple[int, int]], nchan: int, edge: Tuple[int, int], expected: float):
+    score = qacalc.test_sd_wide_lines(lines, nchan, edge)
     assert score == expected
