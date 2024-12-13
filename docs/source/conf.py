294a4823e3c24d01a04c84410c1f3f1a854ff0ed
Rui Xue <rxue@nrao.edu>
2024-12-13 07:26:25 -0600

    PIPE-1883: switch back to the default Pygments style.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -150,7 +150,7 @@ exclude_patterns = ['_build', '**.ipynb_checkpoints', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
 # pygments_style = 'sphinx'
-pygments_style = 'color'
+pygments_style = 'default'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
