67aee588cdf3907abc27c383dab0e5792e3f33ee
Shawn Booth <sbooth@nrao.edu>
2024-12-16 18:27:44 -0500

    PIPE-2383: add logic to spectral line window detection for L- and S-band observations to restrict the detection criteria; changed the logging messages in specline_detect.py to use % notation for optimization

--- pipeline/hifv/heuristics/specline_detect.py
+++ pipeline/hifv/heuristics/specline_detect.py
@@ -24,7 +24,8 @@ def detect_spectral_lines(mset: Type[MeasurementSet], specline_spws: str='auto')
     spws = mset.get_spectral_windows(science_windows_only=True)
     spec_windows = list()
     if specline_spws == 'auto':
-        LOG.info("Spectral line detection set to auto. The pipeline will determine which science spws will be used for spectral line analysis.")
+        LOG.info("Spectral line detection set to auto. "
+                 "The pipeline will determine which science spws will be used for spectral line analysis.")
         for spw in spws:
             _auto_detector(spw)
     elif specline_spws == 'none':
@@ -34,41 +35,51 @@ def detect_spectral_lines(mset: Type[MeasurementSet], specline_spws: str='auto')
         spec_windows = [spw.id for spw in spws]
     else:
         spec_windows = range_to_list(specline_spws)
-        LOG.debug('User-defined spectral windows for spectral line analysis: {}'.format(spec_windows))
+        LOG.debug('User-defined spectral windows for spectral line analysis: %s' % spec_windows)
         if not all([isinstance(x, int) for x in spec_windows]):
-            msg = "Invalid input for user-defined spws: {}".format(spec_windows)
+            msg = "Invalid input for user-defined spws: %s" % spec_windows
             LOG.error(msg)
             raise Exception(msg)
         LOG.info("Spectral line assignment defined by user.")
         non_sci = list(set(spec_windows).difference([x.id for x in spws]))
         if non_sci:
             message = commafy([str(x) for x in non_sci], quotes=False)
-            LOG.info(f"Non-science windows {message} defined by the user will be skipped.")
+            LOG.info("Non-science windows %s defined by the user will be skipped." % message)
             spec_windows = list(set(spec_windows).intersection([x.id for x in spws]))
             specline_spws = find_ranges(spec_windows)
-        LOG.info('The user defined the following spws for spectral line analysis: {}. '
-                    'All other spws will be regarded as continuum.'.format(specline_spws))
+        LOG.info('The user defined the following spws for spectral line analysis: %s. '
+                 'All other spws will be regarded as continuum.' % specline_spws)
     for spw in spws:
         if int(spw.id) in spec_windows:
             spw.specline_window = True
 
 def _auto_detector(spw: Type[SpectralWindow]) -> None:
-    """Determines if a spectral window should be designated as a spectral line window using the following criteria:
+    """Determines if a spectral window should be designated as a spectral line window using the following logic:
        - frequency above L-band (greater than 1GHz) and
-       - window more narrow than 64MHz or more than 128 channels or channel widths less than 0.5 MHz
+         - L- or S-band and window more narrow than 32 MHz and more than 64 channels or
+         - window more narrow than 64MHz or more than 128 channels or channel widths less than 0.5 MHz
 
     Args:
         spw(object): spectral window object stored in pipeline context
     """
 
     spectral_line_spw = False
+    spw_type = "continuum"
     if spw.min_frequency.value < 1000000000.0:
-        LOG.info("Frequencies below 1GHz are not currently supported. Regarding spw {} as a continuum window.".format(spw.id))
+        LOG.info("Frequencies below 1GHz are not currently supported. Regarding spw %s as a continuum window." % spw.id)
     else:
-        if spw.bandwidth.value < 64000000.0 or spw.num_channels > 128 or (spw.bandwidth.value / spw.num_channels) < 500000.0:
-            LOG.info("Spw {} has been identified as a spectral line window.".format(spw.id))
-            spectral_line_spw = True
+        if spw.band.split("(")[-1].split(")")[0] in ["L", "S"]:
+            if all([spw.bandwidth.value < 32000000.0,
+                    spw.num_channels > 64]):
+                LOG.debug("New criteria met for spw %d." % spw.id)
+                spectral_line_spw = True
+                spw_type = "spectral line"
         else:
-            LOG.info("Spw {} has been identified as a continuum window.".format(spw.id))
+            if any([spw.bandwidth.value < 64000000.0,
+                    spw.num_channels > 128,
+                    (spw.bandwidth.value / spw.num_channels) < 500000.0]):
+                spectral_line_spw = True
+                spw_type = "spectral line"
+        LOG.info("Spw %d has been identified as a %s window." % (spw.id, spw_type))
     spw.specline_window = spectral_line_spw
         
