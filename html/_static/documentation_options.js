310a6e9a5d86a3a63e934fb04605931f4b532f64
r-xue <r-xue@users.noreply.github.com>
2024-12-17 22:13:10 +0000

    deploy: c87c9e7864f7d15d6e445a6271d8df7915bdf48e

--- html/_static/documentation_options.js
+++ html/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '2024.2.0.4+166-ga97fc6e53-PIPE-1669-run-dev-pipeline-with-modular-casa6',
+    VERSION: '2024.2.0.4+169-gc87c9e786-PIPE-1669-run-dev-pipeline-with-modular-casa6',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
