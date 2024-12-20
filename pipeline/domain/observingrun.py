05163229aee5e340c517f1eb864defdb71aa79ce
Vincent Geers <vgeers@nrao.edu>
2024-12-20 10:31:58 +0000

    PIPE-2160: remove unused "get_fields" method from pipeline.domain.observingrun.ObservingRun.

--- pipeline/domain/observingrun.py
+++ pipeline/domain/observingrun.py
@@ -14,7 +14,6 @@ from . import MeasurementSet
 
 if TYPE_CHECKING:
     from datetime import datetime
-    from pipeline.domain.field import Field
 
 LOG = infrastructure.get_logger(__name__)
 
@@ -252,33 +251,6 @@ class ObservingRun(object):
                 return ms_dict, dtype
             else:
                 return collections.OrderedDict(), None
-
-    # TODO: appears unused, remove?
-    def get_fields(self, names: str | None = None) -> list[Field]:
-        """
-        Returns fields matching the given arguments from all measurement sets in
-        this observing run.
-
-        Args:
-            names: Field name(s) to match. If None, it will match all field(s) found.
-
-        Returns:
-            List of Field objects for fields in all measurements sets in this
-            observing run, filtered by given field names.
-        """
-        match = [ms.fields for ms in self.measurement_sets]
-        # flatten the fields lists to one sequence
-        match = itertools.chain(*match)
-
-        if names is not None:
-            if isinstance(names, str):
-                names = utils.safe_split(names)
-            names = set(names)
-            match = [f for f in match if f.name in names]
-        else:
-            match = list(match)
-
-        return match
 
     @staticmethod
     def get_real_spw_id_by_name(spw_name: str | None, target_ms: MeasurementSet) -> int | None:
