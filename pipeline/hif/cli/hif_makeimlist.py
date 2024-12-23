r-xue/pipeline

commit 5311c346d8ce83dcc824017fa15a91b08428bc88

PIPE-2230: expose `allow_wproject` in the CLI interface of hif_makeimlist and hif_selfcal.

pipeline/hif/cli/hif_makeimlist.py
@@ -11,7 +11,7 @@ def hif_makeimlist(vis=None, imagename=None, intent=None, field=None,
                    nchan=None, start=None, width=None, nbins=None,
                    robust=None, uvtaper=None, clearlist=None, per_eb=None,
                    per_session=None, calcsb=None, datatype=None, datacolumn=None,
-                   parallel=None):
+                   allow_wproject, parallel=None):
 
     """
     hif_makeimlist ---- Compute list of clean images to be produced
@@ -147,6 +147,7 @@ def hif_makeimlist(vis=None, imagename=None, intent=None, field=None,
                             'REGCAL_LINE_SCIENCE,selfcal_line_science'
     datacolumn    Data column to image. Only to be used for manual overriding
                   when the automatic choice by data type is not appropriate.
+    allow_wproject Allow the wproject heuristics for imaging
     parallel      Use MPI cluster where possible
 
     --------- examples -----------------------------------------------------------

pipeline/hif/cli/hif_selfcal.py
@@ -10,7 +10,7 @@ def hif_selfcal(vis=None, field=None, spw=None, contfile=None,
                 minsnr_to_proceed=None, delta_beam_thresh=None,
                 apply_cal_mode_default=None, rel_thresh_scaling=None,
                 dividing_factor=None, check_all_spws=None, inf_EB_gaincal_combine=None,
-                usermask=None, usermodel=None,
+                usermask=None, usermodel=None, allow_wproject=None,
                 parallel=None):
     """
     hif_selfcal  ---- Determine and apply self-calibration with the science target data

pipeline/hif/heuristics/auto_selfcal/auto_selfcal.py
@@ -56,6 +56,7 @@ class SelfcalHeuristics(object):
         self.phasecenter = scal_target['phasecenter']  # explictly set phasecenter for now
         self.spw_virtual = scal_target['spw']
         self.gridder = scal_target['gridder']
+        self.wprojplanes= scal_target['wprojplanes']
         self.vislist = scal_target['sc_vislist']
         self.parallel = scal_target['sc_parallel']
         self.telescope = scal_target['sc_telescope']
@@ -819,7 +820,7 @@ class SelfcalHeuristics(object):
                             LOG.info('WARNING SPW '+spw+' HAS A >0.05 CHANGE IN BEAM AREA POST SELFCAL')
 
         # attached solints to selfcal_library as a short-term workaround
-        
+
         band=bands[0]
         target=all_targets[0]
         selfcal_library[target][band]['solints'] = solints

pipeline/hif/heuristics/imageparams_vla.py
@@ -264,8 +264,9 @@ class ImageParamsHeuristicsVLA(ImageParamsHeuristics):
             gridder_select = 'mosaic'
 
         # PIPE-1641: switch to gridder='wproject' for L and S band sci-target imaging
-        # PIPE-2225: disable the L/S-band wproject heuristics for efficient imaging
-        use_wproject = False
+        # PIPE-2225/2230: disable the L/S-band wproject heuristics for efficient imaging by default,
+        # but expose it as an option (allow_wproject)
+        use_wproject = self.imaging_params.get('allow_wproject', False)
         if use_wproject:
             vla_band = self._get_vla_band(spwspec)
             if vla_band in ['L', 'S'] and 'TARGET' in intent:

pipeline/hif/tasks/makeimlist/cleantarget.py
@@ -21,6 +21,7 @@ class CleanTarget(dict):
         self['psf_phasecenter'] = None      # string
         self['specmode'] = None             # string
         self['gridder'] = None              # string
+        self['wprojplanes'] = None          # int
         self['datacolumn'] = None           # string
         self['datatype'] = None             # string
         self['datatype_info'] = None        # string

pipeline/hif/tasks/makeimlist/makeimlist.py
@@ -44,6 +44,7 @@ class MakeImListInputs(vdp.StandardInputs):
     parallel = vdp.VisDependentProperty(default='automatic')
     robust = vdp.VisDependentProperty(default=None)
     uvtaper = vdp.VisDependentProperty(default=None)
+    allow_wproject = vdp.VisDependentProperty(default=False)
 
     # properties requiring some processing or MS-dependent logic -------------------------------------------------------
 
@@ -184,7 +185,7 @@ class MakeImListInputs(vdp.StandardInputs):
                  contfile=None, linesfile=None, uvrange=None, specmode=None, outframe=None, hm_imsize=None,
                  hm_cell=None, calmaxpix=None, phasecenter=None, psf_phasecenter=None, nchan=None, start=None, width=None, nbins=None,
                  robust=None, uvtaper=None, clearlist=None, per_eb=None, per_session=None, calcsb=None, datatype=None,
-                 datacolumn=None, parallel=None, known_synthesized_beams=None, scal=False):
+                 datacolumn=None, parallel=None, known_synthesized_beams=None, allow_wproject=False, scal=False):
         self.context = context
         self.output_dir = output_dir
         self.vis = vis
@@ -217,6 +218,7 @@ class MakeImListInputs(vdp.StandardInputs):
         self.datacolumn = datacolumn
         self.parallel = parallel
         self.known_synthesized_beams = known_synthesized_beams
+        self.allow_wproject = allow_wproject
         self.scal = scal
 
 
@@ -1301,6 +1303,7 @@ class MakeImList(basetask.StandardTaskTemplate):
                                                                                    local_selected_datatype_str, target_heuristics)
 
                                 reffreq = target_heuristics.reffreq(deconvolver, inputs.specmode, spwsel)
+                                target_heuristics.imaging_params['allow_wproject'] = inputs.allow_wproject
                                 gridder = target_heuristics.gridder(field_intent[1], field_intent[0], spwspec=actual_spwspec)
 
                                 # Get field-specific uvrange value
@@ -1332,6 +1335,7 @@ class MakeImList(basetask.StandardTaskTemplate):
                                     psf_phasecenter=psf_phasecenters[field_intent[0]],
                                     specmode=inputs.specmode,
                                     gridder=gridder,
+                                    wprojplanes=target_heuristics.wprojplanes(gridder=gridder, spwspec=actual_spwspec),
                                     imagename=imagename,
                                     start=inputs.start,
                                     width=widths[(field_intent[0], spwspec)],

pipeline/hif/tasks/selfcal/selfcal.py
@@ -156,14 +156,15 @@ class SelfcalInputs(vdp.StandardInputs):
     #   usermodel={'IRAS32':['IRAS32-model.tt0','IRAS32-model.tt1'], 'IRS5N':['IRS5N-model.tt0','IRS5N-model.tt1']}
     #   usermodel={'IRAS32':{'Band_6':['IRAS32-model.tt0','IRAS32-model.tt1']}, 'IRS5N':{'Band_6':['IRS5N-model.tt0','IRS5N-model.tt1']}}
     # for clean targets not found in the (nested) dictionary structure (source->band), the usermask value will be empty (default)
-
+    
+    allow_wproject = vdp.VisDependentProperty(default=False)
     restore_resources = vdp.VisDependentProperty(default=None)
 
     def __init__(self, context, vis=None, field=None, spw=None, contfile=None, n_solints=None,
                  amplitude_selfcal=None, gaincal_minsnr=None, refantignore=None,
                  minsnr_to_proceed=None, delta_beam_thresh=None, apply_cal_mode_default=None,
                  rel_thresh_scaling=None, dividing_factor=None, check_all_spws=None, inf_EB_gaincal_combine=None,
-                 usermask=None, usermodel=None,
+                 usermask=None, usermodel=None, allow_wproject=None,
                  apply=None, parallel=None, recal=None, restore_resources=None):
         super().__init__()
         self.context = context
@@ -189,6 +190,7 @@ class SelfcalInputs(vdp.StandardInputs):
         self.restore_resources = restore_resources
         self.usermask = usermask
         self.usermodel = usermodel
+        self.allow_wproject = allow_wproject
 
 
 @task_registry.set_equivalent_casa_task('hif_selfcal')
@@ -645,6 +647,7 @@ class Selfcal(basetask.StandardTaskTemplate):
                                               scal=scal, contfile=self.inputs.contfile,
                                               field=self.inputs.field,
                                               spw=self.inputs.spw,
+                                              allow_wproject=self.inputs.allow_wproject,
                                               parallel=self.inputs.parallel)
         makeimlist_task = MakeImList(makeimlist_inputs)
         makeimlist_results = makeimlist_task.execute()

commit 12bea2073247acc7aa387d69c26a60993699041d

PIPE-2230: fix a bug introduced from 5311c346.

pipeline/hif/cli/hif_makeimlist.py
@@ -11,7 +11,7 @@ def hif_makeimlist(vis=None, imagename=None, intent=None, field=None,
                    nchan=None, start=None, width=None, nbins=None,
                    robust=None, uvtaper=None, clearlist=None, per_eb=None,
                    per_session=None, calcsb=None, datatype=None, datacolumn=None,
-                   allow_wproject, parallel=None):
+                   allow_wproject=None, parallel=None):
 
     """
     hif_makeimlist ---- Compute list of clean images to be produced