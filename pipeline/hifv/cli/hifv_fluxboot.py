b36daee8dd26b52442a3ea77a1453fe14f28eb9f
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2024-12-23 11:03:47 +0900

    PIPE-2385 centralize parameter description of hifv_fluxboot to FluxbootInputs

--- pipeline/hifv/cli/hifv_fluxboot.py
+++ pipeline/hifv/cli/hifv_fluxboot.py
@@ -3,28 +3,13 @@ import sys
 import pipeline.h.cli.utils as utils
 
 
+# docstring and type hints: inherits from hifv.tasks.fluxscale.fluxboot.FluxbootInputs.__init__
 @utils.cli_wrapper
 def hifv_fluxboot(vis=None, caltable=None, fitorder=None, refantignore=None, refant=None):
 
     """Fluxboot
 
     Determine flux density bootstrapping for gain calibrators relative to flux calibrator.
-
-    Args:
-        vis: The list of input MeasurementSets. Defaults to the list of MeasurementSets specified in the h_init or hifv_importdata task.
-
-        caltable: String name of the flagged caltable.
-
-        fitorder: Polynomial order of the spectral fitting for valid flux densities with multiple spws.  The default value of -1 means that the heuristics determine the fit order based on
-            fractional bandwidth and receiver bands present in the observation.
-            An override value of 1,2,3 or 4 may be specified by the user.
-            Spectral index (1) and, if applicable, curvature (2) are reported in the weblog.
-            If no determination can be made by the heuristics, a fitorder of 1 will be used.
-
-        refantignore: String list of antennas to ignore Example:  refantignore='ea02, ea03'
-
-        refant: A csv string of reference antenna(s). When used, disables ``refantignore``. 
-            Example: refant = 'ea01, ea02'
 
     Returns:
         The results object for the pipeline task is returned.
