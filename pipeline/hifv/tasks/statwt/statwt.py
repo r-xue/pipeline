29aa91dd1c5fd4dfe593f2a89acf112df19faf3e
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2024-12-23 12:02:53 +0900

    PIPE-2385 centralize parameter description of hifv_statwt to StatwtInputs

--- pipeline/hifv/tasks/statwt/statwt.py
+++ pipeline/hifv/tasks/statwt/statwt.py
@@ -35,7 +35,25 @@ class StatwtInputs(vdp.StandardInputs):
         else:
             return unprocessed
 
+    # docstring and type hints: supplements hifv_statwt
     def __init__(self, context, vis=None, datacolumn=None, overwrite_modelcol=None, statwtmode=None):
+        """Initialize Inputs.
+
+        Args:
+            context: Pipeline context.
+
+            vis: The list of input MeasurementSets. Defaults to the list of MeasurementSets specified in the h_init or hifv_importdata task.
+
+            datacolumn: Data column used to compute weights. Supported values are "data", "corrected", "residual", and "residual_data"
+                (case insensitive, minimum match supported).
+
+            overwrite_modelcol: Always write the model column, even if it already exists.
+
+            statwtmode: Sets the weighting parameters for general VLA ('VLA') or VLASS Single Epoch ('VLASS-SE') use case. Note that the 'VLASS-SE'
+                mode is meant to be used with datacolumn='residual_data'.
+                Default is 'VLA'.
+
+        """
         super(StatwtInputs, self).__init__()
         self.context = context
         self.vis = vis
