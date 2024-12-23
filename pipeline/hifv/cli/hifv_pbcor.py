968b54ff9dc7050729485bb559151017dec4300c
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2024-12-23 11:20:41 +0900

    PIPE-2385 centralize parameter description of hifv_pbcor to PbcorInputs

--- pipeline/hifv/cli/hifv_pbcor.py
+++ pipeline/hifv/cli/hifv_pbcor.py
@@ -3,13 +3,11 @@ import sys
 import pipeline.h.cli.utils as utils
 
 
+# docstring and type hints: inherits from hifv.tasks.pbcor.pbcor.PbcorInputs.__init__
 @utils.cli_wrapper
 def hifv_pbcor(vis=None):
 
     """Apply primary beam correction to VLA and VLASS images.
-
-    Args:
-        vis: List of input visibility data.
 
     Returns:
         The results object for the pipeline task is returned.
