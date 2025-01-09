b95fb7d85968b6f121ae206c6a54b576279d4c97
Stewart Williams <stewart.williams@stfc.ac.uk>
2025-01-09 14:46:21 +0000

    PIPE-1770: refactor getSpecSetup to operate on domain objects
    Refactors qa_utils.getSpecSetup to populate the cache dict through
    inspection of pipeline domain objects rather than msmd calls.

--- pipeline/hifa/tasks/applycal/extern/qa_utils.py
+++ pipeline/hifa/tasks/applycal/extern/qa_utils.py
@@ -1,12 +1,7 @@
-import os
-import pickle
-
 import numpy as np
 
 from pipeline.domain import MeasurementSet
 from pipeline.domain.measures import FrequencyUnits
-from pipeline.infrastructure import casa_tools
-from pipeline.infrastructure.utils import conversion
 
 #This file contains functions that applycalqa used to borrow from AnalysisUtils, but given how  unreliable that one huge file is,
 #the very few functions we are actually using were moved here
@@ -15,78 +10,38 @@ from pipeline.infrastructure.utils import conversion
 SSOfieldnames = {'Ceres', 'Pallas', 'Vesta', 'Venus', 'Mars', 'Jupiter', 'Uranus', 'Neptune', 'Ganymede', 'Titan', 'Callisto', 'Juno', 'Europa'}
 
 
-def getSpwList(msmd,intent='OBSERVE_TARGET#ON_SOURCE',tdm=True,fdm=True, sqld=False):
-    spws = msmd.spwsforintent(intent)
-    almaspws = msmd.almaspws(tdm=tdm,fdm=fdm,sqld=sqld)
-    scienceSpws = np.intersect1d(spws,almaspws)
-    return(list(scienceSpws))
+def get_spec_setup(ms: MeasurementSet, intents: list[str]):
+    """
+    Obtain spectral setup dictionary from MS.
 
-
-def getSpecSetup(myms: MeasurementSet, intents: list[str], bfolder = None, applycalQAversion=""):
-    '''Obtain spectral setup dictionary from MS.
-    Positional Parameters:
-    myms: MeasurementSet domain object
-    Keyword Parameters:
-    spwlist: List of SPWs to include in the dictionary. Default is to use all science SPWs.
-    intents: List of pipeline intents to include in the dictionary.
-    bfolder: Use buffer folder to read dictionary from previous execution of this command, saved as a pickle file.
-             Default is None. If some folder is given, but no pickle file for the requested MS is found, create
-             it with current result.
-    '''
-    spwlist = []
-
-    #If the buffer folder bfolder is used, and there is a pickle file for the spwsetup, read it
-    spwpkl = f'{bfolder}/{myms.basename})+spwsetup.v{applycalQAversion}.pkl'
-    if (bfolder is not None) and os.path.exists(spwpkl):
-        print("Buffering existing spectral setup at : "+spwpkl)
-        #Pickle file for spw setup
-        spwpklfile = open(spwpkl, 'rb')
-        spwsetup = pickle.load(spwpklfile)
-        spwpklfile.close()
-        # print("using bfolder")
-        return spwsetup
-
-    #Else read in the information from the MS
-    #if spwlist is empty, get all science SPWs
-    with casa_tools.MSMDReader(myms.basename) as msmd:
-        if len(spwlist) == 0:
-            spwlist = getSpwList(msmd)
-        spwsetup = {}
-        spwsetup['spwlist'] = spwlist
-        spwsetup['intentlist'] = intents
-        spwsetup['scan'] = {}
-        spwsetup['fieldname'] = {}
-        spwsetup['fieldid'] = {}
-        pipe_intents_in_ms = [i for i in intents if i in myms.intents]
-        for pipe_intent in pipe_intents_in_ms:
-            casa_intent = conversion.to_CASA_intent(myms, pipe_intent)
-            spwsetup['scan'][pipe_intent] = list(msmd.scansforintent(casa_intent))
-            spwsetup['fieldname'][pipe_intent] = list(msmd.fieldsforintent(casa_intent, asnames=True))
-            spwsetup['fieldid'][pipe_intent] = list(msmd.fieldsforintent(casa_intent, asnames=False))
-
-        spwsetup['antids'] = msmd.antennasforscan(scan = spwsetup['scan'][intents[0]][0])
-
-        #Get SPW info
-        for spwid in spwlist:
-            spwsetup[spwid] = {}
-            #Get SPW information: frequencies of each channel, etc.
-            chanfreqs = msmd.chanfreqs(spw=spwid)
-            nchan = len(chanfreqs)
-            spwsetup[spwid]['chanfreqs'] = chanfreqs
-            spwsetup[spwid]['nchan'] = nchan
-            #Get data descriptor for each SPW
-            spwsetup[spwid]['ddi'] = msmd.datadescids(spw=spwid)[0]
-            spwsetup[spwid]['npol'] = msmd.ncorrforpol(msmd.polidfordatadesc(spwsetup[spwid]['ddi']))
-
-        #Save SPW setup in buffer folder
-        if (bfolder is not None):
-            spwpkl = f'{bfolder}/{myms.basename}_spwsetup.v{applycalQAversion}.pkl'
-            print('Writing pickle dump of SPW setup to '+spwpkl)
-            pklfile = open(spwpkl, 'wb')
-            pickle.dump(spwsetup, pklfile, protocol=2)
-            pklfile.close()
+    @param ms: MeasurementSet domain object
+    @param intents: List of pipeline intents to include in the dictionary.
+    """
+    science_spws = ms.get_spectral_windows(intent=','.join(intents))
+    spwsetup = {}
+    spwsetup['spwlist'] = [spw.id for spw in science_spws]
+    spwsetup['intentlist'] = intents
+    spwsetup['scan'] = {intent: [scan.id for scan in ms.get_scans(scan_intent=intent)]
+                        for intent in intents
+                        if len(ms.get_scans(scan_intent=intent)) > 0}
+    spwsetup['fieldid'] = {intent: [field.id for field in ms.get_fields(intent=intent)]
+                           for intent in intents
+                           if len(ms.get_fields(intent=intent)) > 0}
+    spwsetup['fieldname'] = {intent: [field.name for field in ms.get_fields(intent=intent)]
+                             for intent in intents
+                             if len(ms.get_fields(intent=intent)) > 0}
+    spwsetup['antids'] = sorted([ant.id for ant in ms.get_scans(scan_intent=intents[0])[0].antennas])
+    for spw in science_spws:
+        spwsetup[spw.id] = {
+            # prototype cached the channel centre frequencies
+            'chanfreqs': np.array([float((c.high + c.low).to_units(FrequencyUnits.HERTZ) / 2) for c in spw.channels]),
+            'nchan': len(spw.channels),
+            'ddi': ms.get_data_description(spw=spw.id).id,
+            'npol': ms.get_data_description(spw=spw.id).num_polarizations
+        }
 
     return spwsetup
+
 
 def get_intents_to_process(ms: MeasurementSet, intents: list[str]) -> list[str]:
     """
