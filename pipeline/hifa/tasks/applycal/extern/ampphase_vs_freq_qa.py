0efb9f29ba5ceb425b3e35d14803d8fa570f4f95
Stewart Williams <stewart.williams@stfc.ac.uk>
2025-02-06 11:13:38 +0000

    PIPE-1770: migrate score_X_vs_freq_fits
    Moves score_X_vs_freq_fits from prototype to module

--- pipeline/hifa/tasks/applycal/extern/ampphase_vs_freq_qa.py
+++ pipeline/hifa/tasks/applycal/extern/ampphase_vs_freq_qa.py
@@ -1,12 +1,7 @@
-import functools
-import operator
-
-import numpy as np
-
 import pipeline.infrastructure.logging as logging
 from ..ampphase_vs_freq_qa import get_median_fit, PHASE_REF_FN, \
     AMPLITUDE_SLOPE_THRESHOLD, AMPLITUDE_INTERCEPT_THRESHOLD, PHASE_SLOPE_THRESHOLD, PHASE_INTERCEPT_THRESHOLD, \
-    score_fits
+    score_X_vs_freq_fits
 
 LOG = logging.get_logger(__name__)
 
@@ -52,44 +47,5 @@ def score_all(all_fits, outlier_fn, unitfactor, flag_all: bool = False):
         # TODO only difference with original is passing of unitdicts in the following call
         scores = score_X_vs_freq_fits(all_fits, v[0], v[1], outlier_fn, threshold, unitfactor)
         outliers.extend(scores)
-
-    return outliers
-
-
-def score_X_vs_freq_fits(all_fits, attr, ref_value_fn, outlier_fn, sigma_threshold, unitfactor):
-    """
-    Score a set of best fits, comparing the fits identified by the 'attr'
-    attribute against a reference value calculated by the ref_value_fn,
-    marking outliers that deviate by more than sigma_threshold from this
-    reference value as outliers, to be returned as Outlier objects created by
-    the outlier_fn.
-
-    :param all_fits: a list of fit parameters
-    :param attr: identifier of the fits to consider, e.g., 'amp.slope'
-    :param ref_value_fn: a function that takes a list of fits and returns a
-        value to be used as a reference value in fit comparisons
-    :param outlier_fn: a function returning Outlier objects
-    :param sigma_threshold: the nsigma threshold to be considered an outlier
-    :return: list of Outlier objects
-    """
-    # convert linear fit metadata to a reason that identifies this fit as
-    # originating from this metric in a wider context, e.g., from 'amp.slope'
-    # to 'amp_vs_freq.slope'
-    y_axis, fit_parameter = attr.split('.')
-    reason = f'{y_axis}_vs_freq.{fit_parameter}'
-    outlier_fn_wreason = functools.partial(outlier_fn, reason={reason, })
-
-    accessor = operator.attrgetter(attr)
-    outliers = score_fits(all_fits, ref_value_fn, accessor, outlier_fn_wreason, sigma_threshold, unitfactor)
-
-    # Check for >90deg phase offsets which should have extra QA messages
-    if (y_axis == 'phase') and (fit_parameter == 'intercept'):
-        for i in range(len(outliers)):
-            if outliers[i].delta_physical > 90.0:
-                outliers[i] = outlier_fn(ant=outliers[i].ant, pol=outliers[i].pol,
-                                         num_sigma=outliers[i].num_sigma,
-                                         delta_physical=outliers[i].delta_physical,
-                                         amp_freq_sym_off=outliers[i].amp_freq_sym_off,
-                                         reason={f'gt90deg_offset_{y_axis}_vs_freq.{fit_parameter}', })
 
     return outliers
