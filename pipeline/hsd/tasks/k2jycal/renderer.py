f595b361116a4048df8a7bdfd863606e2ef70257
iihnatie <ihor.ihnatiev@nao.ac.jp>
2025-03-06 14:21:14 +0900

    Final edits to renderer

--- pipeline/hsd/tasks/k2jycal/renderer.py
+++ pipeline/hsd/tasks/k2jycal/renderer.py
@@ -52,14 +52,16 @@ class T2_4MDetailsSingleDishK2JyCalRenderer(basetemplates.T2_4MDetailsDefaultRen
         ms_list = []
         reffile_list = []
         spw_tr = collections.defaultdict(list)
-        dovirtual = sdutils.require_virtual_spw_id_handling(context.observing_run)
-        trfunc_r = lambda _vspwid, _vis, _rspwid, _antenna, _pol, _factor: JyperKTR(_rspwid, _vis, _antenna, _pol, _factor)
-        trfunc_v = lambda _vspwid, _vis, _rspwid, _antenna, _pol, _factor: JyperKTRV(_vspwid, _vis, _rspwid, _antenna, _pol, _factor)
-        trfunc = trfunc_v if dovirtual else trfunc_r
         
+        dovirtual = sdutils.require_virtual_spw_id_handling(context.observing_run)
+        trfunc_r = lambda vsp, vis, rsp, ant, pol, factor: JyperKTR(rsp, vis, ant, pol, factor)
+        trfunc_v = lambda vsp, vis, rsp, ant, pol, factor: JyperKTRV(vsp, vis, rsp, ant, pol, factor)
+        trfunc = trfunc_v if dovirtual else trfunc_r
+
+        # Process each calibration result.
         for r in results:
             ms = context.observing_run.get_ms(name=r.vis)
-            ms_label = ms.basename 
+            ms_label = ms.basename
             ms_list.append(ms_label)
             spw_list = list(ms.get_spectral_windows(science_windows_only=True))
             antennas = list(ms.get_antenna())
@@ -68,86 +70,90 @@ class T2_4MDetailsSingleDishK2JyCalRenderer(basetemplates.T2_4MDetailsDefaultRen
             for spw in spw_list:
                 spwid = spw.id
                 vspwid = context.observing_run.real2virtual_spw_id(spwid, ms)
+                # Initialize spw_data entry if needed.
                 if vspwid not in spw_data:
                     spw_data[vspwid] = {
-                        "spw_obj": spw, 
-                        "all_factors" : [],
+                        "spw_obj": spw,
+                        "all_factors": [],
                         "ms_dict": collections.defaultdict(list),
-                        "outliers":[]
+                        "outliers": []
                     }
                 ddid = ms.get_data_description(spw=spwid)
+                # Get polarization labels.
                 corrs = [ddid.get_polarization_label(i) for i in range(ddid.num_polarizations)]
                 for ant in antennas:
                     ant_name = ant.name
                     for corr in corrs:
                         factor = self.__get_factor(factors_data, ms_label, spwid, ant_name, corr)
-                        # Store the data in ms_dict
                         if factor is not None:
-                            spw_data[vspwid]["ms_dict"][ms_label].append(
-                                (factor, corr, ant_name)
-                            )
+                            spw_data[vspwid]["ms_dict"][ms_label].append((factor, corr, ant_name))
                             spw_data[vspwid]["all_factors"].append(factor)
+                        # Always record the transformation, using a default if factor is None.
                         jyperk = factor if factor is not None else 'N/A (1.0)'
                         spw_tr[vspwid].append(trfunc(vspwid, ms_label, spwid, ant_name, corr, jyperk))
             reffile_list.append(r.reffile)
         reffile_list = list(dict.fromkeys(reffile_list))
         
-        # check for outliers and log warnings
-        def calculate_stats(values, r=3):
-            m = mean(values)
-            s = std(values) if len(values) > 1 else 0.0
-            return {
-                "upper_limit": m + r*s,
-                "lower_limit": m - r*s,
-                "mean": m,
-                "std": s
-            }
-
+        # Capture warnings for outliers.
         extra_logrecords_handler = logging.CapturingHandler(logging.WARNING)
         logging.add_handler(extra_logrecords_handler)
         
+        # Compute stats and flag outliers for each SPW.
         for spw_id, spw_info in spw_data.items():
-            stats = calculate_stats(spw_info["all_factors"], r=3)
-            u, l, m, s = stats["upper_limit"], stats["lower_limit"], stats["mean"], stats["std"]
+            if not spw_info["all_factors"]:
+                continue
+            stats = self.__calculate_stats(spw_info["all_factors"], r=3)
+            upper, lower, m_val, s_val = stats["upper_limit"], stats["lower_limit"], stats["mean"], stats["std"]
             for ms_label, factor_list in spw_info["ms_dict"].items():
-                for (factor, corr, ant_name) in factor_list:
-                    if factor < l or factor > u:
-                        spw_data[spw_id]["outliers"].append((ms_label, factor))
+                for factor, corr, ant in factor_list:
+                    if factor < lower or factor > upper:
+                        spw_info["outliers"].append((ms_label, factor))
                         LOG.warning(
-                            "Value of factor %f for pol %s, spw %s on antenna %s in ms '%s' "
-                            "is significantly away from mean %f (std %f).",
-                            factor, corr, spw_id, ant_name, ms_label, m, s
+                            "Value of factor %f for pol %s, spw %s on antenna %s in ms '%s' is significantly away from mean %f (std %f).",
+                            factor, corr, spw_id, ant, ms_label, m_val, s_val
                         )
-
+        
         logging.remove_handler(extra_logrecords_handler)
         extra_logrecords = extra_logrecords_handler.buffer
 
-        # input Jy/K files
-        stage_dir = os.path.join(context.report_dir, 'stage%s' % results.stage_number)
-        reffile_list = list(dict.fromkeys(reffile_list))   # remove duplicated filenames
+        stage_dir = os.path.join(context.report_dir, f'stage{results.stage_number}')
         reffile_copied_list = []
         for reffile in reffile_list:
             if reffile is not None and os.path.exists(reffile):
-                LOG.debug('copying %s to %s' % (reffile, stage_dir))
+                LOG.debug('copying %s to %s', reffile, stage_dir)
                 shutil.copy2(reffile, stage_dir)
-                reffile_copied_list.append( os.path.join(stage_dir, os.path.basename(reffile)) )
-        reffile_copied = None if len(reffile_copied_list) == 0 else reffile_copied_list
-
-        hist_plots = []
-        any_factors = any(len(spw_info["ms_dict"]) > 0 for spw_info in spw_data.values())
-        if any_factors:
+                reffile_copied_list.append(os.path.join(stage_dir, os.path.basename(reffile)))
+        reffile_copied = None if not reffile_copied_list else reffile_copied_list
+        plots = []
+        if any(len(info["ms_dict"]) > 0 for info in spw_data.values()):
             task = display.K2JyBoxScatterDisplay(stage_dir, spw_data, ms_list)
-            hist_plots.extend(task.plot())
+            plots.extend(task.plot())
         
-        # order table rows so that spw comes first
+        # Merge transformation rows for display.
         row_values = []
         for factor_list in spw_tr.values():
-            row_values += list(factor_list) 
-        ctx.update({'jyperk_rows': utils.merge_td_columns(row_values),
-                    'reffile_list': reffile_copied,
-                    'jyperk_hist': hist_plots,
-                    'dovirtual': dovirtual,
-                    'extra_logrecords': extra_logrecords})
+            row_values.extend(factor_list)
+        
+        # Update the context with tables, plots, and additional info.
+        ctx.update({
+            'jyperk_rows': utils.merge_td_columns(row_values),
+            'reffile_list': reffile_copied,
+            'jyperk_hist': plots,
+            'dovirtual': dovirtual,
+            'extra_logrecords': extra_logrecords
+        })
+
+    @staticmethod
+    def __calculate_stats(values: list = [], r: float = 3) -> Dict[str, float]:
+        """ Helper to compute statistics """
+        m = mean(values)
+        s = std(values) if len(values) > 1 else 0.0
+        return {
+            "upper_limit": m + r * s,
+            "lower_limit": m - r * s,
+            "mean": m,
+            "std": s
+        }
 
     @staticmethod
     def __get_factor(
