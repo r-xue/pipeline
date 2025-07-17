25dc6dd444a1254f1effabe512ac52f59daaa809
Akira Yoshino <yoshino.akira@nao.ac.jp>
2025-07-17 12:25:09 +0900

    PIPE-2168: Refactored about antenna list in qa.py.

--- pipeline/hsd/tasks/applycal/qa.py
+++ pipeline/hsd/tasks/applycal/qa.py
@@ -27,26 +27,13 @@ class SDApplyCalQAHandler(pqa.QAPlugin):
         vis = os.path.basename(result.inputs['vis'])
         ms = context.observing_run.get_ms(vis)
         spwids = [spws.id for spws in ms.get_spectral_windows()]
-        ants = [str(i.name) for i in ms.get_antenna()]
+        ants = ['all']
+        ants.extend([str(i.name) for i in ms.get_antenna()])
         figroot = os.path.join(context.report_dir,
                                'stage%s' % result.stage_number)
         score = 1.0
         scores = []
         for spw in spwids:
-            prefix = '{vis}-{y}_vs_{x}-{ant}-spw{spw}'.format(
-                vis=vis, y='real', x='time', ant='all', spw=spw)
-            figfile = os.path.join(figroot, '{prefix}.png'.format(prefix=prefix))
-            is_figfile_exists = os.path.exists(figfile)
-            if is_figfile_exists:
-                shortmsg = 'Generating amp vs time plot was successful.'
-                longmsg = 'Generating amp vs time plot for spw{0} and ant=all of {1} was successful.'.format(spw, vis)
-            else:
-                shortmsg = 'Generating amp vs time plot was failed.'
-                longmsg = 'Generating amp vs time plot for spw{0} and ant=all of {1} was failed.'.format(spw, vis)
-                score = 0.65
-            scores = [pqa.QAScore(score, longmsg=longmsg, shortmsg=shortmsg)]
-            result.qa.pool.extend(scores)
-
             for ant in ants:
                 prefix = '{vis}-{y}_vs_{x}-{ant}-spw{spw}'.format(
                     vis=vis, y='real', x='time', ant=ant, spw=spw)
@@ -54,10 +41,10 @@ class SDApplyCalQAHandler(pqa.QAPlugin):
                 is_figfile_exists = os.path.exists(figfile)
                 if is_figfile_exists:
                     shortmsg = 'Generating amp vs time plot was successful.'
-                    longmsg = 'Generating amp vs time plot for spw{0} and {1} of {2} was successful.'.format(spw, ant, vis)
+                    longmsg = 'Generating amp vs time plot for Spw{0} and Antenna={1} of {2} was successful.'.format(spw, ant, vis)
                 else:
                     shortmsg = 'Generating amp vs time plot was failed.'
-                    longmsg = 'Generating amp vs time plot for spw{0} and {1} of {2} was failed.'.format(spw, ant, vis)
+                    longmsg = 'Generating amp vs time plot for Spw{0} and Antenna={1} of {2} was failed.'.format(spw, ant, vis)
                     score = 0.65
                 scores = [pqa.QAScore(score, longmsg=longmsg, shortmsg=shortmsg)]
                 result.qa.pool.extend(scores)
