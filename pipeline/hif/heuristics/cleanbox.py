PIPE-2464: Fixed various IQUV statistics calculation bugs.

pipeline/hif/heuristics/cleanbox.py
@@ -241,6 +241,7 @@ def analyse_clean_result(multiterm, model, restored, residual, pb, cleanmask, pb
 
             # define mask outside the cleaned area
             image_stats = None
+            image_stats_iquv = None
             if pb is not None and os.path.exists(pb+extension) and cleanmask is not None and os.path.exists(cleanmask):
                 pb_name = os.path.basename(pb)+extension
                 have_mask = True
@@ -252,6 +253,8 @@ def analyse_clean_result(multiterm, model, restored, residual, pb, cleanmask, pb
                 # Check for number of points per channel (PIPE-541):
                 try:
                     image_stats = image.statistics(mask=statsmask, robust=True, axes=[0, 1, 2], algorithm='chauvenet', maxiter=5, stretch=True)
+                    # For IQUV images we need the individual values along the Stokes axis
+                    image_stats_iquv = image.statistics(mask=statsmask, robust=True, axes=[0, 1], algorithm='chauvenet', maxiter=5, stretch=True)
                     if image_stats['npts'].shape == (0,) or np.median(image_stats['npts']) < 10.0:
                         # Switch to full annulus to avoid zero noise spectrum due to voluminous mask
                         LOG.warning('Using full annulus for noise spectrum due to voluminous mask "%s".' %
@@ -259,12 +262,14 @@ def analyse_clean_result(multiterm, model, restored, residual, pb, cleanmask, pb
                         statsmask = '("%s" > %f) && ("%s" < %f)' % (pb_name, pblimit_image,
                                                                     pb_name, pblimit_cleanmask)
                         image_stats = None
+                        image_stats_iquv = None
                 except Exception as e:
                     # Try full annulus as a fallback
                     LOG.exception('Using full annulus for noise spectrum due to voluminous mask "%s".' % (os.path.basename(cleanmask)), exc_info=e)
                     statsmask = '("%s" > %f) && ("%s" < %f)' % (pb_name, pblimit_image,
                                                                 pb_name, pblimit_cleanmask)
                     image_stats = None
+                    image_stats_iquv = None
             elif pb is not None and os.path.exists(pb+extension):
                 pb_name = os.path.basename(pb)+extension
                 have_mask = True
@@ -285,9 +290,7 @@ def analyse_clean_result(multiterm, model, restored, residual, pb, cleanmask, pb
                 # Avoid repeat if the check for npts was done and is OK.
                 if image_stats is None:
                     image_stats = image.statistics(mask=statsmask, robust=True, axes=[0, 1, 2], algorithm='chauvenet', maxiter=5, stretch=True)
-
-                # For IQUV images we need the individual values along the Stokes axis
-                image_stats_iquv = image.statistics(mask=statsmask, robust=True, axes=[0, 1, 3], algorithm='chauvenet', maxiter=5, stretch=True)
+                    image_stats_iquv = image.statistics(mask=statsmask, robust=True, axes=[0, 1], algorithm='chauvenet', maxiter=5, stretch=True)
 
                 nonpbcor_image_statsmask = statsmask
 
@@ -300,10 +303,14 @@ def analyse_clean_result(multiterm, model, restored, residual, pb, cleanmask, pb
                     cont_chan_ranges = utils.freq_selection_to_channels(nonpbcor_imagename, cont_freq_ranges)
                     cont_chan_indices = np.hstack([np.arange(start, stop+1) for start, stop in cont_chan_ranges])
                     nonpbcor_image_non_cleanmask_rms_vs_chan = image_stats['rms'][cont_chan_indices]
+                    nonpbcor_image_non_cleanmask_rms_vs_chan_iquv = image_stats_iquv['rms'][:,cont_chan_indices]
                 else:
                     nonpbcor_image_non_cleanmask_rms_vs_chan = image_stats['rms']
+                    nonpbcor_image_non_cleanmask_rms_vs_chan_iquv = image_stats_iquv['rms']
 
                 nonpbcor_image_non_cleanmask_robust_rms = image_stats['medabsdevmed'] * 1.4826
+                # TODO: The IQUV version would be needed for plot_spectra of QUV -> probably PL2026
+                #nonpbcor_image_non_cleanmask_robust_rms_iquv = image_stats_iquv['medabsdevmed'] * 1.4826
 
                 nonpbcor_image_non_cleanmask_rms_median = np.median(nonpbcor_image_non_cleanmask_rms_vs_chan)
                 nonpbcor_image_non_cleanmask_rms_mean = np.mean(nonpbcor_image_non_cleanmask_rms_vs_chan)
@@ -311,7 +318,11 @@ def analyse_clean_result(multiterm, model, restored, residual, pb, cleanmask, pb
                 nonpbcor_image_non_cleanmask_rms_max = np.max(nonpbcor_image_non_cleanmask_rms_vs_chan)
                 nonpbcor_image_non_cleanmask_rms = nonpbcor_image_non_cleanmask_rms_median
 
-                nonpbcor_image_non_cleanmask_rms_iquv = image_stats_iquv['rms']
+                nonpbcor_image_non_cleanmask_rms_median_iquv = np.median(nonpbcor_image_non_cleanmask_rms_vs_chan_iquv, axis=(1,))
+                nonpbcor_image_non_cleanmask_rms_mean_iquv = np.mean(nonpbcor_image_non_cleanmask_rms_vs_chan_iquv, axis=(1,))
+                nonpbcor_image_non_cleanmask_rms_min_iquv = np.min(nonpbcor_image_non_cleanmask_rms_vs_chan_iquv, axis=(1,))
+                nonpbcor_image_non_cleanmask_rms_max_iquv = np.max(nonpbcor_image_non_cleanmask_rms_vs_chan_iquv, axis=(1,))
+                nonpbcor_image_non_cleanmask_rms_iquv = nonpbcor_image_non_cleanmask_rms_median_iquv
 
                 if have_mask:
                     area_text = 'annulus'
@@ -372,4 +383,5 @@ def analyse_clean_result(multiterm, model, restored, residual, pb, cleanmask, pb
              'nonpbcor_image_cleanmask_npoints': nonpbcor_image_cleanmask_npoints,
              'cont_freq_ranges': cont_freq_ranges,
              'nonpbcor_image_statsmask': nonpbcor_image_statsmask},
-            pbcor_image_min_iquv, pbcor_image_max_iquv, nonpbcor_image_non_cleanmask_rms_iquv)
+            pbcor_image_min_iquv, pbcor_image_max_iquv, nonpbcor_image_non_cleanmask_rms_min_iquv,
+            nonpbcor_image_non_cleanmask_rms_max_iquv, nonpbcor_image_non_cleanmask_rms_iquv)