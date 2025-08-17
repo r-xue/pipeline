749b797d9c89538a7a2622ac6aef01c3747697b5
Rui Xue <rxue@nrao.edu>
2025-08-16 21:34:00 -0400

    PIPE-2674: replace the "in-house" `/etc/os-release` parser with `platform.freedesktop_os_release()`.

--- pipeline/environment.py
+++ pipeline/environment.py
@@ -3,7 +3,6 @@ environment.py defines functions and variables related to the execution environm
 """
 from __future__ import annotations
 
-import ast
 import dataclasses
 import json
 import operator
@@ -16,6 +15,7 @@ import typing
 from importlib.metadata import version, PackageNotFoundError
 from importlib.util import find_spec
 from pathlib import Path
+import platform
 
 import casatasks
 import pkg_resources
@@ -210,38 +210,11 @@ class LinuxEnvironment(CommonEnvironment):
         self.cgroup_cpu_weight = str(CGroupLimit.CPUWeight.get_limit(cgroup_controllers))
         self.cgroup_mem_limit = str(CGroupLimit.MemoryLimit.get_limit(cgroup_controllers))
 
-        os_release = dict(self.read_os_release())
+        # PIPE-2674: use the standard `platform` APIs:
+        # https://docs.python.org/3/library/platform.html#platform.freedesktop_os_release
+        os_release = platform.freedesktop_os_release()
         self.host_distribution = (f'{os_release.get("NAME", "Linux")} '
                                   f'{os_release.get("VERSION", "(unknown distribution)")}')
-
-    @staticmethod
-    def read_os_release():
-        """
-        Reads OS release information from disk.
-
-        Taken from https://www.freedesktop.org/software/systemd/man/latest/os-release.html
-        """
-        # in Python >= 3.10 we could do this:
-        # return platform.freedesktop_os_release()
-        try:
-            filename = '/etc/os-release'
-            f = open(filename)
-        except FileNotFoundError:
-            filename = '/usr/lib/os-release'
-            f = open(filename)
-
-        for line_number, line in enumerate(f, start=1):
-            line = line.rstrip()
-            if not line or line.startswith('#'):
-                continue
-            m = re.match(r'([A-Z][A-Z_0-9]+)=(.*)', line)
-            if m:
-                name, val = m.groups()
-                if val and val[0] in '"\'':
-                    val = ast.literal_eval(val)
-                yield name, val
-            else:
-                LOG.warning(f'{filename}:{line_number}: bad line {line!r}')
 
     @staticmethod
     def _from_lscpu(lscpu_json: dict, key: str) -> str:
