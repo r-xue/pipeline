710fe7b6c48490382dee40924d30b5e1cf81978f
Kristin Berry <kberry@nrao.edu>
2025-09-11 10:48:52 -0400

    PIPE-2838: Add ms name to no issues qa message

--- pipeline/hifa/tasks/bandpass/qa.py
+++ pipeline/hifa/tasks/bandpass/qa.py
@@ -517,8 +517,8 @@ def _calc_subband_qa_score(spw_dict: dict, ms: MeasurementSet, caltable: str) ->
 
     if f_spw <= 0.0:
         score = 1.0
-        shortmsg = "No correlator subband issues detected"
-        longmsg = "No correlator subband issues detected"
+        shortmsg = "{ms.basename}: No correlator subband issues detected"
+        longmsg = "{ms.basename}: No correlator subband issues detected"
     else:
         # See PIPE-2103 for more information
         qa_max = 0.65
