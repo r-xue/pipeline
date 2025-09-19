aa4ea9ae1bed34b53355830059e898aed5984543
Rui Xue <rxue@nrao.edu>
2025-09-19 09:51:07 -0500

    PIPE-2854: disable smart quotes.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -114,6 +114,9 @@ add_module_names = False
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# PIPE-2854: diable smartquotes
+smartquotes = False
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 
