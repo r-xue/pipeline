7c0a9c3317177fd8dd26e4e16fb1502dfba553ad
Ajay Vibhute <avibhute@nrao.edu>
2026-04-02 09:12:07 -0600

    PIPE-2461: minor bugfix

--- pipeline/hifv/tasks/pbcor/pbcor.py
+++ pipeline/hifv/tasks/pbcor/pbcor.py
@@ -107,7 +107,7 @@ class Pbcor(basetask.StandardTaskTemplate):
                     for suffix in [".alpha", ".alpha.error"]:
                         alpha_file = basename + suffix
                         if not os.path.exists(alpha_file):
-                            if sci_im['multiterm'] > 1 or sci_im['specmode'] == 'cube':
+                            if sci_im['multiterm'] > 1:
                                 LOG.warning(f"Alpha image {alpha_file} not found, skipping header update.")
                             continue
                         with casa_tools.ImageReader(alpha_file) as image:
