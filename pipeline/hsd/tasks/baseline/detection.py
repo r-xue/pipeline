9094b50d9351ad4395fa4560f2ddc754c5bf9d40
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2026-04-28 17:17:28 +0900

    import numpy as np

--- pipeline/hsd/tasks/baseline/detection.py
+++ pipeline/hsd/tasks/baseline/detection.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 import collections
 import math
 import numbers
-import numpy
+import numpy as np
 import os
 import time
 from typing import TYPE_CHECKING, Any
@@ -160,8 +160,8 @@ class DetectLine(basetask.StandardTaskTemplate):
 
     def prepare(self,
                 datatable_dict: dict,
-                grid_table: list[list[int | float | numpy.ndarray]],
-                spectral_data: numpy.ndarray | None = None) -> DetectLineResults:
+                grid_table: list[list[int | float | np.ndarray]],
+                spectral_data: np.ndarray | None = None) -> DetectLineResults:
         """Find spectral line feature.
 
         The process finds emission lines and determines protection regions for baselinefit
@@ -323,7 +323,7 @@ class DetectLine(basetask.StandardTaskTemplate):
         return result
 
     def plot_detectrange(self,
-                         sp: numpy.ndarray,
+                         sp: np.ndarray,
                          protected: list[int],
                          fname: str) -> None:
         """Plot detected line range for testing.
@@ -345,9 +345,9 @@ class DetectLine(basetask.StandardTaskTemplate):
         plt.savefig(fname, format='png')
 
     def MaskBinning(self,
-                    data: numpy.ndarray,
+                    data: np.ndarray,
                     Bin: int,
-                    offset: int = 0) -> numpy.ndarray:
+                    offset: int = 0) -> np.ndarray:
         """Perform Binning for mask array.
 
         Args:
@@ -361,12 +361,12 @@ class DetectLine(basetask.StandardTaskTemplate):
         if Bin == 1:
             return data
         else:
-            return numpy.array([data[i:i+Bin].min() for i in range(offset, len(data)-Bin+1, Bin)], dtype=bool)
+            return np.array([data[i:i+Bin].min() for i in range(offset, len(data)-Bin+1, Bin)], dtype=bool)
 
     def SpBinning(self,
-                  data: numpy.ndarray,
+                  data: np.ndarray,
                   Bin: int,
-                  offset: int = 0) -> numpy.ndarray:
+                  offset: int = 0) -> np.ndarray:
         """Perform Binning for spectral data array.
 
         Args:
@@ -380,7 +380,7 @@ class DetectLine(basetask.StandardTaskTemplate):
         if Bin == 1:
             return data
         else:
-            return numpy.array([data[i:i+Bin].mean() for i in range(offset, len(data)-Bin+1, Bin)], dtype=float)
+            return np.array([data[i:i+Bin].mean() for i in range(offset, len(data)-Bin+1, Bin)], dtype=float)
 
     def analyse(self, result: DetectLineResults) -> DetectLineResults:
         """Analyse result.
@@ -393,8 +393,8 @@ class DetectLine(basetask.StandardTaskTemplate):
         return result
 
     def _detect(self,
-                spectrum: numpy.ndarray,
-                mask: numpy.ndarray,
+                spectrum: np.ndarray,
+                mask: np.ndarray,
                 threshold: float,
                 tweak: bool,
                 edge: list[int]) -> list[list[int]]:
@@ -558,7 +558,7 @@ class LineWindowParser:
                 processed = self._exclude_non_science_spws(
                     self._string2dict(self.window)
                 )
-        elif isinstance(self.window, (list, numpy.ndarray)):
+        elif isinstance(self.window, (list, np.ndarray)):
             # convert string into dictionary
             # keys are all science spectral window ids
             processed = self._list2dict(self.window)
@@ -770,7 +770,7 @@ class LineWindowParser:
         item_type = type(window[0])
 
         # process recursively if item is a list
-        if item_type in (list, numpy.ndarray):
+        if item_type in (list, np.ndarray):
             converted = []
             for w in window:
                 LOG.trace('_freq2chan: w=%s type %s', w, type(w))
