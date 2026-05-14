1bc3d0edf3825efa22f11bcea0a500458d391c15
Luca Di Mascolo <lucadimascolo@gmail.com>
2026-05-14 10:09:16 +0200

    PIPE-2274: fix missing check on flag dictionary

--- pipeline/h/tasks/exportdata/exportdata.py
+++ pipeline/h/tasks/exportdata/exportdata.py
@@ -937,9 +937,10 @@ class ExportData(basetask.StandardTaskTemplate):
     def _save_final_flagversion(self, vis, flag_version_name):
         """Save the final flags to a final flag version."""
         task = casa_tasks.flagmanager(vis=vis, mode='list')
-        flaglist = self._executor.execute(task)
-
-        if any(v['name'] == flag_version_name for v in flaglist.values()):
+        flag_dict = self._executor.execute(task)
+        flag_dict = filter(lambda v: isinstance(v, dict) and "name" in v, flag_dict.values())
+        
+        if any(v['name'] == flag_version_name for v in flag_dict):
             tt = int(time.time())
             tmpname = flag_version_name + '.old.' + str(tt)
 
