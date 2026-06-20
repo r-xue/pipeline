f112aa4a04739ecc20b460499c298c3007da9826
Rui Xue <rx.astro@gmail.com>
2026-06-19 19:22:22 -0500

    PIPE-2890: Update diagnostic plot description for missed line channels in `hsd_imaging`

--- pipeline/hsd/cli/hsd_imaging.py
+++ pipeline/hsd/cli/hsd_imaging.py
@@ -45,7 +45,9 @@ def hsd_imaging(mode=None, restfreq=None, infiles=None, field=None, spw=None):
       (3) maximum intensity map using line-free channels only.
 
     - **Diagnostic plots for possible missed line channels** (PL2025+): generated when line
-      emission is detected outside the line ranges from :py:func:`hsd_baseline <hsd_baseline>` (SNR threshold = 7).
+      emission is detected outside the line ranges from
+      :py:func:`hsd_baseline <hsd_baseline>` (SNR threshold = 7 for "single peak" and
+      SNR threshold = 5 for "extended").
 
     .. figure:: /figures/missedlines.png
        :scale: 60%
